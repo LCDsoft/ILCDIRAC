31cc7180813706ef212c171fe719798f4b9f0099
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-02-10 08:34:03 +0000

    Add SplitByFilesAndEvents that will be used to submit several Mokka jobs using a list of stdhep files.

---
+++ Core/Utilities/SplitByFilesAndEvent.py
@@ -0,0 +1,23 @@
+'''
+LCDDIRAC.Core.Utilities.SplitByFilesAndEvents
+
+Based on Dirac.SplitByFiles, but adding the splitting by number of events
+Gives a list of dictionaries
+
+
+Created on Feb 10, 2010
+
+@author: sposs
+'''
+
+def SplitByFilesAndEvents(listoffiles,evtsperfile):
+  list = []
+  startfromevt = 1
+  for files in listoffiles:
+    dict = {}
+    dict['file']=files
+    dict['startFrom'] = startfromevt
+    startfromevt+=evtsperfile
+    startfromevt+=1
+    list.append(dict)
+  return list
