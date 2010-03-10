75ebb757f7c2d8802f73bed85167816fcb6077d4
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-03-10 23:19:56 +0000

    rip off the mysql-local-db-setup.sh script into python

--- Core/Utilities/SQLWrapper.py
+++ Core/Utilities/SQLWrapper.py
@@ -28,7 +28,8 @@ class SQLWrapper:
       
     self.MokkaTMPDir = ''
     self.softDir = softwareDir
-        
+    self.rootpass = "rootpass"
+    
     """create tmp dir and track it"""
     try:
         self.MokkaTMPDir = tempfile.mkdtemp('','TMP',self.softDir)
@@ -57,83 +58,113 @@ class SQLWrapper:
     else:
       os.environ['LD_LIBRARY_PATH']='%s/mysql4grid/lib64/mysql'%self.softDir
     os.environ['PATH']='%s/mysql4grid/bin:%s'%(self.softDir,os.environ['PATH'])
-    comm = self.softDir+'/mokkadbscripts/mysql-local-db-setup.sh -p ' + self.MokkaTMPDir + ' -d ' + self.MokkaDumpFile
+    safe_options =  "--no-defaults --skip-networking --socket=%s/mysql.sock --datadir=%s/data  --basedir=%s/mysql4grid --pid-file=%s/mysql.pid"%(self.MokkaTMPDir,self.MokkaTMPDir,self.MokkaTMPDir,self.MokkaTMPDir)
+    #comm = self.softDir+'/mokkadbscripts/mysql-local-db-setup.sh -p ' + self.MokkaTMPDir + ' -d ' + self.MokkaDumpFile
+    comm = "mysql_install_db  %s"%(safe_options)
     self.result = shellCall(0,comm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
         
     resultTuple = self.result['Value']
 
     status = resultTuple[0]
-    self.log.info( "Status after the mysql-local-db-setup execution is %s" % str( status ) )
+    #self.log.info( "Status after the mysql-local-db-setup execution is %s" % str( status ) )
+    self.log.info( "Status after the mysql_install_db execution is %s" % str( status ) )
     failed = False
     if status != 0:
-      self.log.error( "mokka-wrapper execution completed with errors:" )
+      self.log.error( "SQLwrapper execution completed with errors:" )
       failed = True
     else:
-      self.log.info( "mokka-wrapper execution completed successfully")
+      self.log.info( "SQLwrapper execution completed successfully")
 
     if failed:
       self.log.error( "==================================\n StdError:\n" )
       self.log.error( self.stdError )
       #self.setApplicationStatus('%s Exited With Status %s' %(self.applicationName,status))
-      self.log.error('mokka-wrapper Exited With Status %s' %(status))
-      return S_ERROR('mokka-wrapper Exited With Status %s' %(status))
+      self.log.error('SQLwrapper Exited With Status %s' %(status))
+      return S_ERROR('SQLwrapper Exited With Status %s' %(status))
     # Still have to set the application status e.g. user job case.
     #self.setApplicationStatus('mokka-wrapper %s Successful' %(self.applicationVersion))
     #return S_OK('mokka-wrapper %s Successful' %(self.applicationVersion))
 
-    if str(os.environ.get('UID_TMP')) == 'None':
-      DIRAC.gLogger.error('No UID_TMP known')
-      self.UID_TMP = ''
-    else:
-      self.UID_TMP = str(os.environ.get('UID_TMP'))
+    ###Now run mysqld
+    os.chdir("%s/mysql4grid"%(self.softDir))
+    print "running mysqld_safe %s"%safe_options
+    mysqldcomm = "mysqld_safe %s"%safe_options
+    self.result = shellCall(0,mysqldcomm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
+    status = resultTuple[0]
+    #self.log.info( "Status after the mysql-local-db-setup execution is %s" % str( status ) )
+    self.log.info( "Status after the mysql_safe execution is %s" % str( status ) )
+    ###go back to previous dir
+    os.chdir("%s"%(self.softDir))
     
-        
-    if not self.MokkaTMPDir[-1] == '/':
-      self.MokkaTMPDir += '/'
-                
-    self.mysqlInstalDir = self.MokkaTMPDir + self.UID_TMP 
-        
-    #init db
-    DIRAC.gLogger.verbose('add all privileges for user consult')
-    #mysql command:
-    MySQLcomm = 'mysql'
-    MySQLparams = ' --socket ' + self.mysqlInstalDir + '/' + '/mysql.sock' + ' -e "GRANT ALL PRIVILEGES ON *.* TO \'consult\'@\'localhost\' IDENTIFIED BY \'consult\';"'
-    comm = MySQLcomm + MySQLparams
-    print "MySQLcomm %s"%comm
-    self.result = shellCall(0,comm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
-        
-    resultTuple = self.result['Value']
-
+    ###changing root pass
+    mysqladmincomm = "mysqladmin --no-defaults -hlocalhost --socket=%s/mysql.sock -uroot password %s"%(self.MokkaTMPDir,self.rootpass)
+    self.result = shellCall(0,mysqldadmincomm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
     status = resultTuple[0]
-    self.log.info( "Status after the application execution is %s" % str( status ) )
-    failed = False
-    if status != 0:
-      self.log.error( "mysql execution completed with errors:" )
-      failed = True
-    else:
-      self.log.info( "mysql execution completed successfully")
-
-    if failed:
-      self.log.error( "==================================\n StdError:\n" )
-      self.log.error( self.stdError )
-      #self.setApplicationStatus('%s Exited With Status %s' %(self.applicationName,status))
-      self.log.error('mysql Exited With Status %s' %(status))
-      return S_ERROR('mysql Exited With Status %s' %(status))
-    # Still have to set the application status e.g. user job case.
-    #self.setApplicationStatus('mokka-wrapper %s Successful' %(self.applicationVersion))
-        
-    #test query
-    DIRAC.gLogger.verbose('test query to mysql')
-    MySQLcomm = 'mysql'
-    MySQLparams = ' --socket ' + self.mysqlInstalDir + '/' + '/mysql.sock' + ' -uconsult -pconsult -e "show databases;"'
-    comm = MySQLcomm + MySQLparams
-    print "MySQLcomm %s"%comm
-         
-    self.result = shellCall(0,comm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
-        
-    resultTuple = self.result['Value']
-
+    #self.log.info( "Status after the mysql-local-db-setup execution is %s" % str( status ) )
+    self.log.info( "Status after the mysql_safe execution is %s" % str( status ) )
+    
+    ###calling mysql
+    mysqlcomm = "mysql  --no-defaults -hlocalhost --socket=%s/mysql.sock -uroot -p%s < %s"%(self.MokkaTMPDir,self.rootpass,self.MokkaDumpFile)
+    self.result = shellCall(0,mysqlcomm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
     status = resultTuple[0]
+    #self.log.info( "Status after the mysql-local-db-setup execution is %s" % str( status ) )
+    self.log.info( "Status after the mysql execution is %s" % str( status ) )
+    
+    
+#    if str(os.environ.get('UID_TMP')) == 'None':
+#      DIRAC.gLogger.error('No UID_TMP known')
+#      self.UID_TMP = ''
+#    else:
+#      self.UID_TMP = str(os.environ.get('UID_TMP'))
+#    
+#        
+#    if not self.MokkaTMPDir[-1] == '/':
+#      self.MokkaTMPDir += '/'
+#                
+#    self.mysqlInstalDir = self.MokkaTMPDir + self.UID_TMP 
+#        
+#    #init db
+#    DIRAC.gLogger.verbose('add all privileges for user consult')
+#    #mysql command:
+#    #MySQLcomm = 'mysql'
+#    MySQLcomm = 'mysql --socket ' + self.mysqlInstalDir + '/' + '/mysql.sock' + ' -e "GRANT ALL PRIVILEGES ON *.* TO \'consult\'@\'localhost\' IDENTIFIED BY \'consult\';"'
+#    
+#    comm = MySQLcomm
+#    print "MySQLcomm %s"%comm
+#    self.result = shellCall(0,comm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
+#        
+#    resultTuple = self.result['Value']
+#
+#    status = resultTuple[0]
+#    self.log.info( "Status after the application execution is %s" % str( status ) )
+#    failed = False
+#    if status != 0:
+#      self.log.error( "mysql execution completed with errors:" )
+#      failed = True
+#    else:
+#      self.log.info( "mysql execution completed successfully")
+#
+#    if failed:
+#      self.log.error( "==================================\n StdError:\n" )
+#      self.log.error( self.stdError )
+#      #self.setApplicationStatus('%s Exited With Status %s' %(self.applicationName,status))
+#      self.log.error('mysql Exited With Status %s' %(status))
+#      return S_ERROR('mysql Exited With Status %s' %(status))
+#    # Still have to set the application status e.g. user job case.
+#    #self.setApplicationStatus('mokka-wrapper %s Successful' %(self.applicationVersion))
+#        
+#    #test query
+#    DIRAC.gLogger.verbose('test query to mysql')
+#    MySQLcomm = 'mysql'
+#    MySQLparams = ' --socket ' + self.mysqlInstalDir + '/' + '/mysql.sock' + ' -uconsult -pconsult -e "show databases;"'
+#    comm = MySQLcomm + MySQLparams
+#    print "MySQLcomm %s"%comm
+#         
+#    self.result = shellCall(0,comm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
+#        
+#    resultTuple = self.result['Value']
+#
+#    status = resultTuple[0]
     self.log.info( "Status after the application execution is %s" % str( status ) )
     failed = False
     if status != 0:
@@ -161,14 +192,23 @@ class SQLWrapper:
     os.chdir(self.softDir)
     DIRAC.gLogger.verbose('clean up db')
     #for now:
-    MySQLcleanUpComm = self.MokkaTMPDir + self.UID_TMP + '/mysql-cleanup.sh'
+    #MySQLcleanUpComm = self.MokkaTMPDir + self.UID_TMP + '/mysql-cleanup.sh'
+    MySQLcleanUpComm = "mysqladmin --no-defaults -hlocalhost --socket=%s/mysql.sock -uroot -p%s shutdown"%(self.MokkaTMPDir,self.rootpass)
             
     self.result = shellCall(0,MySQLcleanUpComm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
-        
+    
     resultTuple = self.result['Value']
 
     status = resultTuple[0]
     self.log.info( "Status after the application execution is %s" % str( status ) )
+    ##kill mysql
+    mysqlkillcomm = "cat %s/mysql.pid | kill"%(self.MokkaTMPDir)
+    self.result = shellCall(0,mysqlkillomm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
+    
+    resultTuple = self.result['Value']
+
+    status = resultTuple[0]
+    self.log.info( "Status after the application execution is %s" % str( status ) )    
     failed = False
     if status != 0:
       self.log.error( "mysql-cleanup execution completed with errors:" )
