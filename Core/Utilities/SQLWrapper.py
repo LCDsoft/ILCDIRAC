c8d13f428231e2f9648e86c8cce0b75c5c264e2c
sposs <sposs@cern.ch>
2010-03-11 02:07:55 +0000

    last try with mysqld_safe, need to find a way to run it really in background
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LCDDIRAC/trunk/LCDDIRAC@22749 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/SQLWrapper.py
+++ Core/Utilities/SQLWrapper.py
@@ -93,7 +93,7 @@ class SQLWrapper:
     mysqld_run = file("mysqld_run.sh","w")
     mysqld_run.write("mysqld_safe %s &"%safe_options)
     mysqld_run.close()
-    mysqldcomm = "mysqld_safe %s "%safe_options
+    mysqldcomm = "mysqld_safe %s &"%safe_options
     #mysqldcomm = "chmod u+x mysqld_run.sh; ./mysqld_run.sh"
     self.result = shellCall(0,mysqldcomm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
     resultTuple = self.result['Value']
