fbe82b95cfe73cc022120c88ce22e118129e500d
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-03-11 01:19:10 +0000

    Don't remove TMPDIR

--- Core/Utilities/SQLWrapper.py
+++ Core/Utilities/SQLWrapper.py
@@ -59,7 +59,7 @@ class SQLWrapper:
       os.environ['LD_LIBRARY_PATH']='%s/mysql4grid/lib64/mysql:%s/mysql4grid/lib64'%(self.softDir,self.softDir)
     os.environ['PATH']='%s/mysql4grid/bin:%s'%(self.softDir,os.environ['PATH'])
     
-    safe_options =  "--no-defaults --skip-networking --socket=%s/mysql.sock --datadir=%s/data  --basedir=%s/mysql4grid --pid-file=%s/mysql.pid"%(self.MokkaTMPDir,self.MokkaTMPDir,self.softDir,self.MokkaTMPDir)
+    safe_options =  "--no-defaults --skip-networking --socket=%s/mysql.sock --datadir=%s/data --basedir=%s/mysql4grid --pid-file=%s/mysql.pid"%(self.MokkaTMPDir,self.MokkaTMPDir,self.softDir,self.MokkaTMPDir)
     #comm = self.softDir+'/mokkadbscripts/mysql-local-db-setup.sh -p ' + self.MokkaTMPDir + ' -d ' + self.MokkaDumpFile
     comm = "mysql_install_db  %s"%(safe_options) 
     print "Executing %s"%comm
@@ -266,7 +266,7 @@ class SQLWrapper:
     if (os.path.exists(self.MokkaTMPDir)):
       try:
         DIRAC.gLogger.verbose('Removing tmp dir')
-        os.rmdir(self.MokkaTMPDir)
+        #os.rmdir(self.MokkaTMPDir)
       except IOError, (errno,strerror):
         DIRAC.gLogger.exception("I/O error({0}): {1}".format(errno, strerror))
         return S_ERROR('Removing tmp dir failed')
