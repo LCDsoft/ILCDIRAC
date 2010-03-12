b32e2cbf8f442f31a7e58dd9626f7457144d1c94
pmajewsk <pmajewsk@cern.ch>
2010-03-12 09:41:53 +0000

    mysqld threading
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LCDDIRAC/trunk/LCDDIRAC@22896 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/SQLWrapper.py
+++ Core/Utilities/SQLWrapper.py
@@ -269,8 +269,8 @@ class SQLWrapper:
     status = resultTuple[0]
     self.log.info( "Status after the application execution is %s" % str( status ) )
     ##kill mysql
-    #mysqlkillcomm = "cat %s/mysql.p id | kill"%(self.MokkaTMPDir)
-    mysqlkillcomm = "kill -9 %s"%(self.mysqldPID)
+    mysqlkillcomm = "cat %s/mysql.pid | kill"%(self.MokkaTMPDir)
+    #mysqlkillcomm = "kill -9 %s"%(self.mysqldPID)
     self.result = shellCall(0,mysqlkillcomm,callbackFunction=self.redirectLogOutput,bufferLimit=20971520)
     
     resultTuple = self.result['Value']
