a52bb1241144ad038470214ad51bd01cb226e6fc
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-03-16 15:17:53 +0000

    Added missing \n in environement variables definitions

--- Workflow/Modules/MarlinAnalysis.py
+++ Workflow/Modules/MarlinAnalysis.py
@@ -152,12 +152,12 @@ class MarlinAnalysis(ModuleBase):
     if (len(marlindll) != 0):
       script.write('declare -x MARLIN_DLL=%s\n'%marlindll)
           
-    script.write('declare -x ROOTSYS=%s/MarlinLibs/ROOT'%(mySoftwareRoot))
+    script.write('declare -x ROOTSYS=%s/MarlinLibs/ROOT\n'%(mySoftwareRoot))
     if os.environ.has_key('LD_LIBRARY_PATH'):
-        script.write('declare -x LD_LIBRARY_PATH=$ROOTSYS/lib:%s/MarlinLibs/LDLibs:%s\n'%(mySoftwareRoot,os.environ['LD_LIBRARY_PATH']))
+      script.write('declare -x LD_LIBRARY_PATH=$ROOTSYS/lib:%s/MarlinLibs/LDLibs:%s\n'%(mySoftwareRoot,os.environ['LD_LIBRARY_PATH']))
     else:
-        script.write('declare -x LD_LIBRARY_PATH=$ROOTSYS/lib:%s/MarlinLibs/LDLibs\n'%(mySoftwareRoot))
-    
+      script.write('declare -x LD_LIBRARY_PATH=$ROOTSYS/lib:%s/MarlinLibs/LDLibs\n'%(mySoftwareRoot))
+    script.write('declare -x PATH=$ROOTSYS/bin:$PATH\n')
     script.write('echo =============================\n')
     script.write('echo LD_LIBRARY_PATH is\n')
     script.write('echo $LD_LIBRARY_PATH | tr ":" "\n"\n')
