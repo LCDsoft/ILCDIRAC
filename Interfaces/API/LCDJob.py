27c5e9e2712821c39a1d3d97773be968b65d01a9
sposs <sposs@cern.ch>
2010-03-17 18:57:28 +0000

    Number of events to process treated the same as input file, read from Mokka parameters in case of chain
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LCDDIRAC/trunk/LCDDIRAC@23236 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/API/LCDJob.py
+++ Interfaces/API/LCDJob.py
@@ -237,7 +237,7 @@ class LCDJob(Job):
     step.addParameter(Parameter("inputXML","","string","","",False,False,"Name of the input XML file"))
     step.addParameter(Parameter("inputGEAR","","string","","",False,False,"Name of the input GEAR file"))
     step.addParameter(Parameter("inputSlcio","","string","","",False,False,"Name of the input slcio file"))
-    step.addParameter(Parameter("EvtsToProcess","","string","","",False,False,"Nunmber of events to proccess"))
+    step.addParameter(Parameter("EvtsToProcess","","int","","",False,False,"Nunmber of events to proccess"))
 
     self.workflow.addStep(step)
     stepInstance = self.workflow.createStepInstance('Marlin',stepName)
@@ -252,8 +252,12 @@ class LCDJob(Job):
     stepInstance.setValue("inputXML",xmlfile)
     stepInstance.setValue("inputGEAR",gearfile)
     if(evtstoprocess):
-      stepInstance.setValue("EvtsToProcess",str(evtstoprocess))
-
+      stepInstance.setValue("EvtsToProcess",evtstoprocess)
+    else:
+      if not self.ioDict.has_key(self.StepCount-1):
+        raise TypeError,'Expected previously defined Mokka step for input number of events to process'
+      stepInstance.setLink('EvtsToProcess',self.ioDict[self.StepCount-1],'numberOfEvents')
+      
     currentApp = "Marlin.%s"%appVersion
 
     swPackages = 'SoftwarePackages'
