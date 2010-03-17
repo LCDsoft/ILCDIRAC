58b79753e9cb68ceccc6d1832184a3fe282fa192
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-03-17 18:57:28 +0000

    Number of events to process treated the same as input file, read from Mokka parameters in case of chain

--- Workflow/Modules/MarlinAnalysis.py
+++ Workflow/Modules/MarlinAnalysis.py
@@ -68,7 +68,7 @@ class MarlinAnalysis(ModuleBase):
       self.jobType = self.workflow_commons['JobType']
       
     if self.workflow_commons.has_key('EvtsToProcess'):
-      self.evtstoprocess = self.workflow_commons['EvtsToProcess']
+      self.evtstoprocess = str(self.workflow_commons['EvtsToProcess'])
       
       
   def execute(self):
