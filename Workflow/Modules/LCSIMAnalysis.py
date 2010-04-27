d3a2541e34500576062cd11fc3b2b0e00bbf337b
sposs <sposs@cern.ch>
2010-04-27 09:41:00 +0000

    add support for alias properties file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@24442 4525493e-7705-40b1-a816-d608a930855b

--- Workflow/Modules/LCSIMAnalysis.py
+++ Workflow/Modules/LCSIMAnalysis.py
@@ -114,7 +114,8 @@ class LCSIMAnalysis(ModuleBase):
         
       aliasproperties = os.path.basename(self.aliasproperties)
       if os.path.exists(aliasproperties):
-        shutil.copy(aliasproperties,os.path.join(homedir,aliasproperties))
+        os.mkdir(os.path.join(homedir,".lcsim"))
+        shutil.copy(aliasproperties,os.path.join(homedir,".lcsim",aliasproperties))
     
     #if tarfile.is_tarfile(self.sourcedir) :
     #  untarred_sourcedir = tarfile.open(self.sourcedir,'r')
