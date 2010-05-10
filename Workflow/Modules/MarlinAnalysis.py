aec100097179849e1925bf48b238547838d2c27c
sposs <sposs@cern.ch>
2010-05-10 10:49:23 +0000

    In Marlin, if evtstoprocess is not specified, use -1 i.e. all events of the file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@24984 4525493e-7705-40b1-a816-d608a930855b

--- Workflow/Modules/MarlinAnalysis.py
+++ Workflow/Modules/MarlinAnalysis.py
@@ -72,8 +72,8 @@ class MarlinAnalysis(ModuleBase):
       self.jobType = self.workflow_commons['JobType']
       
     if self.step_commons.has_key('EvtsToProcess'):
-      if(self.step_commons['EvtsToProcess']>0):
-        self.evtstoprocess = str(self.step_commons['EvtsToProcess'])
+      self.evtstoprocess = str(self.step_commons['EvtsToProcess'])
+      
     if self.step_commons.has_key('debug'):
       self.debug =  self.step_commons['debug']
     return S_OK('Parameters resolved')
