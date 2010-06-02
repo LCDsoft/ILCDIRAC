fb7c3b29f28630fe4ea4dd6c2784bc1c3518f183
sposs <sposs@cern.ch>
2010-06-02 22:24:09 +0000

    Release v1r6p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v1r6p2@25845 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/scripts/dirac-ilc-add-software.py
@@ -0,0 +1,81 @@
+'''
+Add specified software in CS. Allows not using the web interface, therefore reduces the error rate.
+
+Created on May 5, 2010
+
+@author: sposs
+'''
+from DIRAC.FrameworkSystem.Client.NotificationClient     import NotificationClient
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
+from DIRAC import gConfig
+import DIRAC
+from DIRAC.Core.Base import Script
+import string
+
+Script.parseCommandLine( ignoreErrors = True )
+args = Script.getPositionalArgs()
+diracAdmin = DiracAdmin()
+modifiedCS = False
+mailadress = 'stephane.poss@cern.ch'
+
+def usage():
+  print 'Usage: %s <PLATFORM> <NAME> <VERSION>' %(Script.scriptName)
+  DIRAC.exit(2)
+
+if len(args) < 3:
+  usage()
+
+softwareSection = "/Operations/AvailableTarBalls"
+platform = "%s"%args[0]
+appName = args[1]
+appVersion = args[2]
+subject = '%s %s add to DIRAC CS' %(args[1],args[2])
+msg = 'New application %s %s declared into CS' %(args[1],args[2])
+
+av_platforms = gConfig.getSections(softwareSection, [])
+if av_platforms['OK']:
+  if not platform in av_platforms['Value']:
+    print "Platform %s unknown, available are %s."%(platform,string.join(av_platforms['Value'],", "))
+    DIRAC.exit(255)
+else:
+  print "Could not find all platforms available in CS"
+  DIRAC.exit(255)
+
+av_apps = gConfig.getSections("%s/%s"%(softwareSection,platform),[])
+if not av_apps['OK']:
+  print "Could not find all applications available in CS"
+  DIRAC.exit(255)
+
+if appName.lower() in av_apps['Value']:
+  versions = gConfig.getSections("%s/%s/%s"%(softwareSection,platform,appName.lower()),[])
+  if not versions['OK']:
+    print "Could not find all versions available in CS"
+    DIRAC.exit(255)
+  if appVersion in versions['Value']:
+    print 'Application %s %s for %s already in CS, nothing to do'%(appName.lower(),appVersion,platform)
+    DIRAC.exit(0)
+  else:
+    result = diracAdmin.csSetOption("%s/%s/%s/%s/TarBall"%(softwareSection,platform,appName.lower(),appVersion),"%s%s.tgz"%(appName,appVersion))
+    modifiedCS = True
+else:
+  result = diracAdmin.csSetOption("%s/%s/%s/%s/TarBall"%(softwareSection,platform,appName.lower(),appVersion),"%s%s.tgz"%(appName,appVersion))
+  modifiedCS = True
+
+#Commit the changes if nothing has failed and the CS has been modified
+if modifiedCS:
+  result = diracAdmin.csCommitChanges(False)
+  print result
+  if not result[ 'OK' ]:
+    print 'ERROR: Commit failed with message = %s' %(result[ 'Message' ])
+    DIRAC.exit(255)
+  else:
+    print 'Successfully committed changes to CS'
+    notifyClient = NotificationClient()
+    print 'Sending mail for software installation %s' %(mailadress)
+    res = notifyClient.sendMail(mailadress,subject,msg,'stephane.poss@cern.ch',localAttempt=False)
+    if not res[ 'OK' ]:
+        print 'The mail could not be sent'
+else:
+  print 'No modifications to CS required'
+
+DIRAC.exit(0)
