dc904eae8d3db98fdc80ad05b9cce491400269a9
sposs <sposs@cern.ch>
2010-07-22 12:42:52 +0000

    Release v1r7p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v1r7p0@26930 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Core/Utilities/CombinedSoftwareInstallation.py
@@ -0,0 +1,209 @@
+# $HeadURL$
+# $Id$
+"""
+Based on LHCbDIRAC.Core.Utilities.CombinedSoftwareInstalation module, 
+
+New version of CombinedSoftwareInstallation, installs properly ILD soft and SiD soft, and all dependencies
+
+@since: Feb 4, 2010
+
+@author: Stephane Poss and Przemyslaw Majewski
+"""
+import os, string
+#from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+import DIRAC 
+from ILCDIRAC.Core.Utilities.TARsoft import TARinstall
+#from ILCDIRAC.Core.Utilities.JAVAsoft import JAVAinstall 
+from ILCDIRAC.Core.Utilities.DetectOS import NativeMachine
+natOS = NativeMachine()
+
+class CombinedSoftwareInstallation:
+  def __init__(self,argumentsDict):
+    """ Standard constructor
+    
+    Defines, from dictionary of job parameters passed, a set of members to hold e.g. the applications and the system config
+    
+    Also determines the SharedArea and LocalArea.
+    """
+    self.job = {}
+    if argumentsDict.has_key('Job'):
+      self.job = argumentsDict['Job']
+    self.ce = {}
+    if argumentsDict.has_key('CE'):
+      self.ce = argumentsDict['CE']
+    self.source = {}
+    if argumentsDict.has_key('Source'):
+      self.source = argumentsDict['Source']
+
+    apps = []
+    if self.job.has_key('SoftwarePackages'):
+      if type( self.job['SoftwarePackages'] ) == type(''):
+        apps = [self.job['SoftwarePackages']]
+      elif type( self.job['SoftwarePackages'] ) == type([]):
+        apps = self.job['SoftwarePackages']
+
+    self.apps = []
+    for app in apps:
+      DIRAC.gLogger.verbose( 'Requested Package %s' % app )
+      app = tuple(app.split('.'))
+      if len(app)>2:
+        tempapp = app
+        app=[]
+        app.append(tempapp[0])
+        app.append(string.join(tempapp[1:], "."))
+      self.apps.append(app)
+
+    self.jobConfig = ''
+    if self.job.has_key( 'SystemConfig' ):
+      self.jobConfig = self.job['SystemConfig']
+    else:
+      self.jobConfig = natOS.CMTSupportedConfig()[0]
+      
+    self.ceConfigs = []
+    if self.ce.has_key('CompatiblePlatforms'):
+      self.ceConfigs = self.ce['CompatiblePlatforms']
+      if type(self.ceConfigs) == type(''):
+        self.ceConfigs = [self.ceConfigs]
+    #else:
+    ### Use always the list of compatible platform.
+    self.ceConfigs = natOS.CMTSupportedConfig()
+
+    self.sharedArea = SharedArea()
+    self.localArea  = LocalArea()
+    
+  def execute(self):
+    """ Main method of the class executed by DIRAC jobAgent
+
+    Executes the following:
+      - look for the compatible platforms in the CS, see if one matches request
+      - install the applications, calls L{TARsoft}
+
+    @return: S_OK(), S_ERROR()
+    """
+    if not self.apps:
+      # There is nothing to do
+      return DIRAC.S_OK()
+    if not self.jobConfig:
+      DIRAC.gLogger.error( 'No architecture requested' )
+      return DIRAC.S_ERROR( 'No architecture requested' )
+
+    found_config = False
+        
+    DIRAC.gLogger.info("Found CE Configs %s, compatible with system reqs %s"%(string.join(self.ceConfigs,","),self.jobConfig))
+    res = DIRAC.gConfig.getSections('/Operations/AvailableTarBalls')
+    if not res['OK']:
+      return res
+    else:
+      supported_systems = res['Value']
+      ###look in CS if specified platform has software available. Normally consistency check is done at submission time
+      for ceConfig in self.ceConfigs:
+        for supp_systems in supported_systems:
+          if ceConfig == supp_systems:
+            self.jobConfig = ceConfig
+            found_config=True
+            break
+          
+    if not found_config:
+      if self.ceConfigs:  # redundant check as this is done in the job agent, if locally running option might not be defined
+        DIRAC.gLogger.error( 'Requested architecture not supported by CE' )
+        return DIRAC.S_ERROR( 'Requested architecture not supported by CE' )
+      else:
+        DIRAC.gLogger.info( 'Assume locally running job, will install software in /LocalSite/LocalArea=%s' %(self.localArea))
+    for app in self.apps:
+      DIRAC.gLogger.info('Attempting to install %s_%s for %s' %(app[0],app[1],self.jobConfig))
+      res = TARinstall(app,self.jobConfig,self.localArea)
+      if not res['OK']:
+        DIRAC.gLogger.error('Failed to install software','%s_%s' %(app))
+        return DIRAC.S_ERROR('Failed to install software')
+      else:
+        DIRAC.gLogger.info('%s was successfully installed for %s' %(app,self.jobConfig))
+    return DIRAC.S_OK()
+  
+def log( n, line ):
+  DIRAC.gLogger.info( line )
+    
+def SharedArea():
+  """
+   Discover location of Shared SW area
+   This area is populated by a tool independent of the DIRAC jobs
+   Not used yet in ILC DIRAC, but should be
+  """
+  sharedArea = ''
+  if os.environ.has_key('VO_ILC_SW_DIR'):
+    sharedArea = os.environ['VO_ILC_SW_DIR']
+    DIRAC.gLogger.debug( 'Using VO_ILC_SW_DIR at "%s"' % sharedArea )
+    if os.environ['VO_ILC_SW_DIR'] == '.':
+      if not os.path.isdir( 'lcd' ):
+        os.mkdir( 'lcd' )
+  elif DIRAC.gConfig.getValue('/LocalSite/SharedArea',''):
+    sharedArea = DIRAC.gConfig.getValue('/LocalSite/SharedArea')
+    DIRAC.gLogger.debug( 'Using CE SharedArea at "%s"' % sharedArea )
+
+  if sharedArea:
+    # if defined, check that it really exists
+    if not os.path.isdir( sharedArea ):
+      DIRAC.gLogger.error( 'Missing Shared Area Directory:', sharedArea )
+      sharedArea = ''
+  return sharedArea
+
+def CreateSharedArea():
+  """
+   Method to be used by SAM jobs to make sure the proper directory structure is created
+   if it does not exists
+  """
+  if not os.environ.has_key('VO_ILC_SW_DIR'):
+    DIRAC.gLogger.info( 'VO_ILC_SW_DIR not defined.' )
+    return False
+
+  sharedArea = os.environ['VO_ILC_SW_DIR']
+  if sharedArea == '.':
+    DIRAC.gLogger.info( 'VO_ILC_SW_DIR points to "."' )
+    return False
+
+  if not os.path.isdir( sharedArea ):
+    DIRAC.gLogger.error( 'VO_ILC_SW_DIR="%s" is not a directory' % sharedArea )
+    return False
+
+  #sharedArea = os.path.join( sharedArea, 'lcd' )
+  try:
+    if os.path.isdir( sharedArea ) and not os.path.islink( sharedArea ) :
+      return True
+    if not os.path.exists( sharedArea ):
+      os.mkdir( sharedArea )
+      return True
+    os.remove( sharedArea )
+    os.mkdir( sharedArea )
+    return True
+  except Exception,x:
+    DIRAC.gLogger.error('Problem trying to create shared area',str(x))
+    return False
+
+def LocalArea():
+  """
+   Discover Location of Local SW Area.
+   This area is populated by DIRAC job Agent for jobs needing SW not present
+   in the Shared Area.
+   Currently is always the location used as the software is not in the shared area.
+  """
+  if DIRAC.gConfig.getValue('/LocalSite/LocalArea',''):
+    localArea = DIRAC.gConfig.getValue('/LocalSite/LocalArea')
+  else:
+    localArea = os.path.join( DIRAC.rootPath, 'LocalArea' )
+
+  # check if already existing directory
+  if not os.path.isdir( localArea ):
+    # check if we can create it
+    if os.path.exists( localArea ):
+      try:
+        os.remove( localArea )
+      except Exception, x:
+        DIRAC.gLogger.error( 'Cannot remove:', localArea )
+        localArea = ''
+    else:
+      try:
+        os.mkdir( localArea )
+      except Exception, x:
+        DIRAC.gLogger.error( 'Cannot create:', localArea )
+        localArea = ''
+  return localArea
+      
---
+++ Core/Core/scripts/dirac-ilc-add-software.py
@@ -0,0 +1,117 @@
+'''
+Add specified software in CS. Allows not using the web interface, therefore reduces the error rate.
+
+Created on May 5, 2010
+
+@author: sposs
+'''
+from DIRAC.FrameworkSystem.Client.NotificationClient     import NotificationClient
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
+from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+from DIRAC import gConfig,S_OK,S_ERROR
+import DIRAC
+from DIRAC.Core.Base import Script
+import string,os,shutil
+
+Script.parseCommandLine( ignoreErrors = True )
+args = Script.getPositionalArgs()
+diracAdmin = DiracAdmin()
+rm = ReplicaManager()
+modifiedCS = False
+mailadress = 'ilc-dirac@cern.ch'
+
+def usage():
+  print 'Usage: %s <PLATFORM> <NAME> <VERSION>' %(Script.scriptName)
+  DIRAC.exit(2)
+
+def upload(path,tarball):
+  if not os.path.exists(appTar):
+    print "Tar ball %s does not exists, cannot continue."%appTar
+    return S_ERROR()
+  if path.find("http://www.cern.ch/lcd-data")>-1:
+    final_path = "/afs/cern.ch/eng/clic/data/software/"
+    try:
+      shutil.copy(appTar,"%s%s"%(final_path,appTar))
+    except Exception,x:
+      print "Could not copy because %s"%x
+      return S_ERROR()
+  elif path.find("http://")>-1:
+    print "path %s was not forseen, location not known, upload to location yourself, and publish in CS manually"%path
+    return S_ERROR()
+  else:
+    res = rm.putAndRegister("%s%s"%(path,appTar),appTar,"CERN-SRM")
+    return res
+  return S_OK()
+
+if len(args) < 3:
+  usage()
+
+softwareSection = "/Operations/AvailableTarBalls"
+platform = "%s"%args[0]
+appName = "%s"%args[1]
+appVersion = "%s"%args[2]
+appTar = "%s%s.tgz"%(appName,appVersion)
+subject = '%s %s add to DIRAC CS' %(args[1],args[2])
+msg = 'New application %s %s declared into Configuration service' %(args[1],args[2])
+
+av_platforms = gConfig.getSections(softwareSection, [])
+if av_platforms['OK']:
+  if not platform in av_platforms['Value']:
+    print "Platform %s unknown, available are %s."%(platform,string.join(av_platforms['Value'],", "))
+    print "If yours is missing add it in CS"
+    DIRAC.exit(255)
+else:
+  print "Could not find all platforms available in CS"
+  DIRAC.exit(255)
+
+av_apps = gConfig.getSections("%s/%s"%(softwareSection,platform),[])
+if not av_apps['OK']:
+  print "Could not find all applications available in CS"
+  DIRAC.exit(255)
+
+if appName.lower() in av_apps['Value']:
+  versions = gConfig.getSections("%s/%s/%s"%(softwareSection,platform,appName.lower()),[])
+  if not versions['OK']:
+    print "Could not find all versions available in CS"
+    DIRAC.exit(255)
+  if appVersion in versions['Value']:
+    print 'Application %s %s for %s already in CS, nothing to do'%(appName.lower(),appVersion,platform)
+    DIRAC.exit(0)
+  else:
+    result = diracAdmin.csSetOption("%s/%s/%s/%s/TarBall"%(softwareSection,platform,appName.lower(),appVersion),appTar)
+    if result['OK']:
+      modifiedCS = True
+      tarballurl = gConfig.getOption("%s/%s/%s/TarBallURL"%(softwareSection,platform,appName.lower()),"")
+      if len(tarballurl['Value'])>0:
+        res = upload(tarballurl['Value'],appTar)
+        if not res['OK']:
+          print "Upload to %s failed"%tarballurl
+          DIRAC.exit(255)
+else:
+  result = diracAdmin.csSetOption("%s/%s/%s/%s/TarBall"%(softwareSection,platform,appName.lower(),appVersion),appTar)
+  if result['OK']:  
+    modifiedCS = True
+    tarballurl = gConfig.getOption("%s/%s/%s/TarBallURL"%(softwareSection,platform,appName.lower()),"")
+    if len(tarballurl['Value'])>0:
+      res = upload(tarballurl['Value'],appTar)
+      if not res['OK']:
+        print "Upload to %s failed"%tarballurl
+        DIRAC.exit(255)
+#Commit the changes if nothing has failed and the CS has been modified
+if modifiedCS:
+  result = diracAdmin.csCommitChanges(False)
+  print result
+  if not result[ 'OK' ]:
+    print 'ERROR: Commit failed with message = %s' %(result[ 'Message' ])
+    DIRAC.exit(255)
+  else:
+    print 'Successfully committed changes to CS'
+    notifyClient = NotificationClient()
+    print 'Sending mail for software installation %s' %(mailadress)
+    res = notifyClient.sendMail(mailadress,subject,msg,'stephane.poss@cern.ch',localAttempt=False)
+    if not res[ 'OK' ]:
+        print 'The mail could not be sent'
+else:
+  print 'No modifications to CS required'
+
+DIRAC.exit(0)
