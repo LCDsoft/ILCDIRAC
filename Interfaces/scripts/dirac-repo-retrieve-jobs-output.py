b37a3e14f3f59c0e082cbd35cd7cb42b2b93e320
sposs <sposs@cern.ch>
2010-07-29 14:00:14 +0000

    add support for not retrieving outputdata
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@27283 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-repo-retrieve-jobs-output.py
+++ Interfaces/scripts/dirac-repo-retrieve-jobs-output.py
@@ -8,21 +8,33 @@ from DIRAC.Core.Base import Script
 from DIRAC.Interfaces.API.Dirac import Dirac
 import os,sys
 
+Script.registerSwitch( "", "outputdata=","   retrieve also output data")
+
 Script.parseCommandLine( ignoreErrors = False )
-args = sys.argv
+
+outputdata = False
+repoLocation = None
 
 def usage():
-  print 'Usage: %s repo' % (Script.scriptName)
+  print 'Usage: %s repo.cfg --outputdata=True' % (Script.scriptName)
   DIRAC.exit(2)
-if len(args) < 2:
+  
+for switch in Script.getUnprocessedSwitches():
+  if switch[0].lower() == "outputdata":
+    outputdata=True
+
+args = Script.getPositionalArgs()
+if len(args)<2:
   usage()
 
 repoLocation = args[1]
+
 dirac=Dirac(True, repoLocation)
 
 exitCode = 0
 dirac.monitorRepository(False)
 dirac.retrieveRepositorySandboxes()
-dirac.retrieveRepositoryData()
+if outputdata:
+  dirac.retrieveRepositoryData()
 
 DIRAC.exit(exitCode)
