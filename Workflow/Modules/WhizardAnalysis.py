4456ccd86d7b6e84c7935df2836ca59b79c55fdb
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-09-30 12:21:26 +0000

    Force random seed to be job id. Test only

--- Workflow/Modules/WhizardAnalysis.py
+++ Workflow/Modules/WhizardAnalysis.py
@@ -67,7 +67,7 @@ class WhizardAnalysis(ModuleBase):
       self.randomseed = self.step_commons['RandomSeed']
     elif self.workflow_commons.has_key('JOB_ID'):
       self.randomseed = int(self.workflow_commons["JOB_ID"])
-    elif self.jobID:
+    if self.jobID:
       self.randomseed = self.jobID
 
     if self.step_commons.has_key('NbOfEvts'):
