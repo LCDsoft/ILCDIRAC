8ad87c1dc94802331f5218065326f769f2c944b4
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-10-05 18:38:08 +0000

    added gen file in list of files to upload

--- Workflow/Modules/UploadOutputData.py
+++ Workflow/Modules/UploadOutputData.py
@@ -87,7 +87,7 @@ class UploadOutputData(ModuleBase):
         olist = []
         for obj in self.outputList:
           for prodfile in proddata:
-            if (obj['outputFile'].lower().count("_sim") and prodfile.lower().count("_sim_")) or (obj['outputFile'].lower().count("_rec") and prodfile.lower().count("_rec_")) or (obj['outputFile'].lower().count("_dst") and prodfile.lower().count("_dst_")):
+            if (obj['outputFile'].lower().count("_gen") and prodfile.lower().count("_gen_")) or (obj['outputFile'].lower().count("_sim") and prodfile.lower().count("_sim_")) or (obj['outputFile'].lower().count("_rec") and prodfile.lower().count("_rec_")) or (obj['outputFile'].lower().count("_dst") and prodfile.lower().count("_dst_")):
               appdict = obj
               appdict['outputFile'] = os.path.basename(prodfile)
               olist.append(appdict)
