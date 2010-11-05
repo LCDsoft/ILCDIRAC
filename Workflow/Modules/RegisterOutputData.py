af67b59592aff4de2344934994da80b59dd37d68
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-11-05 08:01:40 +0000

    nbevts and lumi were not used...

--- Workflow/Modules/RegisterOutputData.py
+++ Workflow/Modules/RegisterOutputData.py
@@ -37,12 +37,12 @@ class RegisterOutputData(ModuleBase):
       
     if self.workflow_commons.has_key('SoftwarePackages'):
       self.swpackages = self.workflow_commons['SoftwarePackages'].split(";")
-    return S_OK('Parameters resolved')
 
     if self.workflow_commons.has_key('NbOfEvents'):
       self.nbofevents = self.workflow_commons['NbOfEvents']
     if self.workflow_commons.has_key('Luminosity'):
       self.luminosity = self.workflow_commons['Luminosity']
+    return S_OK('Parameters resolved')
   
   def execute(self):
     self.log.info('Initializing %s' %self.version)
