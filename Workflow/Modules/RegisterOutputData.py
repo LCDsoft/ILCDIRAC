28046b41f145185a74429da99ae1a8b9554c4929
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2010-11-26 10:01:31 +0000

    Registration in catalog was failing because of change in FC logic

--- Workflow/Modules/RegisterOutputData.py
+++ Workflow/Modules/RegisterOutputData.py
@@ -63,78 +63,91 @@ class RegisterOutputData(ModuleBase):
 
     for files in self.prodOutputLFNs:
       elements = files.split("/")
-      meta = {}
-      meta['Machine']=elements[3]
+      metaprodid = {}
+      meta = {}  
+      metamach = {}
+      metamach['Machine']=elements[3]
+      meta.update(metamach)
       machine = string.join(elements[0:4],"/")
-      res = self.fc.setMetadata(machine,"Machine",elements[3])
+      res = self.fc.setMetadata(machine,metamach)
       if not res['OK']:
         self.log.error('Could not register metadata Machine, with value %s for %s'%(elements[3],machine))
         return res
-      meta['Energy']=elements[4]
+      metaen={}
+      metaen['Energy']=elements[4]
+      meta.update(metaen)
       energy = string.join(elements[0:5],"/")
-      res = self.fc.setMetadata(energy,"Energy",elements[4])
+      res = self.fc.setMetadata(energy,metaen)
       if not res['OK']:
         self.log.error('Could not register metadata Energy, with value %s for %s'%(elements[4],energy))
         return res      
-      meta['EvtType']=elements[5]
+      metaevt={}
+      metaevt['EvtType']=elements[5]
+      meta.update(metaevt)
       evttype = string.join(elements[0:6],"/")
-      res = self.fc.setMetadata(evttype,"EvtType",elements[5])
+      res = self.fc.setMetadata(evttype,metaevt)
       if not res['OK']:
         self.log.error('Could not register metadata EvtType, with value %s for %s'%(elements[5],evttype))
         return res
       prodid = ''
       if elements[6].lower() == 'gen':
-        meta['Datatype']=elements[6]
+        metadat={}
+        metadat['Datatype']=elements[6]
+        meta.update(metadat)
         datatype = string.join(elements[0:7],"/")
-        res = self.fc.setMetadata(datatype,"Datatype",elements[6])
+        res = self.fc.setMetadata(datatype,metadat)
         if not res['OK']:
           self.log.error('Could not register metadata Datatype, with value %s for %s'%(elements[6],datatype))
           return res
-        meta['ProdID'] = elements[7]
+        metaprodid['ProdID'] = elements[7]
+        meta.update(metaprodid)
         prodid = string.join(elements[0:8],"/")
-        res = self.fc.setMetadata(prodid,"ProdID",elements[7])
+        res = self.fc.setMetadata(prodid,metaprodid)
         if not res['OK']:
           self.log.error('Could not register metadata ProdID, with value %s for %s'%(elements[7],prodid))
           return res
         
       else:
-        meta['DetectorType']=elements[6]
+        metadet={}
+        metadet['DetectorType']=elements[6]
         detectortype=string.join(elements[0:7],"/")
-        res = self.fc.setMetadata(detectortype,"DetectorType",elements[6])
+        res = self.fc.setMetadata(detectortype,metadet)
         if not res['OK']:
           self.log.error('Could not register metadata DetectorType, with value %s for %s'%(elements[6],detectortype))
           return res
-        meta['Datatype']=elements[7]
+        metadat={}
+        metadat['Datatype']=elements[7]
         datatype = string.join(elements[0:8],"/")
-        res = self.fc.setMetadata(datatype,"Datatype",elements[7])
+        res = self.fc.setMetadata(datatype,metadat)
         if not res['OK']:
           self.log.error('Could not register metadata Datatype, with value %s for %s'%(elements[7],datatype))
           return res 
-        meta['ProdID'] = elements[8]
+        metaprodid['ProdID'] = elements[8]
         prodid = string.join(elements[0:9],"/")
-        res = self.fc.setMetadata(prodid,"ProdID",elements[8])
+        res = self.fc.setMetadata(prodid,metaprodid)
         if not res['OK']:
           self.log.error('Could not register metadata ProdID, with value %s for %s'%(elements[8],prodid))
           return res
+        
       if self.nbofevents:
-        meta['NumberOfEvents']=self.nbofevents
-        res = self.fc.setMetadata(prodid,'NumberOfEvents',self.nbofevents)
+        metaprodid['NumberOfEvents']=self.nbofevents
+        res = self.fc.setMetadata(prodid,metaprodid)
         if not res['OK']:
           self.log.error('Could not register metadata NumberOfEvents, with value %s for %s'%(self.nbofevents,prodid))
           return res
       if self.luminosity:
-        meta['Luminosity']=self.luminosity
-        res = self.fc.setMetadata(prodid,'Luminosity',self.luminosity)
+        metaprodid['Luminosity']=self.luminosity
+        res = self.fc.setMetadata(prodid,metaprodid)
         if not res['OK']:
           self.log.error('Could not register metadata Luminosity, with value %s for %s'%(self.luminosity,prodid))
           return res
+      meta.update(metaprodid)
       self.log.info("Registered %s with tags %s"%(files,meta))
       # FIXME: in next DIRAC release, remove loop and replace key,value below by meta  
-      #for key,value in meta.items():
-      #  res = self.fc.setMetadata(os.path.dirname(files),key,value)
-      #  if not res['OK']:
-      #    self.log.error('Could not register metadata %s, with value %s for %s'%(key, value, files))
-      #    return res
+      #res = self.fc.setMetadata(os.path.dirname(files),meta)
+      #if not res['OK']:
+      #  self.log.error('Could not register metadata %s for %s'%(meta, files))
+      #  return res
     
     return S_OK('Output data metadata registered in catalog')
   
