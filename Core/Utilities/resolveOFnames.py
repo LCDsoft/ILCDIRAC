c83e01fba64b33add342ef8fe90ee0ae26b24d1a
sposs <sposs@cern.ch>
2010-12-01 12:51:00 +0000

    Release v1r10p3
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v1r10p3@31067 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/resolveOFnames.py
@@ -0,0 +1,15 @@
+'''
+Created on Jul 30, 2010
+
+@author: sposs
+'''
+
+def getProdFilename(filename,prodID,jobID):
+  outfile = ""
+  if filename.count(".slcio"):
+    name = filename.split(".slcio")
+    outfile = name[0]+"_"+str(prodID)+"_"+str(jobID)+".slcio"
+  elif filename.count(".stdhep"):
+    name = filename.split(".stdhep")
+    outfile = name[0]+"_"+str(prodID)+"_"+str(jobID)+".stdhep"
+  return outfile
