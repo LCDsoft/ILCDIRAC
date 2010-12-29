f936d3fde02468656088a4e2d98a1e629d2cc8c3
sposs <sposs@cern.ch>
2010-12-29 12:35:16 +0000

    Number of event is overwritten if set as 0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@32377 4525493e-7705-40b1-a816-d608a930855b

--- Workflow/Modules/MokkaAnalysis.py
+++ Workflow/Modules/MokkaAnalysis.py
@@ -131,7 +131,8 @@ class MokkaAnalysis(ModuleBase):
           res = getNumberOfevents(self.InputData)
           if res.has_key("nbevts") and not self.workflow_commons.has_key("Luminosity") :
             self.workflow_commons["NbOfEvents"]=res["nbevts"]
-            self.numberOfEvents=res["nbevts"]
+            if not self.numberOfEvents:
+              self.numberOfEvents=res["nbevts"]
           if res.has_key("lumi") and not self.workflow_commons.has_key("NbOfEvents"):
             self.workflow_commons["Luminosity"]=res["lumi"]
 
