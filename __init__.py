837ea402859c8265b55ad50391a708cde84bd04c
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-01-21 16:28:02 +0000

    tag for v1r12p1

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __path__ = extend_path(__path__, __name__)
 
 majorVersion = 1
 minorVersion = 12
-patchLevel   = 0
+patchLevel   = 1
 preVersion   = 0
 
 version      = "v%sr%s" % ( majorVersion, minorVersion )
