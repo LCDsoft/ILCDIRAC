08a4ff0072dc733d2f5cadf2b4d78312d088039d
sposs <sposs@cern.ch>
2011-01-27 10:30:02 +0000

    declare v1r13p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@33121 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ __path__ = extend_path(__path__, __name__)
 # Define Version
 
 majorVersion = 1
-minorVersion = 12
-patchLevel   = 1
+minorVersion = 13
+patchLevel   = 0
 preVersion   = 0
 
 version      = "v%sr%s" % ( majorVersion, minorVersion )
