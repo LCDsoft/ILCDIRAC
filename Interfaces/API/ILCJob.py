44230149dd706aaf0a28e4de732adc249a5d57f7
sposs <sposs@cern.ch>
2011-01-28 08:40:46 +0000

    Add number of signal events per job
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@33167 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/API/ILCJob.py
+++ Interfaces/API/ILCJob.py
@@ -1335,7 +1335,7 @@ class ILCJob(Job):
     self.ioDict["SLICPandoraStep"]=stepInstance.getName()        
     return S_OK()
   
-  def addOverlay(self,detector='',energy='',BXOverlay=0,NbGGtoHadInts=0,ProdID=0):
+  def addOverlay(self,detector='',energy='',BXOverlay=0,NbGGtoHadInts=0,ProdID=0, NSigEvetnsPerJob = 0):
     """ Helper call to define Overlay processor/driver inputs
     
     @param detector: Detector type to use (ILD or SID). Obtained in prod context via the metadata lookup
@@ -1349,7 +1349,8 @@ class ILCJob(Job):
     @param ProdID: Optional parameter to force using one specific prodID for the input files. By default it's the latest one
     @type ProdID: int
     """
-    kwargs = {"detector":detector,"energy":energy,'BXOverlay':BXOverlay,"NbGGtoHadInts":NbGGtoHadInts,'ProdID':ProdID}
+    kwargs = {"detector":detector,"energy":energy,'BXOverlay':BXOverlay,"NbGGtoHadInts":NbGGtoHadInts,'ProdID':ProdID,
+              'NSigEvetnsPerJob':NSigEvetnsPerJob}
     if detector:
       self.detector=detector
     if not self.detector:
@@ -1384,6 +1385,9 @@ class ILCJob(Job):
       step.addParameter(Parameter("ggtohadint",0,"float","","",False,False,"Number of BX to overlay"))
     if ProdID:
       step.addParameter(Parameter("ProdID",0,"int","","",False,False,"ProdID for input"))
+      
+    if NSigEvetnsPerJob:
+      step.addParameter(Parameter("NbSigEvtsPerJob",0,"int","","",False,False,"Number of signal events per job"))
     self.workflow.addStep(step)
     stepInstance = self.workflow.createStepInstance(stepDefn,stepName)
     stepInstance.setValue("Detector",self.detector)
@@ -1393,6 +1397,9 @@ class ILCJob(Job):
       stepInstance.setValue("ggtohadint",NbGGtoHadInts)
     if ProdID:
       stepInstance.setValue("ProdID",ProdID)
+    if NSigEvetnsPerJob:
+      stepInstance.setValue("NbSigEvtsPerJob",NSigEvetnsPerJob)
+      
     return S_OK()
   
   def setRootAppli(self,appVersion, scriptpath,args=None,logFile='',logInOutputData=False):
