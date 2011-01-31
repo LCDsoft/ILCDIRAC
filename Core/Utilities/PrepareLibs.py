cd9508d1622daac661a526208395cd109f37baab
sposs <sposs@cern.ch>
2011-01-31 08:37:25 +0000

    Release v1r12p3
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v1r12p3@33251 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/PrepareLibs.py
@@ -0,0 +1,23 @@
+'''
+Created on Jan 26, 2011
+
+@author: sposs
+'''
+import os
+
+def removeLibc(path):
+  curdir = os.getcwd()
+  os.chdir(path)
+  listlibs = os.listdir(os.getcwd())
+  for lib in listlibs:
+    if lib.count("libc.so") or lib.count("libc-2.5"):
+      try:
+        os.remove(os.getcwd()+os.sep+lib)
+      except:
+        print "Could not remove libc"
+        os.chdir(curdir)
+        return False
+  os.chdir(curdir)
+  return True
+
+  
---
+++ Core/Utilities/resolveIFpaths.py
@@ -0,0 +1,32 @@
+'''
+Created on Jun 28, 2010
+
+@author: sposs
+'''
+import os
+from DIRAC import S_OK,S_ERROR
+def resolveIFpaths(inputfiles):
+  listoffiles = []
+  for file in inputfiles:
+    listoffiles.append(os.path.basename(file))
+  listofpaths = []
+  listofdirs = []
+  for dir in os.listdir(os.getcwd()):
+    if os.path.isdir(dir):
+      listofdirs.append(dir)
+  filefound = False
+  for f in listoffiles:
+    filefound = False
+    if os.path.exists(f):
+      listofpaths.append(os.getcwd()+os.sep+f)
+      filefound=True
+    else:
+      for dir in listofdirs:
+        if os.path.exists(os.getcwd()+os.sep+dir+os.sep+f):
+          listofpaths.append(os.getcwd()+os.sep+dir+os.sep+f)
+          listofdirs.remove(dir)
+          filefound = True
+          break
+  if not filefound:
+    return S_ERROR("File not found")
+  return S_OK(listofpaths)
