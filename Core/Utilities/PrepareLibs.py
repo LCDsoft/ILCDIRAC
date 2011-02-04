5c5abff127f010fc8cc27720eee9303df5673b96
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-02-04 07:47:43 +0000

    remove also libdl, libm, libpthread

--- Core/Utilities/PrepareLibs.py
+++ Core/Utilities/PrepareLibs.py
@@ -6,17 +6,18 @@ Created on Jan 26, 2011
 import os
 
 def removeLibc(path):
-  return True
+  #return True
 
   curdir = os.getcwd()
   os.chdir(path)
   listlibs = os.listdir(os.getcwd())
   for lib in listlibs:
-    if lib.count("libc.so") or lib.count("libc-2.5"):
+    if (lib.count("libc.so") or lib.count("libc-2.5") or lib.count("libm.so") 
+        or lib.count("libpthread.so") or lib.count("libdl.so")):
       try:
         os.remove(os.getcwd()+os.sep+lib)
       except:
-        print "Could not remove libc"
+        print "Could not remove %s"%lib
         os.chdir(curdir)
         return False
   os.chdir(curdir)
