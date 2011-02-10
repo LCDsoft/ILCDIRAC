6a73dfa4e7616d93e81bf3b0069da83ee728be02
sposs <sposs@cern.ch>
2011-02-10 12:54:23 +0000

    Catch an error in proxy-init (return status !=0)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@33689 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/CheckAndGetProdProxy.py
+++ Core/Utilities/CheckAndGetProdProxy.py
@@ -9,19 +9,23 @@ from DIRAC.Core.Security.Misc import getProxyInfo
 
 def getNewProxy(): 
   print 'Getting production proxy ...'
-  call( [ 'proxy-init' , '-g', 'ilc_prod' ] )
-
+  return call( [ 'proxy-init' , '-g', 'ilc_prod' ] )
 def CheckAndGetProdProxy():
   result = getProxyInfo()
   if not result['OK']:
     print 'Could not obtain proxy information: %s'%result['Message']
-    getNewProxy()
+    if getNewProxy():
+      return S_ERROR("proxy-init failed")
   elif not result['Value'].has_key('group'):
     print 'Could not get group from proxy'
     getNewProxy()
+    if getNewProxy():
+      return S_ERROR("proxy-init failed")
   group = result['Value']['group']
 
   if not group=='ilc_prod':
     print 'You do not have a valid group'
     getNewProxy()  
+    if getNewProxy():
+      return S_ERROR("proxy-init failed")
   return S_OK()
