7bbe39e7bf3475d51afc9557c95495e22771ea88
sposs <sposs@cern.ch>
2011-02-10 11:14:53 +0000

    this code can also retrieve the outputdata now
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@33685 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-repo-retrieve-jobs-output.py
+++ Interfaces/scripts/dirac-repo-retrieve-jobs-output.py
@@ -3,29 +3,45 @@ Created on Mar 24, 2010
 
 @author: sposs
 '''
-import DIRAC
 from DIRAC.Core.Base import Script
-from DIRAC.Interfaces.API.Dirac import Dirac
-import os,sys
-
-Script.parseCommandLine( ignoreErrors = False )
 
 outputdata = False
 repoLocation = None
 
-def usage():
-  print 'Usage: %s repo.cfg' % (Script.scriptName)
-  DIRAC.exit(2)
-  
+Script.registerSwitch( 'a:','outputdata','retrieve also the outputdata')
+Script.registerSwitch( 'r:','repository=','repository file to use')
+Script.setUsageMessage( sys.argv[0]+'-r <repo location> (-a)')
 
-repoLocation = sys.argv[1]
+Script.parseCommandLine( ignoreErrors = False )
+switches = Script.getUnprocessedSwitches()
+for switch in switches:
+  opt = switch[0]
+  arg = switch[1]
+  if opt in ('r','repository'):
+    repoLocation=arg
+  else:
+    Script.showHelp()
+  if opt in ('a','outputdata'):
+    outputdata=True  
+    
+import DIRAC
+from DIRAC.Interfaces.API.Dirac import Dirac
 
 dirac=Dirac(True, repoLocation)
 
 exitCode = 0
-dirac.monitorRepository(False)
-dirac.retrieveRepositorySandboxes()
+res = dirac.monitorRepository(False)
+if not res['OK']:
+  print "Failed because %s"%res['Message']
+  DIRAC.exit(1)
+  
+res = dirac.retrieveRepositorySandboxes()
+if not res['OK']:
+  print "Failed because %s"%res['Message']
+  DIRAC.exit(1)
 if outputdata:
-  dirac.retrieveRepositoryData()
-
+  res= dirac.retrieveRepositoryData()
+  if not res['OK']:
+    print "Failed because %s"%res['Message']
+    DIRAC.exit(1)
 DIRAC.exit(exitCode)
