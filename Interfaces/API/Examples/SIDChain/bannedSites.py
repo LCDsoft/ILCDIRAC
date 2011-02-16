7c9f99087e9d54c241f4477aba7fc1310842f3b9
sposs <sposs@cern.ch>
2011-02-16 20:08:18 +0000

    Release v1r15p1
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v1r15p1@34151 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/API/Examples/SIDChain/bannedSites.py
@@ -0,0 +1,9 @@
+bannedSites = [
+'LCG.DESY.de',			# allow share for other ilc users
+'LCG.KEK.jp',				# allow share for other ilc users
+'LCG.RAL-LCG2.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.Weizmann.il',	# crashes running lcsim
+'LCG.TECHNION.il',	# crashes running lcsim
+'LCG.NIPNE.ro',			# crashes running slic
+'LCG.Tau.il'				# crashes running slic
+]
