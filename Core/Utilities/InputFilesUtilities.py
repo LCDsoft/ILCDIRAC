42c0354d75e7fedb6c68e57ed1bc1a8da767750c
sposs <sposs@cern.ch>
2011-03-04 14:23:58 +0000

    Catch the evt type to pass to Mokka
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@35290 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/InputFilesUtilities.py
+++ Core/Utilities/InputFilesUtilities.py
@@ -20,7 +20,9 @@ def getNumberOfevents(inputfile):
         continue
     tags= res['Value']
     if tags.has_key("NumberOfEvents"):
-        nbevts['nbevts']=tags["NumberOfEvents"]
+      nbevts['nbevts']=tags["NumberOfEvents"]
     if tags.has_key("Luminosity"):
-        nbevts['lumi']=tags["Luminosity"]
+      nbevts['lumi']=tags["Luminosity"]
+    if tags.has_key("EvtType"):
+      nbevts['EvtType']=tags["EvtType"]
   return nbevts
