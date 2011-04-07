b13e563dcd99165e810429347eea5e780dc68729
sposs <sposs@cern.ch>
2011-04-07 13:16:39 +0000

    do a rstrip() on the InputData, as it seems there is a space by default
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@37633 4525493e-7705-40b1-a816-d608a930855b

--- Workflow/Modules/OverlayInput.py
+++ Workflow/Modules/OverlayInput.py
@@ -72,7 +72,7 @@ class OverlayInput (ModuleBase):
     if self.step_commons.has_key('BkgEvtType'):
       self.evttype = self.step_commons['BkgEvtType']  
       
-    if len(self.InputData):
+    if len(self.InputData.rstrip()) : 
       res = getNumberOfevents(self.InputData)
       if res.has_key("nbevts"):
         self.nbsigeventsperfile = res["nbevts"]
