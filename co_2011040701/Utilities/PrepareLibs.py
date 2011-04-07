d1a9bae7db4ec1d222a52898b6e7ea0b34a8590a
sposs <sposs@cern.ch>
2011-04-07 16:24:25 +0000

    tag for v1r17p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/Core@37656 4525493e-7705-40b1-a816-d608a930855b

---
+++ co_2011040701/Utilities/PrepareLibs.py
@@ -0,0 +1,26 @@
+'''
+Created on Jan 26, 2011
+
+@author: sposs
+'''
+import os
+
+def removeLibc(path):
+  #return True
+
+  curdir = os.getcwd()
+  os.chdir(path)
+  listlibs = os.listdir(os.getcwd())
+  for lib in listlibs:
+    if (lib.count("libc.so") or lib.count("libc-2.5") or lib.count("libm.so") 
+        or lib.count("libpthread.so") or lib.count("libdl.so")):
+      try:
+        os.remove(os.getcwd()+os.sep+lib)
+      except:
+        print "Could not remove %s"%lib
+        os.chdir(curdir)
+        return False
+  os.chdir(curdir)
+  return True
+
+  
