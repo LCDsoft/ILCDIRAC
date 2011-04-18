e3dba1ba97f4b2f101bd08ced5c90943e5541431
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-04-18 13:07:16 +0000

    Progressed on Service dev, but need to move to machine where GSI is available

--- ProcessProductionSystem/Services/ProcessManagerHandler.py
+++ ProcessProductionSystem/Services/ProcessManagerHandler.py
@@ -5,3 +5,26 @@
 """ Services for ProcessProduction System
 """
 __RCSID__ = " $Id: $ "
+
+from DIRAC                                              import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.Core.DISET.RequestHandler                    import RequestHandler
+
+from ILCDIRAC.ProcessProductionSystem.DB.ProcessDB import ProcessDB
+
+# This is a global instance of the ProcessDB class
+processDB = False
+
+def initializeProcessManagerHandler( serviceInfo ):
+
+  global processDB
+  processDB = ProcessDB()
+  return S_OK()
+
+class ProcessManagerHandler(RequestHandler):
+
+  types_addSoftware = [ StringTypes, StringTypes, StringTypes]
+  def export_addSoftware(self,AppName,AppVersion,Comment):
+    """ Add new software in the DB
+    """
+    return processDB.addSoftware(AppName,AppVersion,Comment)
+  
