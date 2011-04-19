eada7454bb74ee059f8a95d7e68b4b7772cb06b5
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-04-19 13:43:41 +0000

    First import of ProcessProdClient

---
+++ ProcessProductionSystem/Client/ProcessProdClient.py
@@ -0,0 +1,16 @@
+####################################################################
+# $HeadURL: $
+####################################################################
+'''
+Created on Apr 19, 2011
+
+@author: Stephane Poss
+'''
+__RCSID__ = "$Id: $"
+
+from DIRAC.Core.Base.Client                         import Client
+from DIRAC                                          import S_OK, S_ERROR
+
+class ProcessProdClient(Client):
+  def __init__(self,name='ProcessProdClient'):
+    self.setServer('ProcessProduction/ProcessManager')
