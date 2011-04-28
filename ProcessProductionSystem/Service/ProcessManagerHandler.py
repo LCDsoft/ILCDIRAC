e6ea02e6ca4e53ce82b8b9e7ae738bf1c25651ca
sposs <sposs@cern.ch>
2011-04-28 06:03:58 +0000

    Add more DB interface methods
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@38754 4525493e-7705-40b1-a816-d608a930855b

--- ProcessProductionSystem/Service/ProcessManagerHandler.py
+++ ProcessProductionSystem/Service/ProcessManagerHandler.py
@@ -31,32 +31,50 @@ class ProcessManagerHandler(RequestHandler):
     """
     return processDB.getSoftwares()
   
-  types_getProcessInfo = [StringTypes]
-  def export_getProcessInfo(self, ProcessName):
-    """Get all info for a given process
+  types_getProcessInfo = [StringTypes,[ListType,TupleType]]
+  def export_getProcessInfo(self, ProcessName, Params ):
+    """Get info for a given process
     """
-    return processDB.getProcessInfo(ProcessName)
+    return processDB.getProcessInfo(ProcessName, Params )
   
-  types_getTamples = [StringTypes,StringTypes]
+  types_getProductionDetails = [[LongType,IntType],[ListType,TupleType]]
+  def export_getProductionDetails(self,ProdID,Params):
+    """ Get the details of a given production
+    """
+    return processDB.getProductionResults(ProdID,Params)
+  
+  types_getTemplate = [StringTypes,StringTypes]
   def export_getTemplate(self, ProcessName, WhizVersion):
     """ Get the proper template
     """
     return processDB.getTemplate(ProcessName, WhizVersion)
 
+  types_getSoftwareParams = [StringTypes,StringTypes,StringTypes,[ListType,TupleType]]
+  def export_getSoftwareParams(self,AppName,AppVersion,Platform,Params):
+    """ Get the given software status
+    """
+    return processDB.getSoftwareParams(AppName,AppVersion,Platform,Params)
+  
 #######################################################################
 #              Add methods
 #######################################################################
-  types_addSoftware = [ StringTypes, StringTypes, StringTypes, StringTypes]
-  def export_addSoftware(self, AppName, AppVersion, Comment, Path):
+  types_addSoftware = [ StringTypes, StringTypes, StringTypes, StringTypes, StringTypes]
+  def export_addSoftware(self, AppName, AppVersion, Platform, Comment, Path):
     """ Add new software in the DB
     """
-    return processDB.addSoftware(AppName, AppVersion, Comment, Path)
+    return processDB.addSoftware(AppName, AppVersion, Platform, Comment, Path)
     
-  types_addDependency = [StringTypes,StringTypes,StringTypes,StringTypes]
-  def export_addDependency(self, AppName, AppVersion, DepName, DepVersion):
+  types_addDependency = [StringTypes,StringTypes,StringTypes,StringTypes,StringTypes]
+  def export_addDependency(self, AppName, AppVersion, DepName, DepVersion, Platform):
     """ Add a dependency between softwares
     """
-    return processDB.addDependency(AppName, AppVersion, DepName, DepVersion)
+    return processDB.addDependency(AppName, AppVersion, DepName, DepVersion, Platform)
+
+  types_addProcess = [StringTypes, StringTypes, StringTypes, StringTypes]
+  def export_addProcess(self,ProcessName, ProcessDetail, WhizardVers, Template):
+    """ Add a new process
+    """
+    return processDB.addProcess(ProcessName, ProcessDetail, WhizardVers, Template)
  
   types_addSteeringFile = [StringTypes, StringTypes]
   def export_addSteeringFile(self,FileName, Path=''):
@@ -72,15 +90,12 @@ class ProcessManagerHandler(RequestHandler):
         or not ProdDataDict.has_key("Process") 
         or not ProdDataDict.has_key("Path") 
         or not ProdDataDict.has_key("AppName")
-        or not ProdDataDict.has_key("AppVersion")):
+        or not ProdDataDict.has_key("AppVersion")
+        or not ProdDataDict.has_key('Platform')
+        ):
       return S_ERROR('Incorrect dictionary structure')
     return processDB.addProductionData(ProdDataDict)
   
-  types_addProcess = [StringTypes, StringTypes, StringTypes, StringTypes]
-  def export_addProcess(self,ProcessName, ProcessDetail, WhizardVers, Template):
-    """ Add a new process
-    """
-    return processDB.addProcess(ProcessName, ProcessDetail, WhizardVers, Template)
   
 #######################################################################
 #              Change methods
@@ -93,10 +108,10 @@ class ProcessManagerHandler(RequestHandler):
       return S_ERROR("Missing essential dictionary info")
     return processDB.updateCrossSection(ProcessDict)
     
-  types_changeSoftwareStatus = [StringTypes,StringTypes,StringTypes,BooleanType]
-  def export_changeSoftwareStatus(self,AppName,AppVersion,Comment,Status = False):
+  types_changeSoftwareStatus = [StringTypes,StringTypes,StringTypes,StringTypes,BooleanType]
+  def export_changeSoftwareStatus(self,AppName, AppVersion, Platform, Comment, Status = False):
     """ Change the status of a software, by feault to False
     """
-    return processDB.changeSoftwareStatus(AppName, AppVersion, Comment, Status)
+    return processDB.changeSoftwareStatus(AppName, AppVersion, Platform, Comment, Status)
   
 
