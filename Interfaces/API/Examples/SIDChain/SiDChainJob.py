962d3ffbef8e0a00733c4799b8cf94da0a5ee804
Christian Grefe <christian.grefe@cern.ch>
2011-05-18 12:21:19 +0000

    Several fixes and completely reworked verbose output to be more useful

--- Interfaces/API/Examples/SIDChain/SiDChainJob.py
+++ Interfaces/API/Examples/SIDChain/SiDChainJob.py
@@ -1,45 +1,16 @@
-##
-#  Convenience script for submitting jobs with full reconstruction chain (or parts of it) to dirac
+###
+#  Convenience script for submitting jobs with full reconstruction chain (or parts of it) to DIRAC
 #  christian.grefe@cern.ch
 ##
 
 from DIRAC.Core.Base import Script
 import sys
 
-Script.registerSwitch( 'a:', 'alias=', 'name of the alias.properties file to use (default alias.properties)' )
-Script.registerSwitch( 'A', 'agent', 'Submits the job in agent mode, which will run the job on the local machine' )
-Script.registerSwitch( 'b:', 'banlist=', 'file with list of banned sites (default bannedSites.py)' )
-Script.registerSwitch( 'D:', 'detector=', 'name of the detector model (default clic_sid_cdr)' )
-Script.registerSwitch( 'i:', 'input=', 'input python script holding the lfnlist to process' )
-Script.registerSwitch( 'I:', 'prodid=', 'use a production id to define the input lfnlist' )
-Script.registerSwitch( 'l:', 'lcsimxml=', 'lcsim steering xml template (optional)' )
-Script.registerSwitch( 'L:', 'lcsim=', 'lcsim version to use (1.15-SNAPSHOT)' )
-Script.registerSwitch( 'j:', 'jobs=', 'number of jobs that each input file gets split into (default 1)' )
-Script.registerSwitch( 'm:', 'macro=', 'name of the macro file used for SLIC (default default.mac)' )
-Script.registerSwitch( 'M:', 'merge=', 'number of slcio input files used per job, only used if no slic step (default 1)' )
-Script.registerSwitch( 'n:', 'events=', 'number of events per job, -1 for all in file (default -1)' )
-Script.registerSwitch( 'O:', 'overlay=', 'number of bunch crossings overlaid over each event (default 0)' )
-Script.registerSwitch( 'w:', 'overlayweight=', 'number of gg->hadron interactions per bunch crossing (default 3.2)' )
-Script.registerSwitch( 'p:', 'process=', 'process name to be used for naming of path etc.' )
-Script.registerSwitch( 'P:', 'pandora=', 'slicPandora version to use (default CDR0)' )
-Script.registerSwitch( 'S:', 'slic=', 'slic version (default v2r8p4)' )
-Script.registerSwitch( 't:', 'time=', 'CPU time limit per job in seconds (default 300000)' )
-Script.registerSwitch( 'T:', 'title=', 'job title (default fullReco)' )
-Script.registerSwitch( 'v', 'verbose', 'switches on the verbose mode' )
-Script.registerSwitch( 'x:', 'settings=', 'name of pandora settings file (default taken from grid installation)' )
-Script.registerSwitch( 'y:', 'strategy=', 'name of tracking strategy file to use' )
-Script.registerSwitch( 'z:', 'maxfiles=', 'maximum number of files to process (default -1)' )
-
-Script.setUsageMessage( sys.argv[0]+'-n <nEvts> (-i <inputList> OR -I <prodID> AND/OR -m <macro>) (<additional options>)' )
-
-Script.parseCommandLine()
-switches = Script.getUnprocessedSwitches()
-
 # default parameters
 macroFile = 'slicMacros/defaultClicCrossingAngle.mac'
 slicPandoraVer = 'CDR1'
-lcsimVer = '1.15-SNAPSHOT'
-slicVer = 'v2r8p4'
+lcsimVer = '1.18-SNAPSHOT'
+slicVer = 'v2r9p8'
 detector = 'clic_sid_cdr'
 jobTitle = 'fullReco'
 inputFileList = None
@@ -49,11 +20,13 @@ settingsFile = ''
 cpuLimit = 300000
 mergeSlcioFiles = 1
 slicPandoraPath = 'LFN:/ilc/prod/software/slicpandora/'
+slicPandoraDetector = detector
 lcsimPath = 'LFN:/ilc/prod/software/lcsim/'
 systemConfig = 'x86_64-slc5-gcc43-opt'
 storageElement = 'CERN-SRM'
 aliasFile = 'alias.properties'
-xmlFile = 'defaultPrePandoraLcsim.xml'
+xmlFile = lcsimPath+'clic_cdr_prePandora.lcsim'
+xmlPostPandora = lcsimPath+'clic_cdr_postPandoraOverlay.lcsim' # always use the overlay version to create the selected PFO files
 lcsimTemplate = ''
 strategyFile = 'defaultStrategies.xml'
 banlistFile = 'bannedSites.py'
@@ -65,6 +38,36 @@ prodID = None
 process = None
 debug = False
 agentMode = False
+
+Script.registerSwitch( 'a:', 'alias=', 'name of the alias.properties file to use (default %s)'%(aliasFile) )
+Script.registerSwitch( 'A', 'agent', 'Submits the job in agent mode, which will run the job on the local machine' )
+Script.registerSwitch( 'b:', 'banlist=', 'file with list of banned sites (default %s)'%(banlistFile) )
+Script.registerSwitch( 'D:', 'detector=', 'name of the detector model (default %s)'%(detector) )
+Script.registerSwitch( 'i:', 'input=', 'input python script holding the lfnlist to process' )
+Script.registerSwitch( 'I:', 'prodid=', 'use a production id to define the input lfnlist' )
+Script.registerSwitch( 'l:', 'lcsimxml=', 'lcsim steering xml template (default %s)'%(lcsimPath+xmlFile) )
+Script.registerSwitch( 'L:', 'lcsim=', 'lcsim version to use (default %s)'%(lcsimVer) )
+Script.registerSwitch( 'j:', 'jobs=', 'number of jobs that each input file gets split into (default %s)'%(nJobs) )
+Script.registerSwitch( 'm:', 'macro=', 'name of the macro file used for SLIC (default %s)'%(macroFile) )
+Script.registerSwitch( 'M:', 'merge=', 'number of slcio input files used per job, only used if no slic step (default %s)'%(mergeSlcioFiles) )
+Script.registerSwitch( 'n:', 'events=', 'number of events per job, -1 for all in file (default %s)'%(nEvts) )
+Script.registerSwitch( 'O:', 'overlay=', 'number of bunch crossings overlaid over each event (default %s)'%(overlayBX) )
+Script.registerSwitch( 'w:', 'overlayweight=', 'number of gg->hadron interactions per bunch crossing (default %s)'%(overlayWeight) )
+Script.registerSwitch( 'p:', 'process=', 'process name to be used for naming of path etc.' )
+Script.registerSwitch( 'P:', 'pandora=', 'slicPandora version to use (default %s)'%(slicPandoraVer) )
+Script.registerSwitch( 'S:', 'slic=', 'slic version (default %s)'%(slicVer) )
+Script.registerSwitch( 't:', 'time=', 'CPU time limit per job in seconds (default %s)'%(cpuLimit) )
+Script.registerSwitch( 'T:', 'title=', 'job title (default %s)'%(jobTitle) )
+Script.registerSwitch( 'v', 'verbose', 'switches on the verbose mode' )
+Script.registerSwitch( 'x:', 'settings=', 'name of pandora settings file (default taken from grid installation)' )
+Script.registerSwitch( 'y:', 'strategy=', 'name of tracking strategy file to use (default %s)'%(strategyFile) )
+Script.registerSwitch( 'z:', 'maxfiles=', 'maximum number of files to process (default %s)'%(maxFiles) )
+Script.registerSwitch( 'g:', 'pandoradetector=', 'name of pandora geometry xml file or just detector name (default %s)'%(maxFiles) )
+
+Script.setUsageMessage( sys.argv[0]+'-n <nEvts> (-i <inputList> OR -I <prodID> AND/OR -m <macro>) (<additional options>)' )
+
+Script.parseCommandLine()
+switches = Script.getUnprocessedSwitches()
 
 for switch in switches:
 	opt = switch[0]
@@ -115,14 +118,26 @@ for switch in switches:
 		maxFiles = int(arg)
 	if opt in ('v','verbose'):
 		debug = True
+	if opt in ('g','pandoradetector'):
+		slicPandoraDetector = arg
+
+if debug:
+	print ''
+	print '################################'
+	print ' SiD job submission to DIRAC'
+	print '        christian.grefe@cern.ch'
+	print '################################'
+	print ''
 
 if not inputFileList and not prodID:
 	if macroFile == 'slicMacros/default.mac':
 		Script.showHelp()
 		sys.exit(2)
 		
-if overlayBX > 0 and not lcsimTemplate:
-	xmlFile = 'defaultPrePandoraLcsimOverlay.xml'
+if overlayBX > 0:
+	if not lcsimTemplate:
+		xmlFile = lcsimPath+'clic_cdr_prePandoraOverlay.lcsim'
+	xmlPostPandora = lcsimPath+'clic_cdr_postPandoraOverlay.lcsim'
 
 
 # helper function to replace strings in a textfile
@@ -152,6 +167,8 @@ inputSandbox.append( 'LFN:/ilc/prod/software/lcsim/trackingStrategies/'+detector
 # JNI bindings for root writer
 inputSandbox.append( 'lib' )
 
+outputSandbox = [ "*.log", "*.xml", "*.lcsim" ]
+
 # read file with list of banned sites
 if banlistFile:
 	f = open( banlistFile, 'r')
@@ -159,22 +176,24 @@ if banlistFile:
 if not bannedSites:
 	bannedSites = ['']
 
+if lcsimVer and overlayBX > 0:
+	jobTitle += '_%sBXgghad'%(overlayBX)
 repositoryFile = 'repositoryFiles/'+detector+'.'+jobTitle+'.'
 
 # create a repository file and generate list of lfns to process
 if prodID:      # use a production id to define the lfnlist
 	meta = {}
 	meta[ 'ProdID' ] = prodID
-	print 'Using production ID to define LFN list...'
+	print 'Using production ID to define LFN list:'
 	result = fileCatalog.getCompatibleMetadata( meta )
 	if not result['OK']:
 		print "Error looking up the file catalog for metadata"
 		sys.exit(2)
 	metaData = result['Value']
-	print 'Found production ID %s. Associated meta data:'%( prodID )
+	print '  Found production ID %s. Associated meta data:'%( prodID )
 	for key, value in metaData.iteritems():
 		if not len(value) == 0:
-			print '\t%s: %s'%( key, value[0] )
+			print '    %s: %s'%( key, value[0] )
 	if metaData['Datatype'][0] == 'SIM':
 		slicVer = ''		# data has been simulated, can skip SLIC step
 	if metaData['EvtType'] and not process:
@@ -187,7 +206,8 @@ if prodID:      # use a production id to define the lfnlist
 		print "Error looking up the file catalog for metadata"
 		sys.exit(2)
 	lfnlist = map(lambda x: "LFN:"+x, result['Value'])	# need to add "LFN:" to each entry
-	print 'Found %s files associated with meta data'%( len(lfnlist) )
+	print '  Found %s files associated with meta data'%( len(lfnlist) )
+	print ''
 	repositoryFile += 'prod'+prodID+'.cfg'
 elif inputFileList:     # read a file containing an lfnlist
 	repositoryFile += inputFileList.split('/')[-1].replace('.py','.cfg')
@@ -199,9 +219,6 @@ elif inputFileList:     # read a file containing an lfnlist
 else:           # no input files, GEANT4 particle source should be defined in slic macro
 	repositoryFile += macroFile.split('/')[-1].replace('.mac','.cfg')
 	lfnlist = [ '' ]
-
-if debug:
-	print 'Writing repository file to', repositoryFile, '...'
 
 dirac = DiracILC ( True , repositoryFile )
 
@@ -220,8 +237,9 @@ for inputFile in lfnlist:
 			continue
 		inputSlcios = inputFiles
 		inputFiles = []
-		
+	
 	if inputFile:
+	
 		if slicVer:
 			outputFileBase = jobTitle+'_'+inputFile.split('/')[-1].replace( '.stdhep', '' )
 			if not process:
@@ -239,9 +257,6 @@ for inputFile in lfnlist:
 		if not process:
 			print 'ERROR: no process defined. Use -p <processName> to define the storage path'
 			sys.exit(2)
-	
-	if lcsimVer and overlayBX > 0:
-		outputFileBase += '_%sBX'%(overlayBX)
 
 	for job in xrange( nJobs ):
 		
@@ -278,33 +293,12 @@ for inputFile in lfnlist:
 				if key.count('output') and count:
 					outputData.append( rep )		# add to list of output files if key denotes some output and is present in template
 		else:
-			outputData.append( outputFile+'.slcio' )		# default reco only creates one output file
+			if slicPandoraVer and lcsimVer:
+				outputData.append( outputFile+'_REC.slcio' )
+				outputData.append( outputFile+'_DST.slcio' )
+			else:
+				outputData.append( outputFile+'.slcio' )		# default reco only creates one output file
 		
-		
-		if debug:
-			print 'Parameters:'
-			print '\tProcess:', process
-			print '\tOutputFile:', outputFile
-			print '\tDetector:', detector
-			print '\tJobs per input file:', nJobs
-			print '\tMaximum files:', maxFiles
-			print '\tEvents per job:', nEvts
-			print '\tMerged files per job:', mergeSlcioFiles
-			print '\tTotal number of jobs:', len(lfnlist)*nJobs/mergeSlcioFiles
-			print '\tSlic version:', slicVer
-			print '\tMacro file:', macroFile
-			print '\tOverlay bunch crossings:', overlayBX
-			print '\tgg->hadron events per bunch crossings:', overlayWeight
-			print '\tLCSim version:', lcsimVer
-			print '\tLCSim file:', xmlFile
-			print '\tTracking strategies:', strategyFile
-			print '\tAlias file:', aliasFile
-			print '\tSlicPandora version:', slicPandoraVer
-			print '\tPandora settings:', settingsFile
-			print '\tInput Sandbox:', inputSandbox
-			print '\tStorage path:', storagePath
-			print '\tOutput data:', outputData
-			print '\tBanned sites:', bannedSites
 		
 		startEvt = job*nEvts
 		
@@ -354,7 +348,7 @@ for inputFile in lfnlist:
 		
 		if slicPandoraVer:
 			res = job.setSLICPandora ( appVersion=slicPandoraVer ,
-				detectorgeo=detector ,
+				detectorgeo=slicPandoraDetector ,
 				inputslcio=inputSlcios ,
 				pandorasettings=settingsFile ,
 				nbevts=nEvts ,
@@ -363,8 +357,19 @@ for inputFile in lfnlist:
 			if not res['OK']:
 				print res['Message']
 				sys.exit(2)
+			if lcsimVer:
+				res = job.setLCSIM ( lcsimVer ,
+					xmlfile=xmlPostPandora ,
+					aliasproperties=aliasFile ,
+					evtstoprocess=nEvts,
+					outputRECFile=outputFile+'_REC.slcio',
+					outputDSTFile=outputFile+'_DST.slcio'
+					)
+				if not res['OK']:
+					print res['Message']
+					sys.exit(2)
 		
-		job.setOutputSandbox ( [ "*.log", "*.xml" ] )
+		job.setOutputSandbox ( outputSandbox )
 		job.setInputSandbox ( inputSandbox )
 		job.setOutputData ( outputData, storageElement, storagePath )	
 		job.setCPUTime( cpuLimit )
@@ -372,14 +377,91 @@ for inputFile in lfnlist:
 		job.setName ( detector+"_"+process+"_"+jobTitle )
 		job.setBannedSites( bannedSites )
 		#job.setDestination('LCG.CERN.ch')
+		
 		if debug:
-			answer = raw_input('Submit job(s)? (Y/N): ')
+			print ''
+			print 'Jobs to submit:'
+			nTotal = len(lfnlist)*nJobs/mergeSlcioFiles
+			if inputFile:
+				print '  Number of input files:', len(lfnlist)
+				if maxFiles > 0:
+					print '  Maximum input files to use:', maxFiles
+					nTotal = maxFiles*nJobs/mergeSlcioFiles
+				if mergeSlcioFiles > 1:
+					print '  Merged input files per job:', mergeSlcioFiles
+			if nJobs != 1:
+				print '  Jobs per input file:', nJobs
+			if nEvts < 0:
+				print '  Events per job: all'
+			else :
+				print '  Events per job:', nEvts
+			print '  Total number of jobs:', nTotal
+			print '  Maximum CPU time per job:', cpuLimit, 'sec'
+			print ''
+			
+			print 'General parameters:'
+			print '  Detector model:', detector
+			print '  Process name:', process
+			print '  Job title:', jobTitle
+			print '  Banned sites:', bannedSites
+			print '  Repository file:', repositoryFile
+			print ''
+			
+			print 'Files:'
+			print '  Input sand box:', inputSandbox
+			print '  Output sand box:', outputSandbox
+			print '  Output data:', outputData
+			print '  Output storage path:', storagePath
+			print '  Output storage element:', storageElement
+			print ''
+			
+			print 'Steps executed:'
+			step = 0
+			if slicVer:
+				step += 1
+				print '  %s) Slic step:'%(step)
+				print '    Slic version:', slicVer
+				print '    Macro file:', macroFile
+				print ''
+			
+			if lcsimVer:
+				step += 1
+				print '  %s) LCSim step:'%(step)
+				print '    LCSim version:', lcsimVer
+				print '    LCSim file:', xmlFile
+				print '    Tracking strategies:', strategyFile
+				print '    Detector alias file:', aliasFile
+				if overlayBX > 0:
+					print '    Number of bunch crossings overlayed:', overlayBX
+					print '    gg->hadron events per bunch crossing:', overlayWeight
+				print ''
+			
+			if slicPandoraVer:
+				step += 1
+				print '  %s) SlicPandora step:'%(step)
+				print '    SlicPandora version:', slicPandoraVer
+				mySettingsFile = settingsFile
+				if not settingsFile:
+					mySettingsFile = 'default'
+				print '    Pandora settings file:', mySettingsFile
+				print '    Pandora detector file:', slicPandoraDetector
+				print ''
+				if lcsimVer:
+					step += 1
+					print '  %s) LCSim step:'%(step)
+					print '    LCSim version:', lcsimVer
+					print '    LCSim file:', xmlPostPandora
+					print '    Detector alias file:', aliasFile
+					print ''
+
+			answer = raw_input('Proceed and submit job(s)? (Y/N): ')
 			if not answer.lower() in ('y', 'yes'):
 				sys.exit(2)
 			else:
 				# no more debug output in further loops
 				debug = False
 		if not agentMode:
+			#print "submitting"
 			dirac.submit ( job )
 		else:
 			dirac.submit ( job, mode="Agent" )
