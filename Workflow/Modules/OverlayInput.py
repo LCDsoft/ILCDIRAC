3ede77631f593cd8d3ca73e91507e8fa761d229b
sposs <sposs@cern.ch>
2011-06-27 06:28:54 +0000

    define additionnal env vars (host not known)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@40812 4525493e-7705-40b1-a816-d608a930855b

--- Workflow/Modules/OverlayInput.py
+++ Workflow/Modules/OverlayInput.py
@@ -449,19 +449,21 @@ class OverlayInput (ModuleBase):
       print res
     #comm.append("xrdcp root://ccdcacsn179.in2p3.fr:1094%s ./ -s"%file)
     #command = string.join(comm,";")
-    
-#      comm4= ['declare','-x','CNS_HOST=castorns.ads.rl.ac.uk']
-#      res = subprocess.call(comm4)
-#      print res
-#      comm5= ['declare','-x','STAGE_SVCCLASS=ilcTape']
+    logfile = file(self.applicationLog,"w")
+    os.environ['CNS_HOST']='castorns.ads.rl.ac.uk'
+    #comm4= ['declare','-x','CNS_HOST=castorns.ads.rl.ac.uk']
+    #res = subprocess.Popen(comm4,stdout=logfile,stderr=subprocess.STDOUT)
+    #print res
+    os.environ['STAGE_SVCCLASS']='ilcTape'
+#     comm5= ['declare','-x','STAGE_SVCCLASS=ilcTape']
 #      res = subprocess.call(comm5)
 #      print res
+    os.environ['STAGE_HOST']='cgenstager.ads.rl.ac.uk'
 #      comm6=['declare','-x','STAGE_HOST=cgenstager.ads.rl.ac.uk']
 #      res = subprocess.call(comm6)
 #      print res
     basename=os.path.basename(lfile)
     comm7=["/usr/bin/rfcp","'rfio://cgenstager.ads.rl.ac.uk:9002?svcClass=ilcTape&path=%s'"%lfile,"file:%s"%basename]
-    logfile = file(self.applicationLog,"w")
     try:
       res = subprocess.Popen(comm7,stdout=logfile,stderr=subprocess.STDOUT)
     except Exception,x:
