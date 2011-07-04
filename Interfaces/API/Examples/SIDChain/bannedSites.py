d06ebefa5529153488aedf492201068fcaf10ccf
cgrefe <cgrefe@cern.ch>
2011-07-04 09:25:16 +0000

    Some updates to default values
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@41076 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/API/Examples/SIDChain/bannedSites.py
+++ Interfaces/API/Examples/SIDChain/bannedSites.py
@@ -1,7 +1,7 @@
 bannedSites = [
-'LCG.DESY.de',		# allow share for other ilc users
-'LCG.KEK.jp',		# allow share for other ilc users
 'LCG.RAL-LCG2.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.Bristol.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.UKI-NORTHGRID-LANCS-HEP.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
 'LCG.Weizmann.il',	# crashes running lcsim
 'LCG.TECHNION.il',	# crashes running lcsim
 'LCG.NIPNE.ro',		# crashes running slic
