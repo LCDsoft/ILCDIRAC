84712bb7376baccfa282cb10fe4a6e3d278924fa
sposs <sposs@cern.ch>
2011-07-13 14:37:52 +0000

    tag v1r19p0, for OverlayInput
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@41273 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ __path__ = extend_path(__path__, __name__)
 # Define Version
 
 majorVersion = 1
-minorVersion = 18
-patchLevel   = 16
+minorVersion = 19
+patchLevel   = 0
 preVersion   = 0
 
 version      = "v%sr%s" % ( majorVersion, minorVersion )
