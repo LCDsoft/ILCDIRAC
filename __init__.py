d60a8689fd4e9683efda3ed68d3017432844f443
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-07-22 14:52:55 +0000

    defined next release

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ __path__ = extend_path(__path__, __name__)
 # Define Version
 
 majorVersion = 2
-minorVersion = 1
-patchLevel   = 10
+minorVersion = 2
+patchLevel   = 0
 preVersion   = 0
 
 version      = "v%sr%s" % ( majorVersion, minorVersion )
