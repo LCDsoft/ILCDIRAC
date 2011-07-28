ef7140e1932e8430f26c1b8f750109cee6c9033a
sposs <sposs@cern.ch>
2011-07-28 20:42:03 +0000

    First import and first draft of new interface
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@41769 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/API/NewInterface/ProductionJob.py
@@ -0,0 +1,38 @@
+'''
+Created on Jul 28, 2011
+
+@author: Stephane Poss
+'''
+
+from ILCDIRAC.Interfaces.API.NewInterface.Job import Job
+from DIRAC.Resources.Catalog.Client.FileCatalogClient import FileCatalogClient
+from DIRAC import S_OK,S_ERROR
+
+class ProductionJob(Job):
+  def __init__(self):
+    Job.__init__(self)
+    self.fc = FileCatalogClient()
+    
+  def setInputDataQuery(self,metadict):
+    res = self.fc.findFilesByMetadata(metadict)
+    if not res['OK']:
+      return res
+    """ Also get the compatible metadata such as energy, evttype, etc, populate dictionary
+    """
+    return S_OK()
+  
+  def createProd(self):
+    return S_OK()
+  
+  def finalizeProd(self):
+    return S_OK()  
+  
+  def _jobSpecificParams(self,app):
+    """ For production additional checks are needed: ask the user
+    """
+    if not app.logfile:
+      logf = app.appname+"_"+app.version+".log"
+      app.setLogFile(logf)
+      #in fact a bit more tricky as the log files have the prodID and jobID in them
+    return S_OK()
+  
