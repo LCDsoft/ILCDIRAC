fd63b0873b59e0f03adc8651947266ad13b135d5
sposs <sposs@cern.ch>
2011-07-29 15:37:09 +0000

    Release v2r5p1
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v2r5p1@41782 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/scripts/dirac-repo-retrieve-jobs-output-data.py
@@ -0,0 +1,27 @@
+'''
+Created on Mar 24, 2010
+
+@author: sposs
+'''
+import DIRAC
+from DIRAC.Core.Base import Script
+from DIRAC.Interfaces.API.Dirac import Dirac
+import os,sys
+
+Script.parseCommandLine( ignoreErrors = False )
+args = sys.argv
+
+def usage():
+  print 'Usage: %s repo' % (Script.scriptName)
+  DIRAC.exit(2)
+if len(args) < 2:
+  usage()
+
+repoLocation = args[1]
+dirac=Dirac(True, repoLocation)
+
+exitCode = 0
+dirac.monitorRepository(False)
+dirac.retrieveRepositoryData()
+
+DIRAC.exit(exitCode)
