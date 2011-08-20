be138c25bca2935a716b2048f3d1828697ecf583
Remi Ete <remi.ete@gmail.com>
2011-08-20 18:02:37 +0000

    add OverlayInput ...

--- Interfaces/API/NewInterface/Applications.py
+++ Interfaces/API/NewInterface/Applications.py
@@ -1150,6 +1150,127 @@ class SLIC(Application):
     return S_OK()   
   
   
+#################################################################
+#            OverlayInput : 
+#################################################################  
+class OverlayInput(Application):
+  """ Run a script (python or shell) in an application environment. 
+  
+  Example:
+  
+  >>> ga = GenericApplication()
+  >>> ga.setScript("myscript.py")
+  >>> ga.setArguments("some command line arguments")
+  >>> ga.setDependency({"root":"5.26"})
+  
+  """
+  def __init__(self, paramdict = None):
+    self.script = None
+    self.arguments = ''
+    self.dependencies = {}
+    ### The Application init has to come last as if not the passed parameters are overwritten by the defaults.
+    Application.__init__(self, paramdict)
+    #Those have to come last as the defaults from Application are not right
+    self._modulename = "ApplicationScript"
+    self.appname = self._modulename
+    self._moduledescription = 'An Application script module that can execute any provided script in the given project name and version environment'
+      
+  def setScript(self,script):
+    """ Define script to use
+    
+    @param script: Script to run on. Can be shell or python. Can be local file or LFN.
+    @type script: string
+    """
+    self._checkArgs( {
+        'script' : types.StringTypes
+      } )
+    if os.path.exists(script) or script.lower().count("lfn:"):
+      self.inputSB.append(script)
+    self.script = script
+    return S_OK()
+    
+  def setArguments(self,args):
+    """ Define the arguments of the script (if any)
+    
+    @param arguments: Arguments to pass to the command line call
+    @type arguments: string
+    
+    """
+    self._checkArgs( {
+        'args' : types.StringTypes
+      } )  
+    self.arguments = args
+    return S_OK()
+      
+  def setDependency(self,appdict):
+    """ Define list of application you need
+    
+    >>> app.setDependency({"mokka":"v0706P08","marlin":"v0111Prod"})
+    
+    @param appdict: Dictionary of applciation to use: {"App":"version"}
+    @type appdict: dict
+    
+    """  
+    #check that dict has proper structure
+    self._checkArgs( {
+        'appdict' : types.DictType
+      } )
+    
+    self.dependencies.update(appdict)
+    return S_OK()
+
+  def _applicationModule(self):
+    m1 = self._createModuleDefinition()
+    m1.addParameter(Parameter("script", "", "string", "", "", False, False, "Script to execute"))
+    m1.addParameter(Parameter("arguments", "", "string", "", "", False, False, "Arguments to pass to the script"))
+    m1.addParameter(Parameter("debug", False, "bool", "", "", False, False, "debug mode"))
+    return m1
+  
+  def _applicationModuleValues(self,moduleinstance):
+    moduleinstance.setValue("script",self.script)
+    moduleinstance.setValue('arguments',self.arguments)
+    moduleinstance.setValue('debug',self.debug)
+  
+  def _userjobmodules(self,stepdefinition):
+    res1 = self._setApplicationModuleAndParameters(stepdefinition)
+    res2 = self._setUserJobFinalization(stepdefinition)
+    if not res1["OK"] or not res2["OK"] :
+      return S_ERROR('userjobmodules failed')
+    return S_OK() 
+
+  def _prodjobmodules(self,stepdefinition):
+    res1 = self._setApplicationModuleAndParameters(stepdefinition)
+    res2 = self._setOutputComputeDataList(stepdefinition)
+    if not res1["OK"] or not res2["OK"] :
+      return S_ERROR('prodjobmodules failed')
+    return S_OK()    
+
+  def _addParametersToStep(self,stepdefinition):
+    res = self._addBaseParameters(stepdefinition)
+    if not res["OK"]:
+      return S_ERROR("Failed to set base parameters")
+    return S_OK()
+  
+  def _setStepParametersValues(self, instance):
+    self._setBaseStepParametersValues(instance)
+    for depn,depv in self.dependencies.items():
+      self.job._addSoftware(depn,depv)
+    return S_OK()
+      
+  def _checkConsistency(self):
+    """ Checks that script and dependencies are set.
+    """
+    if not self.script:
+      return S_ERROR("Script not defined")
+    elif not self.script.lower().count("lfn:") and not os.path.exists(self.script):
+      return S_ERROR("Specified script is not an LFN and was not found on disk")
+      
+    if not len(self.dependencies):
+      return S_ERROR("Dependencies not set")
+    return S_OK() 
+  
+  
+  
 ##########################################################################
 #            Marlin: Reconstructor after Mokka
 ##########################################################################
