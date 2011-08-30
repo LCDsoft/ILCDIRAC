a9c4281e9d641f65b9da4adddbc85e4e875bdabb
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-08-30 11:03:35 +0000

    always print outpuList

--- Workflow/Modules/UploadOutputData.py
+++ Workflow/Modules/UploadOutputData.py
@@ -112,7 +112,6 @@ class UploadOutputData(ModuleBase):
                 olist.append(appdict)
                 break
         self.outputList = olist
-        self.log.verbose("OutputList : %s"%self.outputList)
       else:
         olist = []
         for obj in self.outputList:
@@ -121,7 +120,7 @@ class UploadOutputData(ModuleBase):
                                                   int(self.workflow_commons["JOB_ID"]))
           olist.append(appdict)
         self.outputList = olist
-        self.log.verbose("OutputList : %s"%self.outputList)  
+      self.log.verbose("OutputList : %s"%self.outputList)  
 
     if self.workflow_commons.has_key('outputMode'):
       self.outputMode = self.workflow_commons['outputMode']
