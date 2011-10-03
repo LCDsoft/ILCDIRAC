5a49d27fec6e11d7b1a4768e3d84078b483372ff
sposs <sposs@cern.ch>
2011-10-03 16:29:08 +0000

    tag for v3r1p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/Core@43215 4525493e-7705-40b1-a816-d608a930855b

---
+++ co_2011100301/Utilities/GeneratorModels.py
@@ -0,0 +1,15 @@
+###########################################################################
+# $HeadURL: $
+###########################################################################
+
+""" Contains the list of models and their properties
+"""
+__RCSID__ = " $Id: $ "
+
+class GeneratorModels(dict):
+  """ Contains the list of known models
+  """
+  def __init__(self):
+    self["sm"] = None
+    self["slsqhh"] = "LesHouches_slsqhh.msugra_1.in"
+    self["chne"] = "LesHouches_chne.msugra_1.in"
