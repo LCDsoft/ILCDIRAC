aac031b48751a3b96cedaba8c691515d9f76995d
sposs <sposs@cern.ch>
2011-10-20 15:20:36 +0000

    Release v3r3p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v3r3p0@44048 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/GeneratorModels.py
@@ -0,0 +1,30 @@
+###########################################################################
+# $HeadURL: $
+###########################################################################
+
+""" Contains the list of models and their properties
+"""
+__RCSID__ = " $Id: $ "
+
+from DIRAC import gConfig,S_OK,S_ERROR
+
+class GeneratorModels():
+  """ Contains the list of known models
+  """
+  def __init__(self):
+    self.models = {}
+    res = gConfig.getOptionsDict("/Operations/Models")
+    if res['OK']:
+      self.models = res['Value']
+
+  def hasModel(self,model):
+    if self.models.has_key(model):
+      return S_OK()
+    else:
+      return S_ERROR("Model %s is not defined, use any of %s"%(model, self.models.keys()))
+
+  def getFile(self,model):
+    res = self.hasModel(model)
+    if not res['OK']:
+      return res
+    return S_OK(self.models[model])
