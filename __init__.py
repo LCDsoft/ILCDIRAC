8420761b07190a307998ef794e4e74bbab84f9fe
sposs <sposs@cern.ch>
2011-10-20 15:20:18 +0000

    tag v3r3p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@44043 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ __path__ = extend_path(__path__, __name__)
 # Define Version
 
 majorVersion = 3
-minorVersion = 2
-patchLevel   = 1
+minorVersion = 3
+patchLevel   = 0
 preVersion   = 0
 
 version      = "v%sr%s" % ( majorVersion, minorVersion )
