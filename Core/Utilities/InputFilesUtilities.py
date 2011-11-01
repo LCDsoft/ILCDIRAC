a4376ace62306315546365860496f652baaed1f8
sposs <sposs@cern.ch>
2011-11-01 12:56:00 +0000

    Release v3r4p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v3r4p0@44350 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/InputFilesUtilities.py
@@ -0,0 +1,29 @@
+'''
+Created on Nov 2, 2010
+
+@author: sposs
+'''
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+import string,os
+
+def getNumberOfevents(inputfile):
+  """ Find from the FileCatalog the number of events in a file
+  """
+  files = inputfile.split(";")
+  fc = FileCatalogClient()
+  nbevts = {} 
+  nbevts['nbevts'] = 0
+  nbevts['lumi'] = 0
+  for file in files:
+    print file
+    res = fc.getDirectoryMetadata(file)
+    if not res['OK']:
+        continue
+    tags= res['Value']
+    if tags.has_key("NumberOfEvents"):
+      nbevts['nbevts']+=tags["NumberOfEvents"]
+    if tags.has_key("Luminosity"):
+      nbevts['lumi']+=tags["Luminosity"]
+    if tags.has_key("EvtType"):
+      nbevts['EvtType']=tags["EvtType"]
+  return nbevts
