8608e9bc88f99ab4e57097ff7dfa4f69260ed0b9
sposs <sposs@cern.ch>
2011-11-08 14:48:41 +0000

    First import of module containing the list of available files (in the future, same functionality should be provided by service, for flexibility)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@44571 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/InstalledFiles.py
@@ -0,0 +1,20 @@
+'''
+Created on Nov 8, 2011
+
+@author: Stephane Poss
+'''
+from DIRAC import S_OK,S_ERROR
+
+def Exists(file):
+  files = ["defaultClicCrossingAngle.mac","clic_ild_cdr500.steer",
+           "clic_ild_cdr.steer","clic_cdr_prePandora.lcsim",
+           "clic_cdr_postPandora.lcsim","clic_cdr_prePandoraOverlay.lcsim",
+           "clic_cdr_postPandoraOverlay.lcsim","clic_ild_cdr.gear",
+           "clic_ild_cdr500.gear","clic_ild_cdr_steering_overlay.xml",
+           "clic_ild_cdr500_steering_overlay.xml","clic_ild_cdr_steering.xml",
+           "clic_ild_cdr500_steering.xml"]
+  if file in files:
+    return S_OK()
+  else:
+    return S_ERROR("File is not installed")
+  
