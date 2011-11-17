e07bde832a0c1e76b12b1a445db78be6649e97df
sposs <sposs@cern.ch>
2011-11-17 15:54:59 +0000

    Release v4r1p7
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v4r1p7@45032 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/API/Examples/SIDChain/bannedSites.py
@@ -0,0 +1,10 @@
+bannedSites = [
+'LCG.RAL-LCG2.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.Bristol.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.UKI-NORTHGRID-LANCS-HEP.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.Weizmann.il',	# crashes running lcsim
+'LCG.TECHNION.il',	# crashes running lcsim
+'LCG.NIPNE.ro',		# crashes running slic
+'LCG.Tau.il',		# crashes running slic
+'LCG.Liverpool.uk'	# crashes running lcsim
+]
---
+++ Interfaces/API/__init__.py
@@ -0,0 +1,7 @@
+# $HeadURL$
+# $Id$
+"""
+ILCDIRAC.Interfaces.API package
+
+   This contains the ILC specific API plugins for DIRAC.
+"""
