27b05236c840a2d289b75aa75f9427b922fdfb5c
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-11-17 15:54:42 +0000

    Tag v4r1p7

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __path__ = extend_path(__path__, __name__)
 
 majorVersion = 4
 minorVersion = 1
-patchLevel   = 6
+patchLevel   = 7
 preVersion   = 0
 
 version      = "v%sr%s" % ( majorVersion, minorVersion )
