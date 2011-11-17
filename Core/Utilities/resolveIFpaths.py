b1bc2bffa15b3579a38e93e6dd51cdad53096c56
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-11-17 13:58:31 +0000

    add info message in resolveIFpaths

--- Core/Utilities/resolveIFpaths.py
+++ Core/Utilities/resolveIFpaths.py
@@ -5,12 +5,17 @@ Created on Jun 28, 2010
 '''
 import os
 from DIRAC import S_OK,S_ERROR
+from DIRAC import gLogger
 def resolveIFpaths(inputfiles):
   """ Try to find out in which sub-directory are each file. In the future, should be useless if PoolXMLCatalog can be used. 
   """
   listoffiles = []
+  string = "Will look for:"
   for file in inputfiles:
     listoffiles.append(os.path.basename(file))
+    string += "%s\n"%file
+  gLogger.info(string)
+  
   listofpaths = []
   listofdirs = []
   for dir in os.listdir(os.getcwd()):
@@ -30,5 +35,5 @@ def resolveIFpaths(inputfiles):
           filefound = True
           break
   if not filefound:
-    return S_ERROR("File not found")
+    return S_ERROR("resolveIFPath: Input file not found locally")
   return S_OK(listofpaths)
