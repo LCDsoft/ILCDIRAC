e07309a808656c01bd0e9a5e8247e848b05548ea
sposs <sposs@cern.ch>
2011-11-17 15:54:58 +0000

    Release v4r1p7
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v4r1p7@45031 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/scripts/dirac-ilc-show-software.py
@@ -0,0 +1,34 @@
+'''
+Created on Dec 17, 2010
+
+@author: sposs
+'''
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+
+from DIRAC import gConfig
+
+import string
+base = '/Operations/AvailableTarBalls'
+
+platforms = gConfig.getSections(base)
+
+for platform in platforms['Value']:
+  print "For platform %s, here are the available software"%platform
+  apps = gConfig.getSections(base+"/"+platform)
+  for app in apps['Value']:
+    print "   - %s"%app
+    versions = gConfig.getSections(base+"/"+platform+"/"+app)
+    for vers in  versions['Value']:
+      print "     * %s"%vers
+      depsb = gConfig.getSections(base+"/"+platform+"/"+app+"/"+vers)
+      if len(depsb['Value']):
+        print "       Depends on"
+        deps = gConfig.getSections(base+"/"+platform+"/"+app+"/"+vers+"/Dependencies")
+        for dep in deps['Value']:
+          depversions = gConfig.getOption(base+"/"+platform+"/"+app+"/"+vers+"/Dependencies/"+dep+"/version")
+          print "         %s %s"%(dep,depversions['Value'])
+                    
+    if not len(versions['Value']):
+      print "      No version available"
+
