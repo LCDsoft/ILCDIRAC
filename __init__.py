1dbcc6d73ec27d388094f53741b324cfdc5f8d17
sposs <sposs@cern.ch>
2011-11-24 12:40:43 +0000

    Release v4r2p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v4r2p2@45341 4525493e-7705-40b1-a816-d608a930855b

---
+++ __init__.py
@@ -0,0 +1,22 @@
+# $HeadURL: svn+ssh://sposs@svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC/__init__.py $
+# $Id: __init__.py 45339 2011-11-24 12:40:22Z sposs $
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+# Define Version
+
+majorVersion = 4
+minorVersion = 2
+patchLevel = 2
+preVersion = 0
+    
+version      = "v%sr%s" % ( majorVersion, minorVersion )
+buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
+if patchLevel:
+  version = "%sp%s" % ( version, patchLevel )
+  buildVersion = "%s build %s" % ( buildVersion, patchLevel )
+if preVersion:
+  version = "%s-pre%s" % ( version, preVersion )
+  buildVersion = "%s pre %s" % ( buildVersion, preVersion )
+
