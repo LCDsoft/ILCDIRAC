d751313a261678dc59c64d584ce19de4addac142
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-11-29 20:42:57 +0000

    First try to include model dependent params in WHIZARD

--- Core/Utilities/GeneratorModels.py
+++ Core/Utilities/GeneratorModels.py
@@ -30,3 +30,97 @@ class GeneratorModels():
     if not self.models[model]:
       return S_ERROR("No file attached to model %s"%model)
     return S_OK(self.models[model])
+  
+  def getParamsForWhizard(self,model):
+    params = ''
+    if model=='sm':
+      params = """<GF type="float" value="1.16639E-5">
+<!-- Fermi constant -->
+</GF>
+<mZ type="float" value="91.1882">
+<!-- Z-boson mass -->
+</mZ>
+<mW type="float" value="80.419">
+<!-- W-boson mass -->
+</mW>
+<mH type="float" value="120">
+<!-- Higgs mass -->
+</mH>
+<alphas type="float" value="0.1178">
+<!-- Strong coupling constant alpha_s(MZ) -->
+</alphas>
+<me type="float" value="0.">
+<!-- electron mass -->
+</me>
+<mmu type="float" value="0.1066">
+<!-- muon mass -->
+</mmu>
+<mtau type="float" value="1.777">
+<!-- tau-lepton mass -->
+</mtau>
+<ms type="float" value="0.">
+<!-- s-quark mass -->
+</ms>
+<mc type="float" value="0.54">
+<!-- c-quark mass -->
+</mc>
+<mb type="float" value="2.9">
+<!-- b-quark mass -->
+</mb>
+<mtop type="float" value="174">
+<!-- t-quark mass -->
+</mtop>
+<wtop type="float" value="1.523">
+<!-- t-quark width -->
+</wtop>
+<wZ type="float" value="2.443">
+<!-- Z-boson width -->
+</wZ>
+<wW type="float" value="2.049">
+<!-- W-boson width -->
+</wW>
+<wH type="float" value="0.3605E-02">
+<!-- Higgs width -->
+</wH>
+<vckm11 type="float" value="0.97383">
+<!-- Vud -->
+</vckm11>
+<vckm12 type="float" value="0.2272">
+<!-- Vus -->
+</vckm12>
+<vckm13 type="float" value="0.00396">
+<!-- Vub -->
+</vckm13>
+<vckm21 type="float" value="-0.2271">
+<!-- Vcd -->
+</vckm21>
+<vckm22 type="float" value="0.97296">
+<!-- Vcs -->
+</vckm22>
+<vckm23 type="float" value="0.04221">
+<!-- Vcb -->
+</vckm23>
+<vckm31 type="float" value="0.00814">
+<!-- Vtd -->
+</vckm31>
+<vckm32 type="float" value="-0.04161">
+<!-- Vts -->
+</vckm32>
+<vckm33 type="float" value="0.99910">
+<!-- Vtb -->
+</vckm33>
+<khgaz type="float" value="1.000">
+<!-- anomaly Higgs coupling K factors -->
+</khgaz>
+<khgaga type="float" value="1.000">
+<!-- anomaly Higgs coupling K factors -->
+</khgaga>
+<khgg type="float" value="1.000">
+<!-- anomaly Higgs coupling K factors -->
+</khgg>
+"""
+    elif model=="":
+      params = """
+      """
+    return S_OK(params)
+      
