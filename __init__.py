446207bc745396a0c9466af90abceb787903e362
sposs <sposs@cern.ch>
2011-12-02 08:19:57 +0000

    Move to DIRAC v6r0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@45627 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -6,9 +6,9 @@ __path__ = extend_path(__path__, __name__)
 
 # Define Version
 
-majorVersion = 4
-minorVersion = 2
-patchLevel   = 7
+majorVersion = 5
+minorVersion = 0
+patchLevel   = 0
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
