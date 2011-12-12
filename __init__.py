a1193837e56ad2a7f5a4465043962c0df87f1253
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2011-12-12 21:26:36 +0000

    tag v5r0p1

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __path__ = extend_path(__path__, __name__)
 
 majorVersion = 5
 minorVersion = 0
-patchLevel   = 0
+patchLevel   = 1
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
