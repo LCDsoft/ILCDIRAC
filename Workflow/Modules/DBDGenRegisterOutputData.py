cb040ac125e348ace064acd4eabf2db69e6d802c
sposs <sposs@cern.ch>
2012-01-26 12:55:52 +0000

    Better template for meta data registration
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@46543 4525493e-7705-40b1-a816-d608a930855b

--- Workflow/Modules/DBDGenRegisterOutputData.py
+++ Workflow/Modules/DBDGenRegisterOutputData.py
@@ -9,6 +9,7 @@ from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
 
 from DIRAC import S_OK,S_ERROR,gLogger
 
+import string
 
 class DBDGenRegisterOutputData(ModuleBase):
   def __init__(self):
@@ -19,14 +20,37 @@ class DBDGenRegisterOutputData(ModuleBase):
     self.enable=True
     self.fc = FileCatalogClient()
     
+  def applicationSpecificInputs(self):
+    if self.workflow_commons.has_key('ProductionOutputData'):
+      self.prodOutputLFNs=self.workflow_commons['ProductionOutputData'].split(";")
+    else:
+      self.prodOutputLFNs = []
+    return S_OK("Paramters resolved")
+      
   def execute(self):
+    self.log.info('Initializing %s' %self.version)
+    result = self.resolveInputVariables()
+    if not result['OK']:
+      self.log.error(result['Message'])
+      return result
     
-    metadict = {}
-    path =''
+    if not self.workflowStatus['OK'] or not self.stepStatus['OK']:
+      self.log.verbose('Workflow status = %s, step status = %s' %(self.workflowStatus['OK'],self.stepStatus['OK']))
+      return S_OK('No registration of output data metadata attempted')
+
+    if len(self.prodOutputLFNs)==0:
+      self.log.info('No production data found, so no metadata registration to be done')  
+      return S_OK("No files' metadata to be registered")
     
-    res = self.fc.setMetadata(path,metadict)
-    if not res['OK']:
-      self.log.error("Could not register %s for %s"%(metadict,path))
-      return res
+    self.log.verbose("Will try to set the metadata for the following files: \n %s"%string.join(self.prodOutputLFNs,"\n"))
+    
+    for files in self.prodOutputLFNs:
+      metadict = {}
+      path = files
+    
+      res = self.fc.setMetadata(path,metadict)
+      if not res['OK']:
+        self.log.error("Could not register %s for %s"%(metadict,path))
+        return res
     
     return S_OK()
