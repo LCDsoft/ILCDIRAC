16f1978cadad1e2c598b13826bbcd8c120f6d442
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-01-26 12:35:16 +0000

    Add FinalizationStep

--- Interfaces/API/NewInterface/DBDGeneration.py
+++ Interfaces/API/NewInterface/DBDGeneration.py
@@ -7,10 +7,68 @@ Created on Jan 26, 2012
 from ILCDIRAC.Interfaces.API.NewInterface.ProductionJob import ProductionJob
 from DIRAC                                                  import S_OK, S_ERROR, gConfig
 from decimal import Decimal
+import string
 
 class DBDGeneration(ProductionJob):
   def __init__(self, script = None):
     ProductionJob.__init__(self, script)
+    
+  def addFinalization(self, uploadData=False, registerData=False, uploadLog = False, sendFailover=False):
+    """ Add finalization step
+
+    @param uploadData: Upload or not the data to the storage
+    @param uploadLog: Upload log file to storage (currently only available for admins, thus add them to OutputSandbox)
+    @param sendFailover: Send Failover requests, and declare files as processed or unused in transfDB
+    @param registerData: Register data in the file catalog
+    @todo: Do the registration only once, instead of once for each job
+
+    """
+    self.importLine = 'from ILCDIRAC.Workflow.Modules.<MODULE> import <MODULE>'
+    dataUpload = ModuleDefinition('UploadOutputData')
+    dataUpload.setDescription('Uploads the output data')
+    self._addParameter(dataUpload,'enable','bool',False,'EnableFlag')
+    body = string.replace(self.importLine,'<MODULE>','UploadOutputData')
+    dataUpload.setBody(body)
+
+    failoverRequest = ModuleDefinition('FailoverRequest')
+    failoverRequest.setDescription('Sends any failover requests')
+    self._addParameter(failoverRequest,'enable','bool',False,'EnableFlag')
+    body = string.replace(self.importLine,'<MODULE>','FailoverRequest')
+    failoverRequest.setBody(body)
+
+    registerdata = ModuleDefinition('DBDGenRegisterOutputData')
+    registerdata.setDescription('Module to add in the metadata catalog the relevant info about the files')
+    self._addParameter(registerdata,'enable','bool',False,'EnableFlag')
+    body = string.replace(self.importLine,'<MODULE>','DBDGenRegisterOutputData')
+    registerdata.setBody(body)
+
+    logUpload = ModuleDefinition('UploadLogFile')
+    logUpload.setDescription('Uploads the output log files')
+    self._addParameter(logUpload,'enable','bool',False,'EnableFlag')
+    body = string.replace(self.importLine,'<MODULE>','UploadLogFile')
+    logUpload.setBody(body)
+
+    finalization = StepDefinition('Job_Finalization')
+    finalization.addModule(dataUpload)
+    up = finalization.createModuleInstance('UploadOutputData','dataUpload')
+    up.setValue("enable",uploadData)
+
+    finalization.addModule(registerdata)
+    ro = finalization.createModuleInstance('DBDGenRegisterOutputData','DBDGenRegisterOutputData')
+    ro.setValue("enable",registerData)
+
+    finalization.addModule(logUpload)
+    ul  = finalization.createModuleInstance('UploadLogFile','logUpload')
+    ul.setValue("enable",uploadLog)
+
+    finalization.addModule(failoverRequest)
+    fr = finalization.createModuleInstance('FailoverRequest','failoverRequest')
+    fr.setValue("enable",sendFailover)
+    
+    self.workflow.addStep(finalization)
+    finalizeStep = self.workflow.createStepInstance('Job_Finalization', 'finalization')
+
+    return S_OK()
     
   def _jobSpecificParams(self,application):  
     if self.created:
