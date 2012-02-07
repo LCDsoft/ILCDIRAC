84b5521cf4750635e37c748198aaf1e746ae585a
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-02-07 13:09:02 +0000

    tag v6r4p1

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __path__ = extend_path(__path__, __name__)
 
 majorVersion = 6
 minorVersion = 4
-patchLevel   = 0
+patchLevel   = 1
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
