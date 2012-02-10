f8584fe4fb84ea0cf0f081fa8cae84e599c1b836
sposs <sposs@cern.ch>
2012-02-10 12:30:19 +0000

    Added utility to obtain the SteeringFile directory name for any given application
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@47071 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/FindSteeringFileDir.py
@@ -0,0 +1,18 @@
+'''
+Created on Feb 10, 2012
+
+@author: Stephane Poss
+'''
+
+from DIRAC import gConfig,S_OK,S_ERROR
+
+def getSteeringFileDirName(systemConfig,application,applicationVersion):
+  dir = ''
+  version = gConfig.getValue('/Operations/AvailableTarBalls/%s/%s/%s/Dependencies/SteeringFile/Version'%(systemConfig,application,applicationVersion),'')
+  if not version:
+    return S_ERROR("Could not find attached SteeringFile version")
+  TarBall = gConfig.getValue('/Operations/AvailableTarBalls/%s/SteeringFile/%s/TarBall'%(systemConfig,version),'')
+  if not TarBall:
+    return S_ERROR("Could not find tar ball for SteeringFile")
+  dir = TarBall.replace(".tgz","").replace(".tar.gz","")
+  return S_OK('')
