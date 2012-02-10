07b754a899ed185022056810eec2f96456401512
sposs <sposs@cern.ch>
2012-02-10 23:13:58 +0000

    tag v6r6p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@47176 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __path__ = extend_path(__path__, __name__)
 
 majorVersion = 6
 minorVersion = 6
-patchLevel   = 1
+patchLevel   = 2
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
