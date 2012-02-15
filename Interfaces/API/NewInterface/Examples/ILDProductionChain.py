b875e9568713572da3b05266fda3ee45f0bc5925
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-02-15 08:24:41 +0000

    add SM

--- Interfaces/API/NewInterface/Examples/ILDProductionChain.py
+++ Interfaces/API/NewInterface/Examples/ILDProductionChain.py
@@ -26,13 +26,14 @@ energy = 1400
 ##Start by defining the whizard application
 
 wh = Whizard(processlist=dirac.getProcessList())
-wh.setModel("susyStagedApproach")
+wh.setModel("sm")
+#wh.setModel("susyStagedApproach")
 pdict = {}
 pdict['process_input'] = {}
-pdict['process_input']['process_id']='stau1stau1_r'
+pdict['process_input']['process_id']='e2e2n1n1'
 pdict['process_input']['sqrts'] = energy
-pdict['process_input']['input_file'] = "LesHouches.msugra_1"
-pdict['process_input']['input_slha_format'] = 'T'
+#pdict['process_input']['input_file'] = "LesHouches.msugra_1"
+#pdict['process_input']['input_slha_format'] = 'T'
 
 pdict['integration_input'] = {}
 pdict['integration_input']['calls'] = '1  500000 10  500000  1  1500000'
@@ -42,9 +43,10 @@ pdict['simulation_input']['normalize_weight']='F'
 pdict['simulation_input']['n_events']= 1000
 pdict['simulation_input']['keep_initials'] = 'T'
 pdict['simulation_input']['events_per_file'] = 500000000
-pdict['simulation_input']['pythia_parameters'] = "PMAS(25,1)=120.; PMAS(25,2)=0.3605E-02; MSTU(22)=20 ; MSTJ(28)=2 ;PARJ(21)=0.40000;PARJ(41)=0.11000; PARJ(42)=0.52000; PARJ(81)=0.25000; PARJ(82)=1.90000; MSTJ(11)=3; PARJ(54)=-0.03100; PARJ(55)=-0.00200;PARJ(1)=0.08500; PARJ(3)=0.45000; PARJ(4)=0.02500; PARJ(2)=0.31000; PARJ(11)=0.60000; PARJ(12)=0.40000; PARJ(13)=0.72000;PARJ(14)=0.43000; PARJ(15)=0.08000; PARJ(16)=0.08000; PARJ(17)=0.17000; MSTP(3)=1;IMSS(1)=11; IMSS(21)=71; IMSS(22)=71"
+#pdict['simulation_input']['pythia_parameters'] = "PMAS(25,1)=120.; PMAS(25,2)=0.3605E-02; MSTU(22)=20 ; MSTJ(28)=2 ;PARJ(21)=0.40000;PARJ(41)=0.11000; PARJ(42)=0.52000; PARJ(81)=0.25000; PARJ(82)=1.90000; MSTJ(11)=3; PARJ(54)=-0.03100; PARJ(55)=-0.00200;PARJ(1)=0.08500; PARJ(3)=0.45000; PARJ(4)=0.02500; PARJ(2)=0.31000; PARJ(11)=0.60000; PARJ(12)=0.40000; PARJ(13)=0.72000;PARJ(14)=0.43000; PARJ(15)=0.08000; PARJ(16)=0.08000; PARJ(17)=0.17000; MSTP(3)=1;IMSS(1)=11; IMSS(21)=71; IMSS(22)=71"
+pdict['simulation_input']['pythia_parameters'] = "PMAS(25,1)=12000.; PMAS(25,2)=0.3605E-02; MSTU(22)=20 ; MSTJ(28)=2 ;PARJ(21)=0.40000;PARJ(41)=0.11000; PARJ(42)=0.52000; PARJ(81)=0.25000; PARJ(82)=1.90000; MSTJ(11)=3; PARJ(54)=-0.03100; PARJ(55)=-0.00200;PARJ(1)=0.08500; PARJ(3)=0.45000; PARJ(4)=0.02500; PARJ(2)=0.31000; PARJ(11)=0.60000; PARJ(12)=0.40000; PARJ(13)=0.72000;PARJ(14)=0.43000; PARJ(15)=0.08000; PARJ(16)=0.08000; PARJ(17)=0.17000; MSTP(3)=1;"
 pdict['parameter_input'] = {}
-#pdict['parameter_input']['mH']=120.
+pdict['parameter_input']['mH']=12000.
 pdict['beam_input_1'] = {}
 pdict['beam_input_1']['particle_name']='e1'
 pdict['beam_input_1']['polarization'] = "0.0 0.0"
