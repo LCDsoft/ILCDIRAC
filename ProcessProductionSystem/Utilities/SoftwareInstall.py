0aa5e25ba7d61a038bd938d28d989fb7d58c8f10
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-02-17 16:36:31 +0000

    Added specific application for software management

---
+++ ProcessProductionSystem/Utilities/SoftwareInstall.py
@@ -0,0 +1,12 @@
+'''
+Created on Feb 17, 2012
+
+@author: Stephane Poss
+'''
+
+from ILCDIRAC.Interfaces.NewInterface.Aopplication import Application
+
+class SoftwareInstall(Application):
+  def __init__(self):
+    Application.__init__(self)
+    
---
+++ ProcessProductionSystem/Utilities/__init__.py
@@ -0,0 +1,5 @@
+'''
+Created on Feb 17, 2012
+
+@author: Stephane Poss
+'''
