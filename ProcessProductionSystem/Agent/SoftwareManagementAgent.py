59f7dece4e4302616a39a720a35a74c6e58ae04c
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-02-17 16:45:23 +0000

    SoftwareManagementAgent update

--- ProcessProductionSystem/Agent/SoftwareManagementAgent.py
+++ ProcessProductionSystem/Agent/SoftwareManagementAgent.py
@@ -7,8 +7,10 @@ __RCSID__ = "$ Id: $"
 
 from DIRAC                                                                import S_OK, S_ERROR, gConfig, gLogger, gMonitor
 from DIRAC.Core.Base.AgentModule                                          import AgentModule
-
+from DIRAC.Core.DISET.RPCClient                                           import RPCClient
+from DIRAC.Interfaces.API.DiracAdmin import DiracAdmin
 from ILCDIRAC.ProcessProductionSystem.Client                              import ProcessProdClient
+from ILCDIRAC.ProcessProductionSystem.Utilities.SoftwareInstall import SoftwareInstall
 
 from ILCDIRAC.Interfaces.API.NewInterface.UserJob import UserJob
 from ILCDIRAC.Interfaces.API.DiracILC import DiracILC
@@ -24,13 +26,25 @@ class SoftwareManagementAgent(AgentModule):
     gMonitor.registerActivity("Iteration","Agent Loops",AGENT_NAME,"Loops/min",gMonitor.OP_SUM)
     self.ppc = ProcessProdClient()
     self.dirac = DiracILC()
+    self.diracadmin = DiracAdmin()
     return S_OK()
 
   ##############################################################################
   def execute(self):
-    ##First we need to set the site statuses according to what is in the CS
-    self.ppc.changeSiteStatus()
-    
+    """  First we update the site list and banned site
+    """
+    sites= self.diracadmin.getSiteMask()['Value']
+    for site in sites:
+      res =self.ppc.changeSiteStatus({'SiteName':site,'Status':'OK'})
+      if not res['OK']:
+        self.log.error('Cannot add or update site %s'%site)
+        
+    banned_sites = self.diracadmin.getBannedSites()
+    for banned_site in banned_sites:
+      self.ppc.changeSiteStatus({'SiteName':banned_site,'Status':'Banned'})
+      if not res['OK']:
+        self.log.error('Cannot mark as banned site %s'%site)    
+        
     ##Then we need to get new installation tasks
     res = self.ppc.getInstallSoftwareTask()
     if not res['OK']:
@@ -40,11 +54,14 @@ class SoftwareManagementAgent(AgentModule):
       for site in softdict['Site']:
         j = UserJob()
         j.setSystemConfig(softdict['Platform'])
-        j.dontPrompMe()
+        j.dontPromptMe()
         j.setDestination(site)
-        
+        j._addSoftware(softdict['AppName'],softdict['AppVersion'])
         #Add the application here somehow.
-        
+        res  = j.append(SoftwareInstall())
+        if not res['OK']:
+          self.log.error(res['Message'])
+          continue
         res = self.dirac.submit(j)
         if not res['OK']:
           self.log.error('Could not create the job')
