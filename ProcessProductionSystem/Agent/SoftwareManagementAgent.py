bd81060db144c500afdee3bc39ed88908a361d4c
sposs <sposs@cern.ch>
2012-02-18 12:19:39 +0000

    Better dict
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@47463 4525493e-7705-40b1-a816-d608a930855b

--- ProcessProductionSystem/Agent/SoftwareManagementAgent.py
+++ ProcessProductionSystem/Agent/SoftwareManagementAgent.py
@@ -8,8 +8,10 @@ __RCSID__ = "$ Id: $"
 from DIRAC                                                                import S_OK, S_ERROR, gConfig, gLogger, gMonitor
 from DIRAC.Core.Base.AgentModule                                          import AgentModule
 from DIRAC.Core.DISET.RPCClient                                           import RPCClient
+from DIRAC.Core.Security.ProxyInfo                                        import getProxyInfo
+
 from DIRAC.Interfaces.API.DiracAdmin import DiracAdmin
-from ILCDIRAC.ProcessProductionSystem.Client                              import ProcessProdClient
+from ILCDIRAC.ProcessProductionSystem.Client.ProcessProdClient            import ProcessProdClient
 from ILCDIRAC.ProcessProductionSystem.Utilities.SoftwareInstall import SoftwareInstall
 
 from ILCDIRAC.Interfaces.API.NewInterface.UserJob import UserJob
@@ -27,19 +29,30 @@ class SoftwareManagementAgent(AgentModule):
     self.ppc = ProcessProdClient()
     self.dirac = DiracILC()
     self.diracadmin = DiracAdmin()
+    self.am_setOption( 'shifterProxy', 'Admin' )
+    
     return S_OK()
 
   ##############################################################################
   def execute(self):
     """  First we update the site list and banned site
     """
+    res = getProxyInfo(False,False)
+    if not res['OK']:
+      self.log.error("submitTasks: Failed to determine credentials for submission",res['Message'])
+      return res
+    proxyInfo = res['Value']
+    owner = proxyInfo['username']
+    ownerGroup = proxyInfo['group']
+    self.log.info("submitTasks: Jobs will be submitted with the credentials %s:%s" % (owner,ownerGroup))    
+    
     sites= self.diracadmin.getSiteMask()['Value']
     for site in sites:
       res =self.ppc.changeSiteStatus({'SiteName':site,'Status':'OK'})
       if not res['OK']:
         self.log.error('Cannot add or update site %s'%site)
         
-    banned_sites = self.diracadmin.getBannedSites()
+    banned_sites = self.diracadmin.getBannedSites()['Value']
     for banned_site in banned_sites:
       self.ppc.changeSiteStatus({'SiteName':banned_site,'Status':'Banned'})
       if not res['OK']:
@@ -51,11 +64,13 @@ class SoftwareManagementAgent(AgentModule):
       self.log.error('Failed to obtain task')
     task_dict = res['Value']
     for softdict in task_dict.values():
-      for site in softdict['Site']:
+      for site in softdict['Sites']:
         j = UserJob()
         j.setSystemConfig(softdict['Platform'])
         j.dontPromptMe()
         j.setDestination(site)
+        j.setJobGroup("Installation")
+        j.setName('install_%s'%site)
         j._addSoftware(softdict['AppName'],softdict['AppVersion'])
         #Add the application here somehow.
         res  = j.append(SoftwareInstall())
@@ -67,13 +82,15 @@ class SoftwareManagementAgent(AgentModule):
           self.log.error('Could not create the job')
           continue
         jobdict = {}
-        jobdict.update(softdict)
+        jobdict['AppName'] = softdict['AppName']
+        jobdict['AppVersion'] = softdict['AppVersion']
+        jobdict['Platform'] = softdict['Platform']
         jobdict['JobID'] = res['Value']
         jobdict['Status'] = 'Waiting'
-        
+        jobdict['Site'] = site
         res = self.ppc.addOrUpdateJob(jobdict)
         if not res['OK']:
-          self.log.error('Could not add job:%s'%res['Message'])
+          self.log.error('Could not add job: %s'%res['Message'])
     
     return S_OK()
   
