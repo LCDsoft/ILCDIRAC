d2fdd66b4e810311722ed3b43b050eaf96703afa
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-02-20 10:42:58 +0000

    typo in looking for steeringfiles

--- Core/Utilities/FindSteeringFileDir.py
+++ Core/Utilities/FindSteeringFileDir.py
@@ -8,10 +8,10 @@ from DIRAC import gConfig,S_OK,S_ERROR
 
 def getSteeringFileDirName(systemConfig,application,applicationVersion):
   dir = ''
-  version = gConfig.getValue('/Operations/AvailableTarBalls/%s/%s/%s/Dependencies/steeringfile/version'%(systemConfig,application,applicationVersion),'')
+  version = gConfig.getValue('/Operations/AvailableTarBalls/%s/%s/%s/Dependencies/steeringfiles/version'%(systemConfig,application,applicationVersion),'')
   if not version:
     return S_ERROR("Could not find attached SteeringFile version")
-  TarBall = gConfig.getValue('/Operations/AvailableTarBalls/%s/steeringfile/%s/TarBall'%(systemConfig,version),'')
+  TarBall = gConfig.getValue('/Operations/AvailableTarBalls/%s/steeringfiles/%s/TarBall'%(systemConfig,version),'')
   if not TarBall:
     return S_ERROR("Could not find tar ball for SteeringFile")
   dir = TarBall.replace(".tgz","").replace(".tar.gz","")
