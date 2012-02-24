867d7f41634377e31b85f68df0b172ce7d569f29
sposs <sposs@cern.ch>
2012-02-24 06:43:51 +0000

    Release v8r0p5
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v8r0p5@47753 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/scripts/ilc-proxy-init.py
@@ -0,0 +1,22 @@
+# $HeadURL$
+# $Id$
+#!/usr/bin/env python
+########################################################################
+# File :   ilc-proxy-init.py
+# Author : Adrian Casajus, adapted for ILC by S POSS
+########################################################################
+__RCSID__   = "$Id$"
+__VERSION__ = "$Revision: 1. $"
+
+
+import sys
+
+print 'This command is outdated, please use proxy-init instead'
+sys.exit(2)
+
+
+
+
+
+
+
