c9cf7d003fe06890d20b4fc6a804c28e586ecc2f
sposs <sposs@cern.ch>
2012-02-24 13:37:27 +0000

    Release v8r1p3
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v8r1p3@47869 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/resolveOFnames.py
@@ -0,0 +1,27 @@
+'''
+Created on Jul 30, 2010
+
+@author: sposs
+'''
+
+def getProdFilename(filename,prodID,jobID):
+  """ Build the output file names based of local job property.
+  @param filename: File name before change
+  @type filename: string
+  @param prodID: Production ID
+  @type prodID: int
+  @param jobID: Job ID
+  @type jobID: int
+  @return: the modified file name
+  """
+  outfile = ""
+  if filename.count(".slcio"):
+    name = filename.split(".slcio")
+    outfile = name[0]+"_"+str(prodID)+"_"+str(jobID)+".slcio"
+  elif filename.count(".stdhep"):
+    name = filename.split(".stdhep")
+    outfile = name[0]+"_"+str(prodID)+"_"+str(jobID)+".stdhep"
+  elif filename.count(".root"):
+    name = filename.split(".root")
+    outfile = name[0]+"_"+str(prodID)+"_"+str(jobID)+".root"
+  return outfile
