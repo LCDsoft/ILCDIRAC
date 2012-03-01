ab40d95cc2a90a9552bfaf3e3aa1ca4353748c0d
sposs <sposs@cern.ch>
2012-03-01 06:45:34 +0000

    tag v8r1p12
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@48119 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __path__ = extend_path(__path__, __name__)
 
 majorVersion = 8
 minorVersion = 1
-patchLevel   = 11
+patchLevel   = 12
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
