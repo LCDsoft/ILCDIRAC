203e1b308fce010f11c227d55b66c443a372cc01
sposs <sposs@cern.ch>
2012-03-01 13:55:10 +0000

    Release v8r1p15
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v8r1p15@48198 4525493e-7705-40b1-a816-d608a930855b

---
+++ __init__.py
@@ -0,0 +1,22 @@
+# $HeadURL: svn+ssh://sposs@svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC/__init__.py $
+# $Id: __init__.py 48196 2012-03-01 13:54:47Z sposs $
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+# Define Version
+
+majorVersion = 8
+minorVersion = 1
+patchLevel = 15
+preVersion = 0
+    
+version      = "v%sr%s" % ( majorVersion, minorVersion )
+buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
+if patchLevel:
+  version = "%sp%s" % ( version, patchLevel )
+  buildVersion = "%s build %s" % ( buildVersion, patchLevel )
+if preVersion:
+  version = "%s-pre%s" % ( version, preVersion )
+  buildVersion = "%s pre %s" % ( buildVersion, preVersion )
+
