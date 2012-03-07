2781c9872714cce4fc50213d2b756c04c9bc2f4c
sposs <sposs@cern.ch>
2012-03-07 15:32:04 +0000

    add method to LCSIM to tell it to deal properly with SLICPandora
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@48291 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/API/NewInterface/ProductionJob.py
+++ Interfaces/API/NewInterface/ProductionJob.py
@@ -680,7 +680,7 @@ class ProductionJob(Job):
     
     path = self.basepath  
     ###Need to resolve file names and paths
-    if hasattr(application,"setOutputRecFile"):
+    if hasattr(application,"setOutputRecFile") and not application.willBeCut:
       path = self.basepath+self.machine+energypath+self.evttypepath+application.detectortype+"/REC"
       self.finalMetaDict[self.basepath+self.machine+energypath+self.evttypepath]= {"EvtType":self.evttype}
       self.finalMetaDict[self.basepath+self.machine+energypath+self.evttypepath+application.detectortype] = {"DetectorType":application.detectortype}
