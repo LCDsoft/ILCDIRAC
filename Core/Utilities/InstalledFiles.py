0c19949530112b8640a74365775c357864785b2b
sposs <sposs@cern.ch>
2012-03-19 16:37:33 +0000

    added the new reco overlay files
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@49093 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/InstalledFiles.py
+++ Core/Utilities/InstalledFiles.py
@@ -8,9 +8,11 @@ from DIRAC import S_OK,S_ERROR
 def Exists(file):
   files = ["defaultClicCrossingAngle.mac","clic_ild_cdr500.steer",
            "clic_ild_cdr.steer","clic_cdr_prePandora.lcsim",
-           "clic_cdr_postPandora.lcsim","clic_cdr_prePandoraOverlay.lcsim",
+           "clic_cdr_postPandora.lcsim","clic_cdr_prePandoraOverlay.lcsim","clic_cdr_prePandoraOverlay_1400.0.lcsim",
+           "clic_cdr_prePandoraOverlay_3000.0.lcsim",
            "clic_cdr_postPandoraOverlay.lcsim","clic_ild_cdr.gear",
-           "clic_ild_cdr500.gear","clic_ild_cdr_steering_overlay.xml",
+           "clic_ild_cdr500.gear","clic_ild_cdr_steering_overlay.xml","clic_ild_cdr_steering_overlay_3000.0.xml",
+           "clic_ild_cdr_steering_overlay_1400.0.xml",
            "clic_ild_cdr500_steering_overlay.xml","clic_ild_cdr_steering.xml",
            "clic_ild_cdr500_steering.xml","GearOutput.xml",'cuts_e1e1ff_500gev.txt',
            "cuts_e2e2ff_500gev.txt",'cuts_qq_nunu_1400.txt','cuts_e3e3nn_1400.txt',
