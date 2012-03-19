7ce1f76ffcfdcc74e5e1374bf1eee2d97a451c9c
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-03-19 10:24:39 +0000

    tag v8r2p18

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __path__ = extend_path(__path__, __name__)
 
 majorVersion = 8
 minorVersion = 2
-patchLevel   = 17
+patchLevel   = 18
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
