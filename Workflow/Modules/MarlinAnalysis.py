d957348ed412460dbcced16298639e71eaeb42e9
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-03-27 11:58:35 +0000

    Don't check the length of the list, check the list itself.

--- Workflow/Modules/MarlinAnalysis.py
+++ Workflow/Modules/MarlinAnalysis.py
@@ -122,7 +122,7 @@ class MarlinAnalysis(ModuleBase):
           self.InputFile = [getProdFilename(self.InputFile,int(self.workflow_commons["PRODUCTION_ID"]),
                                               int(self.workflow_commons["JOB_ID"]))]
           
-    if len(self.InputData):
+    if self.InputData:
       if not self.workflow_commons.has_key("Luminosity") or not self.workflow_commons.has_key("NbOfEvents"):
         res = getNumberOfevents(self.InputData)
         if res.has_key("nbevts") and not self.workflow_commons.has_key("Luminosity") :
