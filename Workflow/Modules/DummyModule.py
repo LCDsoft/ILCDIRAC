cf4d0e7a866f629d18ee2f45249d3121f59a2c53
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-03-27 11:59:42 +0000

    Get the number of events from any input file

--- Workflow/Modules/DummyModule.py
+++ Workflow/Modules/DummyModule.py
@@ -12,6 +12,7 @@ __RCSID__ = "$Id: $"
 
 from ILCDIRAC.Workflow.Modules.ModuleBase                 import ModuleBase
 from DIRAC                                                import S_OK, S_ERROR, gLogger
+from ILCDIRAC.Core.Utilities.InputFilesUtilities             import getNumberOfevents
 
 class DummyModule(ModuleBase):
   """ Dummy module used to check Workflow Parameters (Parametric jobs check)
@@ -22,8 +23,17 @@ class DummyModule(ModuleBase):
     self.log = gLogger.getSubLogger( "DummyModuleChecking" )
     
   def applicationSpecificInputs(self):
+    
+    if self.InputData:
+      if not self.workflow_commons.has_key("Luminosity") or not self.workflow_commons.has_key("NbOfEvents"):
+        res = getNumberOfevents(self.InputData)
+        if res.has_key("nbevts") and not self.workflow_commons.has_key("Luminosity") :
+          self.workflow_commons["NbOfEvents"]=res["nbevts"]
+        if res.has_key("lumi") and not self.workflow_commons.has_key("NbOfEvents"):
+          self.workflow_commons["Luminosity"]=res["lumi"]
     for key,val in self.workflow_commons.items():
       self.log.info("%s=%s" % (key, val))
+      
     return S_OK()
 
   def execute(self):
