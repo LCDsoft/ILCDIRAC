4e10ca60ddcedcf142f5088b623793ac7a7f4e21
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-04-26 08:43:06 +0000

    Added steering file for 350GeV

--- Core/Utilities/InstalledFiles.py
+++ Core/Utilities/InstalledFiles.py
@@ -13,7 +13,7 @@ def Exists(file):
            "clic_cdr_postPandoraOverlay.lcsim","clic_ild_cdr.gear",
            "clic_ild_cdr500.gear","clic_ild_cdr_steering_overlay.xml","clic_ild_cdr_steering_overlay_3000.0.xml",
            "clic_ild_cdr_steering_overlay_1400.0.xml",
-           "clic_ild_cdr500_steering_overlay.xml","clic_ild_cdr_steering.xml",
+           "clic_ild_cdr500_steering_overlay.xml","clic_ild_cdr500_steering_overlay_350.0.xml","clic_ild_cdr_steering.xml",
            "clic_ild_cdr500_steering.xml","GearOutput.xml",'cuts_e1e1ff_500gev.txt',
            "cuts_e2e2ff_500gev.txt",'cuts_qq_nunu_1400.txt','cuts_e3e3nn_1400.txt',
            "cuts_e3e3_1400.txt","cuts_e1e1e3e3_o_1400.txt","cuts_aa_e3e3_o_1400.txt",
