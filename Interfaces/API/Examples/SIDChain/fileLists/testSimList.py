9816428f7040aff2770c6b2323504e75d69d0d7f
sposs <sposs@cern.ch>
2012-05-16 11:56:10 +0000

    Release v10r0p7
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v10r0p7@52588 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/API/Examples/SIDChain/fileLists/testSimList.py
@@ -0,0 +1,4 @@
+lfnlist = [
+"LFN:/ilc/prod/clic/3tev/ee_h_mumu/SID/SIM/00000191/000/ee_h_mumu_sim_191_2.slcio",
+"LFN:/ilc/prod/clic/3tev/ee_h_mumu/SID/SIM/00000191/000/ee_h_mumu_sim_191_4.slcio"
+]
