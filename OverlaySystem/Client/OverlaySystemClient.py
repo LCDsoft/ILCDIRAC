03454def6d7e62e4a1b2659506c7de0d521098b2
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-06-19 11:40:04 +0000

    more pylint compliant

--- OverlaySystem/Client/OverlaySystemClient.py
+++ OverlaySystem/Client/OverlaySystemClient.py
@@ -4,7 +4,6 @@ Created on Jul 25, 2011
 @author: Stephane Poss
 '''
 from DIRAC.Core.Base.Client                               import Client
-from DIRAC                                                import S_OK, S_ERROR,gLogger,gConfig
 
 class OverlaySystemClient (Client):
   def __init__(self):
