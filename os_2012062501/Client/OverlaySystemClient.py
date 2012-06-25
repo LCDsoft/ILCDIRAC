1dbfa8b498d1a8aca7793bad3395cae94e96f1d5
sposs <sposs@cern.ch>
2012-06-25 17:19:10 +0000

    tag v10r1p1
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/OverlaySystem@53751 4525493e-7705-40b1-a816-d608a930855b

---
+++ os_2012062501/Client/OverlaySystemClient.py
@@ -0,0 +1,13 @@
+'''
+Created on Jul 25, 2011
+
+@author: Stephane Poss
+'''
+from DIRAC.Core.Base.Client                               import Client
+
+class OverlaySystemClient (Client):
+  """ Client of the OverlaySystemHandler. Used from the ResetCounter Agent
+  """
+  def __init__(self):
+    self.setServer("Overlay/Overlay")
+    
