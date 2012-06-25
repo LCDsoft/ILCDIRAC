d89027c09a4aa4f4191700863eba6a67a2fbca3f
sposs <sposs@cern.ch>
2012-06-25 12:57:23 +0000

    add doc strings
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@53659 4525493e-7705-40b1-a816-d608a930855b

--- OverlaySystem/Service/OverlayHandler.py
+++ OverlaySystem/Service/OverlayHandler.py
@@ -17,7 +17,7 @@ from types import StringTypes, DictType
 overlayDB = False
 
 def initializeOverlayHandler( serviceInfo ):
-  """ Clobal initialize for the Overlay service handler
+  """ Global initialize for the Overlay service handler
   """
   global overlayDB
   overlayDB = OverlayDB()
@@ -28,21 +28,33 @@ class OverlayHandler(RequestHandler):
   """
   types_canRun = [StringTypes]
   def export_canRun(self, site):
+    """ Check if current job can access the data
+    """
     return overlayDB.canRun(site)
 
   types_jobDone = [StringTypes]
   def export_jobDone(self, site):
+    """ report that a given job is done downloading the 
+    files at a given site
+    """
     return overlayDB.jobDone(site)
   
   types_getJobsAtSite =  [StringTypes]
   def export_getJobsAtSite(self, site):
+    """ Get the jobs running at a given site
+    """
     return overlayDB.getJobsAtSite(site)
   
   types_getSites = []
   def export_getSites(self):
+    """ Get all sites registered
+    """
     return overlayDB.getSites()
   
   types_setJobsAtSites = [ DictType ]
   def export_setJobsAtSites(self, sitedict):
+    """ Set the number of jobs running at each site: 
+    called from the ResetCounter agent
+    """
     return overlayDB.setJobsAtSites(sitedict)
   
