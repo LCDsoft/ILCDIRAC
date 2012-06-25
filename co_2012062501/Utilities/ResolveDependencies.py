2de11fb7d77716e2017f731db0e716a3800ca876
sposs <sposs@cern.ch>
2012-06-25 17:17:49 +0000

    tag v10r1p1
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/Core@53739 4525493e-7705-40b1-a816-d608a930855b

---
+++ co_2012062501/Utilities/ResolveDependencies.py
@@ -0,0 +1,67 @@
+'''
+ILCDIRAC.Core.Utilities.ResolveDependencies
+
+Set of functions used to resolve the applications' dependencies, looking into the CS
+
+Works recursively
+
+@since: Apr 26, 2010
+
+@author: Stephane Poss
+'''
+
+from DIRAC import gConfig, gLogger
+
+def resolveDeps(sysconfig, appli, appversion):
+  """ Resolve the dependencies
+  
+  @param sysconfig: system configuration
+  @type sysconfig: string
+  @param appli: application name
+  @type appli: string
+  @param appversion: application version
+  @type appversion: string
+  
+  @return: array of dictionaries
+  """
+  deps = gConfig.getSections('/Operations/AvailableTarBalls/%s/%s/%s/Dependencies' % (sysconfig, appli, 
+                                                                                      appversion), '')
+  depsarray = []
+  if deps['OK']:
+    for dep in deps['Value']:
+      vers = gConfig.getOption('/Operations/AvailableTarBalls/%s/%s/%s/Dependencies/%s/version' % (sysconfig, appli, 
+                                                                                                   appversion, dep), '')
+      depvers = ''
+      if vers['OK']:
+        depvers = vers['Value']
+      else:
+        gLogger.error("Retrieving dependency version for %s failed, skipping to next !" % (dep))
+        continue
+      gLogger.verbose("Found dependency %s %s" % (dep, depvers))
+      depdict = {}
+      depdict["app"] = dep
+      depdict["version"] = depvers
+      depsarray.append(depdict)
+      ##resolve recursive dependencies
+      depsofdeps = resolveDeps(sysconfig, dep, depvers)
+      depsarray.extend(depsofdeps)
+  else:
+    gLogger.verbose("Could not find any dependency for %s %s, ignoring" % (appli, appversion))
+  return depsarray
+
+def resolveDepsTar(sysconfig, appli, appversion):
+  """ Return the dependency tar ball name, if available
+  
+  Uses same parameters as L{resolveDeps}.
+  @return: array of strings
+  """
+  deparray = resolveDeps(sysconfig, appli, appversion)
+  depsarray = []
+  for dep in deparray:
+    dep_tar = gConfig.getOption('/Operations/AvailableTarBalls/%s/%s/%s/TarBall' % (sysconfig, dep["app"], 
+                                                                                    dep["version"]), '')
+    if dep_tar['OK']:
+      depsarray.append(dep_tar["Value"])
+    else:
+      gLogger.error("Dependency %s version %s is not defined in CS, please check !" % (dep["app"], dep["version"]))         
+  return depsarray
---
+++ co_2012062501/Utilities/resolveIFpaths.py
@@ -0,0 +1,44 @@
+'''
+Created on Jun 28, 2010
+
+@author: sposs
+'''
+import os
+from DIRAC import S_OK, S_ERROR
+from DIRAC import gLogger
+
+def resolveIFpaths(inputfiles):
+  """ Try to find out in which sub-directory are each file. In the future, should be useless if PoolXMLCatalog can be used. 
+  """
+  listoffiles = []
+  string = "Will look for:"
+  for myfile in inputfiles:
+    if not len(myfile):
+      continue
+    listoffiles.append(os.path.basename(myfile))
+    string += "%s\n" % os.path.basename(myfile)
+  gLogger.info(string)
+
+  listofpaths = []
+  listofdirs = []
+  for mydir in os.listdir(os.getcwd()):
+    if os.path.isdir(mydir):
+      listofdirs.append(mydir)
+  filesnotfound = []
+  for infile in listoffiles:
+    filefound = False
+    if os.path.exists(infile):
+      listofpaths.append(os.getcwd() + os.sep + infile)
+      filefound = True
+    else:
+      for mydir in listofdirs:
+        if os.path.exists(os.getcwd() + os.sep + mydir + os.sep + infile):
+          listofpaths.append(os.getcwd() + os.sep + mydir + os.sep + infile)
+          listofdirs.remove(mydir)
+          filefound = True
+          break
+    if not filefound:
+      filesnotfound.append(infile)
+  if len(filesnotfound):
+    return S_ERROR("resolveIFPath: Input file(s) '%s' not found locally" % (", ".join(filesnotfound)))
+  return S_OK(listofpaths)
