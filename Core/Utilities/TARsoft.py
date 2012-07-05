141e9befa9a81f5bd0dcdfe1166e8a600276ccbc
sposs <sposs@cern.ch>
2012-07-05 16:43:54 +0000

    Release v10r2p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v10r2p0@54219 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/TARsoft.py
@@ -0,0 +1,247 @@
+'''
+Function to download and untar the applications, called from CombinedSoftwareInstallation
+
+Also installs all dependencies for the applications
+
+@since:  Apr 7, 2010
+
+@author: Stephane Poss
+'''
+from DIRAC import gConfig, gLogger, S_OK, S_ERROR
+from ILCDIRAC.Core.Utilities.ResolveDependencies      import resolveDeps
+from ILCDIRAC.Core.Utilities.PrepareLibs              import removeLibc
+from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+import os, urllib, tarfile, subprocess, shutil
+
+def TARinstall(app, config, area):
+  """ For the specified app, install all dependencies
+  """
+  appName    = app[0].lower()
+  appVersion = app[1]
+  deps = resolveDeps(config, appName, appVersion)
+  for dep in deps:
+    depapp = []
+    depapp.append(dep["app"])
+    depapp.append(dep["version"])
+    gLogger.info("Installing dependency %s %s" % (dep["app"], dep["version"]))
+    res = install(depapp, config, area)
+    if not res['OK']:
+      gLogger.error("Could not install dependency %s %s" % (dep["app"], dep["version"]))
+      return S_ERROR('Failed to install software')
+  res = install(app, config, area)
+  return res
+
+def install(app, config, area):
+  """ Actually install the applications. Set the environment for some of them.
+  """
+  curdir = os.getcwd()
+
+  appName    = app[0]
+  appVersion = app[1]
+  appName = appName.lower()
+  app_tar = gConfig.getValue('/Operations/AvailableTarBalls/%s/%s/%s/TarBall' % (config, appName, appVersion), '')
+  overwrite = gConfig.getValue('/Operations/AvailableTarBalls/%s/%s/%s/Overwrite' % (config, appName, appVersion), False)
+
+  if not app_tar:
+    gLogger.error('Could not find tar ball for %s %s'%(appName, appVersion))
+    return S_ERROR('Could not find tar ball for %s %s'%(appName, appVersion))
+  TarBallURL = gConfig.getValue('/Operations/AvailableTarBalls/%s/%s/TarBallURL' % (config, appName), '')
+  if not TarBallURL:
+    gLogger.error('Could not find TarBallURL in CS for %s %s' % (appName, appVersion))
+    return S_ERROR('Could not find TarBallURL in CS')
+
+  #Check if folder is already there:
+  folder_name = app_tar.replace(".tgz", "").replace(".tar.gz", "")
+  if appName == "slic":
+    folder_name = "%s%s" % (appName, appVersion)
+  appli_exists = False
+  os.chdir(area)
+
+  if os.path.exists(folder_name):
+    # and not appName =="slic":
+    appli_exists = True
+    if not overwrite:
+      gLogger.info("Folder or file %s found in %s, skipping install !" % (folder_name, area))
+    else:
+      gLogger.info("Overwriting %s found in %s" % (folder_name, area))
+      appli_exists = False
+      if CanWrite(area):
+        gLogger.info("First we delete existing version %s" % folder_name)
+        if os.path.exists(folder_name):
+          if os.path.isdir(folder_name):
+            try:
+              shutil.rmtree(folder_name)
+            except Exception, x:
+              gLogger.error("Failed deleting %s because %s,%s" % (folder_name, Exception, str(x)))
+          else:
+            try:
+              os.remove(folder_name)
+            except Exception, x:
+              gLogger.error("Failed deleting %s because %s,%s"%(folder_name, Exception, str(x)))
+        if os.path.exists(folder_name):
+          gLogger.error("Oh Oh, something was not right, the directory %s is still here" % folder_name)  
+    #os.chdir(curdir)
+    #return S_OK()
+  if not appli_exists:
+    if not CanWrite(area):
+      os.chdir(curdir)
+      return S_ERROR("Not allowed to write in %s" % area)
+  #downloading file from url, but don't do if file is already there.
+  app_tar_base = os.path.basename(app_tar)
+  if not os.path.exists("%s/%s"%(os.getcwd(), app_tar_base)) and not appli_exists:
+    if TarBallURL.find("http://")>-1:
+      try :
+        gLogger.debug("Downloading software", '%s_%s' % (appName, appVersion))
+        #Copy the file locally, don't try to read from remote, soooo slow
+        #Use string conversion %s%s to set the address, makes the system more stable
+        tarball, headers = urllib.urlretrieve("%s%s" % (TarBallURL, app_tar), app_tar_base)
+      except:
+        gLogger.exception()
+        os.chdir(curdir)
+        return S_ERROR('Exception during url retrieve')
+    else:
+      rm = ReplicaManager()
+      res = rm.getFile("%s%s" % (TarBallURL, app_tar))
+      if not res['OK']:
+        os.chdir(curdir)
+        return res
+
+  if not os.path.exists("%s/%s" % (os.getcwd(), app_tar_base)) and not appli_exists:
+    gLogger.error('Failed to download software','%s_%s' % (appName, appVersion))
+    os.chdir(curdir)
+    return S_ERROR('Failed to download software')
+
+  if not appli_exists:    
+    if tarfile.is_tarfile(app_tar_base):##needed because LCSIM is jar file
+      app_tar_to_untar = tarfile.open(app_tar_base)
+      try:
+        app_tar_to_untar.extractall()
+      except Exception, e:
+        gLogger.error("Could not extract tar ball %s because of %s, cannot continue !" % (app_tar_base, e))
+        os.chdir(curdir)
+        return S_ERROR("Could not extract tar ball %s because of %s, cannot continue !"%(app_tar_base, e))
+      if appName == "slic":
+        slicname = "%s%s" % (appName, appVersion)
+        members = app_tar_to_untar.getmembers()
+        fileexample = members[0].name
+        basefolder = fileexample.split("/")[0]
+        try:
+          os.rename(basefolder, slicname)
+        except:
+          os.chdir(curdir)
+          return S_ERROR("Could not rename slic directory")
+    try:
+      dircontent = os.listdir(folder_name)
+      if not len(dircontent):
+        os.chdir(curdir)
+        return S_ERROR("Folder %s is empty, considering install as failed" % folder_name)
+    except:
+      pass
+    
+  ### Set env variables  
+  basefolder = folder_name
+  removeLibc(os.path.join(os.getcwd(), basefolder) + "/LDLibs")
+  if os.path.isdir(os.path.join(os.getcwd(), basefolder) + "/lib"):
+    removeLibc(os.path.join(os.getcwd(), basefolder) + "/lib")
+  if appName == "slic":
+    os.environ['SLIC_DIR'] = basefolder
+    slicv = ''
+    lcddv = ''
+    xercesv = ''
+    try:
+      slicv = os.listdir(os.path.join(basefolder, 'packages/slic/'))[0]
+      lcddv = os.listdir(os.path.join(basefolder, 'packages/lcdd/'))[0]
+      if os.path.exists(os.path.join(basefolder, 'packages/xerces/')):
+        xercesv = os.listdir(os.path.join(basefolder, 'packages/xerces/'))[0]
+    except:
+      os.chdir(curdir)
+      return S_ERROR("Could not resolve slic env variables, folder content does not match usual pattern")
+    #for mem in members:
+    #  if mem.name.find('/packages/slic/')>0:
+    #    slicv = mem.name.split("/")[3]
+    #  if mem.name.find('/packages/lcdd/')>0:
+    #    lcddv = mem.name.split("/")[3]
+    #  if mem.name.find('/packages/xerces/')>0:
+    #    xercesv = mem.name.split("/")[3]
+    if slicv:
+      os.environ['SLIC_VERSION'] = slicv
+    if xercesv:
+      os.environ['XERCES_VERSION'] = xercesv
+    if lcddv:
+      os.environ['LCDD_VERSION'] = lcddv
+  elif appName == "root":
+    #members = app_tar_to_untar.getmembers()
+    #fileexample = members[0].name
+    #fileexample.split("/")[0]
+    os.environ['ROOTSYS'] = os.path.join(os.getcwd(), basefolder)
+    if os.environ.has_key('LD_LIBRARY_PATH'):
+      os.environ['LD_LIBRARY_PATH'] = os.environ['ROOTSYS'] + "/lib:" + os.environ['LD_LIBRARY_PATH']
+    else:
+      os.environ['LD_LIBRARY_PATH'] = os.environ['ROOTSYS'] + "/lib"
+    os.environ['PATH'] = os.environ['ROOTSYS'] + "/bin:" + os.environ['PATH']
+    os.environ['PYTHONPATH'] = os.environ['ROOTSYS'] + "/lib:" + os.environ["PYTHONPATH"]
+  elif appName == 'java':
+    os.environ['PATH'] = os.path.join(os.getcwd(), basefolder) + "/Executable:" + os.environ['PATH']
+    if os.environ.has_key('LD_LIBRARY_PATH'):
+      os.environ['LD_LIBRARY_PATH'] = os.path.join(os.getcwd(), basefolder) + "/LDLibs:" + os.environ['LD_LIBRARY_PATH']
+    else:
+      os.environ['LD_LIBRARY_PATH'] = os.path.join(os.getcwd(), basefolder) + "/LDLibs"
+  elif appName == "lcio":
+    os.environ['LCIO'] = os.path.join(os.getcwd(), basefolder)
+    os.environ['PATH'] = os.path.join(os.getcwd(), basefolder) + "/bin:" + os.environ['PATH']
+    res = checkJava(curdir)
+    if not res['OK']:
+      return res
+  elif appName == "lcsim":
+    res = checkJava(curdir)
+    if not res['OK']:
+      return res
+
+  #remove now useless tar ball
+  if os.path.exists("%s/%s" % (os.getcwd(), app_tar_base)):
+    if app_tar_base.find(".jar") < 0:
+      try:
+        os.unlink(app_tar_base)
+      except:
+        gLogger.error("Could not remove tar ball")
+  os.chdir(curdir)
+  return S_OK()
+
+def remove():
+  """ For the moment, this is done in L{RemoveSoft}
+  """
+  pass
+
+def CanWrite(area):
+  """ Check if user is allowed to write in the area
+  """
+  curdir = os.getcwd()
+  os.chdir(area)
+  try:
+    f = open("testfile.txt","w")
+    f.write("Testing writing\n")
+    f.close()
+    os.remove("testfile.txt")
+  except Exception, x:
+    gLogger.error('Problem trying to write in area %s: ' % area, str(x))
+    return False
+  finally:
+    os.chdir(curdir)
+  return True
+    
+
+def checkJava(dir):
+  """ Check if JAVA is availalbe locally.
+  """
+  args = ['java', "-version"]
+  try:
+    p = subprocess.check_call(args)
+    if p:
+      os.chdir(dir)
+      return S_ERROR("Something is wrong with Java")
+  except:
+    gLogger.error("Java was not found on this machine, cannot proceed")
+    os.chdir(dir)
+    return S_ERROR("Java was not found on this machine, cannot proceed")
+
+  return S_OK()
---
+++ Core/scripts/dirac-ilc-add-whizard.py
@@ -0,0 +1,380 @@
+'''
+Created on Sep 21, 2010
+
+@author: sposs
+'''
+
+import DIRAC
+from DIRAC.Core.Base import Script
+Script.parseCommandLine( ignoreErrors = False )
+args = Script.getPositionalArgs()
+
+whizard_location = "%s" % args[0]
+platform = "%s" % args[1]
+whizard_version = "%s" % args[2]
+appVersion = whizard_version
+beam_spectra_version = "%s" % args[3]
+
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
+from DIRAC.DataManagementSystem.Client.ReplicaManager        import ReplicaManager
+from DIRAC.Core.Utilities.Subprocess                         import shellCall
+from DIRAC.RequestManagementSystem.Client.RequestContainer   import RequestContainer
+from DIRAC.RequestManagementSystem.Client.RequestClient      import RequestClient
+from ILCDIRAC.Core.Utilities.ProcessList                     import ProcessList
+
+from DIRAC import gConfig, S_ERROR, S_OK
+
+import os, tarfile, shutil, sys, string
+
+
+diracAdmin = DiracAdmin()
+rm = ReplicaManager()
+request = RequestContainer()
+requestClient = RequestClient()
+modifiedCS = False
+
+def usage():
+  print 'Usage: %s <directory_where_whizard_is> <platform> <whizard_version> <beam_spectra_version>' % (Script.scriptName)
+  DIRAC.exit(2)
+
+def upload(path, appTar):
+  """ Upload to storage
+  """
+  global appVersion
+  if not os.path.exists(appTar):
+    print "File %s does not exists, cannot continue." % appTar
+    return S_ERROR()
+  if path.find("http://www.cern.ch/lcd-data") > -1:
+    final_path = "/afs/cern.ch/eng/clic/data/software/"
+    try:
+      shutil.copy(appTar, "%s%s" % (final_path, appTar))
+    except Exception, x:
+      print "Could not copy because %s" % x
+      return S_ERROR()
+  elif path.find("http://") > -1:
+    print "path %s was not forseen, location not known, upload to location yourself, and publish in CS manually" % path
+    return S_ERROR()
+  else:
+    lfnpath = "%s%s" % (path, os.path.basename(appTar))
+    res = rm.putAndRegister(lfnpath, appTar, "CERN-SRM")
+    if not res['OK']:
+      return res
+    res = request.addSubRequest({'Attributes': {'Operation' : 'replicateAndRegister',
+                                                'TargetSE' : 'IN2P3-SRM', 'ExecutionOrder' : 0}},
+                                 'transfer')
+    #res = rm.replicateAndRegister("%s%s"%(path,appTar),"IN2P3-SRM")
+    if not res['OK']:
+      return res
+    index = result['Value']
+    fileDict = {'LFN' : lfnpath, 'Status' : 'Waiting'}
+    request.setSubRequestFiles(index, 'transfer', [fileDict])
+    requestName = appTar.replace('.tgz','').replace('.cfg','_%s' % appVersion)
+    request.setRequestAttributes({'RequestName' : requestName})
+    requestxml = request.toXML()['Value']
+    res = requestClient.setRequest(requestName, requestxml)
+    if not res['OK']:
+      print 'Could not set replication request %s' % res['Message']
+    return S_OK('Application uploaded')
+  return S_OK()
+
+def redirectLogOutput(fd, message):
+  """ Needed to catch the log output of the shellCall below
+  """
+  sys.stdout.flush()
+  print message
+  
+def readPRCFile(prc):
+  """ Read the prc file to create the process description
+  """
+  list = {}
+  myprc = file(prc)
+  model = ""
+  for process in myprc:
+    process = process.rstrip()
+    if not len(process):
+      continue
+    if process[0] == "#":
+      continue
+    elems = process.split()
+    if elems[0] == "alias":
+      continue
+    elif elems[0] == "model":
+      model = elems[1]
+    elif not elems[0] == "model":
+      list[elems[0]] = {}
+      list[elems[0]]['Detail'] = string.join(elems[1:3], "->")
+      list[elems[0]]['Generator'] = elems[3]
+      list[elems[0]]['Restrictions'] = "none"
+      if len(elems) > 4:
+        list[elems[0]]['Restrictions'] = string.join(elems[4:], " ")
+      list[elems[0]]['Model'] = model
+      list[elems[0]]['InFile'] = "whizard.template.in"
+    else:
+      continue
+  
+  return list
+
+def getDetailsFromPRC(prc, processin):
+  """ Get the process details from the prc file
+  """
+  details = {}
+  myprc = file(prc)
+  model = ""
+  for process in myprc:
+    process = process.rstrip()
+    if not len(process):
+      continue
+    elems = process.split()
+    if process[0] == "#":
+      continue
+    elif elems[0] == "model":
+      model = elems[1]
+    elif not elems[0] == "model":
+      if elems[0] == processin:
+        details['Model'] = model
+        details['Generator'] = elems[3]
+        details['Restrictions'] = "none"
+        if len(elems) > 4:
+          details['Restrictions'] = string.join(elems[4:], " ")
+        break
+  return details
+
+  
+if len(args) < 3:
+  usage()
+
+
+softwareSection = "/Operations/AvailableTarBalls"
+processlistLocation = "/Operations/ProcessList/Location"
+
+appName = "whizard"
+
+
+path_to_process_list = gConfig.getOption(processlistLocation, None)
+if not path_to_process_list:
+  print "Could not find process list Location in CS"
+  DIRAC.exit(2)
+
+res = rm.getFile(path_to_process_list['Value'])
+if not res['OK']:
+  print "Error while getting process list from storage"
+  DIRAC.exit(2)
+print "done"
+
+processlist = os.path.basename(path_to_process_list['Value'])
+if not os.path.exists(processlist):
+  print "Process list does not exist locally"
+  DIRAC.exit(2)
+
+
+pl = ProcessList(processlist)
+
+startdir = os.getcwd()
+inputlist = {}
+os.chdir(whizard_location)
+folderlist = os.listdir(os.getcwd())
+whiz_here = folderlist.count("whizard")
+if whiz_here == 0:
+  print "whizard executable not found in %s, please check" % whizard_location
+  os.chdir(startdir)
+  DIRAC.exit(2)
+whizprc_here = folderlist.count("whizard.prc")
+if whizprc_here == 0:
+  print "whizard.prc not found in %s, please check" % whizard_location
+  os.chdir(startdir)
+  DIRAC.exit(2)
+whizmdl_here = folderlist.count("whizard.mdl")
+if whizprc_here == 0:
+  print "whizard.mdl not found in %s, please check" % whizard_location
+  os.chdir(startdir)
+  DIRAC.exit(2)
+ 
+  
+print "Preparing process list"
+
+for f in folderlist:
+  if f.count(".in"):
+    infile = file(f, "r")
+    found_detail = False
+    
+    for line in infile:
+      if line.count("decay_description"):
+        currprocess = f.split(".template.in")[0] 
+        inputlist[currprocess] = {}        
+        inputlist[currprocess]["InFile"] = f.rstrip("~")
+        inputlist[currprocess]["Detail"] = line.split("\"")[1]
+        found_detail = True
+      if line.count("process_id") and found_detail:
+        process_id = line.split("\"")[1]
+        inputlist[currprocess]["Model"] = ""
+        inputlist[currprocess]["Generator"] = ""
+        inputlist[currprocess]["Restrictions"] = ""
+        for process in process_id.split():
+          print "Looking for detail of process %s" % (process)
+          process_detail = getDetailsFromPRC("whizard.prc", process)  
+          inputlist[currprocess]["Model"] = process_detail["Model"]
+          inputlist[currprocess]["Generator"] = process_detail["Generator"]
+          if len(inputlist[currprocess]["Restrictions"]):
+            inputlist[currprocess]["Restrictions"] = inputlist[currprocess]["Restrictions"] + ", " + process_detail["Restrictions"]
+          else:
+            inputlist[currprocess]["Restrictions"] = process_detail["Restrictions"]
+    #if len(inputlist[currprocess].items()):
+    #  inputlist.append(processdict)    
+
+##Update inputlist with what was found looking in the prc file
+processes = readPRCFile("whizard.prc")
+inputlist.update(processes)
+
+##get from cross section files the cross sections for the processes in inputlist
+#Need full process list
+for f in folderlist:
+  if f.count("cross_sections_"):
+    crossfile = file(f, "r")
+    for line in crossfile:
+      line = line.rstrip().lstrip()
+      if not len(line):
+        continue
+      if line[0] == "#" or line[0] == "!":
+        continue
+      if len(line.split()) < 2:
+        continue
+      currprocess = line.split()[0]
+      if inputlist.has_key(currprocess):
+        inputlist[currprocess]['CrossSection'] = line.split()[1]
+
+
+print "Preparing Tar ball"
+appTar = os.path.join(os.getcwd(), "whizard" + whizard_version + ".tgz")
+
+if os.path.exists('lib'):
+  shutil.rmtree('lib')
+scriptName = 'ldd.sh'
+script = file(scriptName, "w")
+script.write('#!/bin/bash \n')
+script.write("""string1=$(ldd whizard | grep '=>' | sed 's/.*=>//g' | sed 's/(.*)//g')
+string=""
+for file in $string1; do
+  string=\"$file $string\"
+done
+mkdir lib
+cp $string ./lib
+whizarddir=%s 
+rm -rf $whizarddir
+mkdir $whizarddir
+cp -r *.cut1 *.in cross_sections_* whizard whizard.prc whizard.mdl lib/ $whizarddir
+""" % ("whizard" + whizard_version))
+script.close()
+os.chmod(scriptName, 0755)
+comm = 'source %s' % (scriptName)
+result = shellCall(0, comm, callbackFunction = redirectLogOutput, bufferLimit = 20971520)
+os.remove("ldd.sh")
+myappTar = tarfile.open(appTar, "w:gz")
+myappTar.add("whizard" + whizard_version)
+myappTar.close()
+print "Done"
+print "Registering new Tar Ball in CS"
+tarballurl = {}
+
+av_platforms = gConfig.getSections(softwareSection, [])
+if av_platforms['OK']:
+  if not platform in av_platforms['Value']:
+    print "Platform %s unknown, available are %s." % (platform, string.join(av_platforms['Value'], ", "))
+    print "If yours is missing add it in CS"
+    DIRAC.exit(255)
+else:
+  print "Could not find all platforms available in CS"
+  DIRAC.exit(255)
+
+av_apps = gConfig.getSections("%s/%s" % (softwareSection, platform), [])
+if not av_apps['OK']:
+  print "Could not find all applications available in CS"
+  DIRAC.exit(255)
+
+if appName.lower() in av_apps['Value']:
+  versions = gConfig.getSections("%s/%s/%s" % (softwareSection, platform, appName.lower()), 
+                                 [])
+  if not versions['OK']:
+    print "Could not find all versions available in CS"
+    DIRAC.exit(255)
+  if appVersion in versions['Value']:
+    print 'Application %s %s for %s already in CS, nothing to do' % (appName.lower(), appVersion, platform)
+    DIRAC.exit(0)
+  else:
+    result = diracAdmin.csSetOption("%s/%s/%s/%s/TarBall" % (softwareSection, platform, appName.lower(), appVersion),
+                                    os.path.basename(appTar))
+    if result['OK']:
+      modifiedCS = True
+      tarballurl = gConfig.getOption("%s/%s/%s/TarBallURL" % (softwareSection, platform, appName.lower()), "")
+      if len(tarballurl['Value']) > 0:
+        res = upload(tarballurl['Value'], appTar)
+        if not res['OK']:
+          print "Upload to %s failed" % tarballurl
+          DIRAC.exit(255)
+    result = diracAdmin.csSetOption("%s/%s/%s/%s/Dependencies/beam_spectra/version" % (softwareSection,
+                                                                                       platform,
+                                                                                       appName.lower(),
+                                                                                       appVersion),
+                                    beam_spectra_version)
+    
+
+else:
+  result = diracAdmin.csSetOption("%s/%s/%s/%s/TarBall" % (softwareSection, platform,
+                                                           appName.lower(), appVersion),
+                                  os.path.basename(appTar))
+  if result['OK']:  
+    modifiedCS = True
+    tarballurl = gConfig.getOption("%s/%s/%s/TarBallURL" % (softwareSection, platform, appName.lower()),
+                                   "")
+    if len(tarballurl['Value']) > 0:
+      res = upload(tarballurl['Value'], appTar)
+      if not res['OK']:
+        print "Upload to %s failed" % tarballurl
+        DIRAC.exit(255)
+  result = diracAdmin.csSetOption("%s/%s/%s/%s/Dependencies/beam_spectra/version" % (softwareSection,
+                                                                                     platform,
+                                                                                     appName.lower(),
+                                                                                     appVersion),
+                                  beam_spectra_version)
+print "Done"
+
+os.remove(appTar)
+#Set for all new processes the TarBallURL
+for process in inputlist.keys():
+  inputlist[process]['TarBallCSPath'] = tarballurl['Value'] + os.path.basename(appTar)
+
+
+knownprocess = pl.getProcessesDict()
+knownprocess.update(inputlist)
+pl.updateProcessList(knownprocess)
+print "Done"
+
+#Return to initial location
+os.chdir(startdir)
+
+pl.writeProcessList()
+
+res = rm.removeFile(path_to_process_list['Value'])
+if not res['OK']:
+  print "Could not remove process list from storage, do it by hand"
+  DIRAC.exit(2)
+
+
+res = upload(os.path.dirname(path_to_process_list['Value']) + "/", processlist)
+if not res['OK']:
+  print "something went wrong in the copy"
+  DIRAC.exit(2)
+
+localprocesslistpath = gConfig.getOption("/LocalSite/ProcessListPath", "")
+if localprocesslistpath['Value']:
+  try:
+    shutil.copy(processlist, localprocesslistpath['Value'])
+  except:
+    print "Copy of process list to %s failed!" % localprocesslistpath['Value']
+print "Done"
+#Commit the changes if nothing has failed and the CS has been modified
+if modifiedCS:
+  result = diracAdmin.csCommitChanges(False)
+  print result
+
+exitCode = 0
+DIRAC.exit(exitCode)
