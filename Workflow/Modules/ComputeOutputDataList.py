8ce9b2a532106f903e4460f25fd30073b4d95f8e
sposs <sposs@cern.ch>
2012-07-26 06:41:32 +0000

    Release v11r0p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v11r0p0@54517 4525493e-7705-40b1-a816-d608a930855b

---
+++ Workflow/Modules/ComputeOutputDataList.py
@@ -0,0 +1,50 @@
+#####################################################
+# $HeadURL: $
+#####################################################
+'''
+Module to compute the outputdata list for production jobs
+
+Created on Jun 30, 2010
+
+@author: sposs
+'''
+
+__RCSID__ = "$Id: $"
+
+from ILCDIRAC.Workflow.Modules.ModuleBase                 import ModuleBase
+
+from DIRAC import gLogger, S_OK
+
+class ComputeOutputDataList(ModuleBase):
+  """ In case the previous module executed properly, add the output to the listoutput. 
+  This is used in the prduction context to ensure only the files coming from successful applications
+  are added to the output list. Otherwise, there is a risk to register corrupted files.
+  """
+  def __init__(self):
+    """Module initialization.
+    """
+    ModuleBase.__init__(self)
+    self.version = __RCSID__
+    self.log = gLogger.getSubLogger( "ComputeOutputData" )
+    self.listoutput = []
+
+  def applicationSpecificInputs(self):
+    """ Update the workflow_commons dictionary with the current step's output
+    """
+    if self.step_commons.has_key('listoutput'):
+      self.listoutput = self.step_commons['listoutput']
+
+    if self.workflow_commons.has_key('outputList'):
+      self.workflow_commons['outputList'] = self.workflow_commons['outputList'] + self.listoutput
+    else:
+      self.workflow_commons['outputList'] = self.listoutput
+    
+    return S_OK()
+  
+  def execute(self):
+    """ Not much to do...
+    """
+    res = self.resolveInputVariables()
+    if not res['OK']:
+      return res
+    return S_OK()
