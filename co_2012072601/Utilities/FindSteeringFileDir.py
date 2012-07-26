980fd5843856826c519f3b4f0cf12a3fe6b57ef6
sposs <sposs@cern.ch>
2012-07-26 11:25:13 +0000

    tag v11r0p1 for Operations Helper
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/Core@54523 4525493e-7705-40b1-a816-d608a930855b

---
+++ co_2012072601/Utilities/FindSteeringFileDir.py
@@ -0,0 +1,28 @@
+'''
+Created on Feb 10, 2012
+
+@author: Stephane Poss
+'''
+
+from DIRAC import gConfig, S_OK, S_ERROR
+from ILCDIRAC.Core.Utilities.CombinedSoftwareInstallation  import getSoftwareFolder
+from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+
+def getSteeringFileDirName(systemConfig, application, applicationVersion):
+  """ Locate the path of the steering file directory assigned to the specified appliciation
+  """
+  ops = Operations(setup='Defaults')
+  version = ops.getValue('/AvailableTarBalls/%s/%s/%s/Dependencies/steeringfiles/version' % (systemConfig, 
+                                                                                            application,
+                                                                                            applicationVersion), '')
+  if not version: 
+    return S_ERROR("Could not find attached SteeringFile version")
+  TarBall = ops.getValue('/AvailableTarBalls/%s/steeringfiles/%s/TarBall' % (systemConfig, version), '')
+  if not TarBall:
+    return S_ERROR("Could not find tar ball for SteeringFile")
+  mydir = TarBall.replace(".tgz", "").replace(".tar.gz", "")
+  res = getSoftwareFolder(mydir)
+  if not res['OK']:
+    return res
+  mySoftDir = res['Value']
+  return S_OK(mySoftDir)
---
+++ co_2012072601/Utilities/ResolveDependencies.py
@@ -0,0 +1,70 @@
+'''
+ILCDIRAC.Core.Utilities.ResolveDependencies
+
+Set of functions used to resolve the applications' dependencies, looking into the CS
+
+Works recursively
+
+@since: Apr 26, 2010
+
+@author: Stephane Poss
+'''
+
+from DIRAC import gConfig, gLogger
+from DIRAC.ConfigurationSystem.Client.Helpers.Operations            import Operations
+
+def resolveDeps(sysconfig, appli, appversion):
+  """ Resolve the dependencies
+  
+  @param sysconfig: system configuration
+  @type sysconfig: string
+  @param appli: application name
+  @type appli: string
+  @param appversion: application version
+  @type appversion: string
+  
+  @return: array of dictionaries
+  """
+  ops = Operations(setup='Defaults')
+  deps = ops.getSections('/AvailableTarBalls/%s/%s/%s/Dependencies' % (sysconfig, appli, 
+                                                                       appversion), '')
+  depsarray = []
+  if deps['OK']:
+    for dep in deps['Value']:
+      vers = ops.getOption('/AvailableTarBalls/%s/%s/%s/Dependencies/%s/version' % (sysconfig, appli, 
+                                                                                    appversion, dep), '')
+      depvers = ''
+      if vers['OK']:
+        depvers = vers['Value']
+      else:
+        gLogger.error("Retrieving dependency version for %s failed, skipping to next !" % (dep))
+        continue
+      gLogger.verbose("Found dependency %s %s" % (dep, depvers))
+      depdict = {}
+      depdict["app"] = dep
+      depdict["version"] = depvers
+      depsarray.append(depdict)
+      ##resolve recursive dependencies
+      depsofdeps = resolveDeps(sysconfig, dep, depvers)
+      depsarray.extend(depsofdeps)
+  else:
+    gLogger.verbose("Could not find any dependency for %s %s, ignoring" % (appli, appversion))
+  return depsarray
+
+def resolveDepsTar(sysconfig, appli, appversion):
+  """ Return the dependency tar ball name, if available
+  
+  Uses same parameters as L{resolveDeps}.
+  @return: array of strings
+  """
+  ops = Operations(setup='Defaults')
+  deparray = resolveDeps(sysconfig, appli, appversion)
+  depsarray = []
+  for dep in deparray:
+    dep_tar = ops.getOption('/AvailableTarBalls/%s/%s/%s/TarBall' % (sysconfig, dep["app"], 
+                                                                     dep["version"]), '')
+    if dep_tar['OK']:
+      depsarray.append(dep_tar["Value"])
+    else:
+      gLogger.error("Dependency %s version %s is not defined in CS, please check !" % (dep["app"], dep["version"]))         
+  return depsarray
