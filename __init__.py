f142a14e68bdecc4e42c5c7ea7fcf709a91f5edd
sposs <sposs@cern.ch>
2012-07-27 09:02:59 +0000

    tag v11r1p0, move to dirac v6r4p15
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@54675 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -9,8 +9,8 @@ __path__ = extend_path(__path__, __name__)
 # Define Version
 
 majorVersion = 11
-minorVersion = 0
-patchLevel   = 2
+minorVersion = 1
+patchLevel   = 0
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
