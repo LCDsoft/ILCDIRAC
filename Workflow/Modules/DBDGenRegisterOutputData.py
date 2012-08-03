e13bce048b08c65f209bde3be40d1ea8288413c9
sposs <sposs@cern.ch>
2012-08-03 10:29:46 +0000

    Release v12r0p8
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v12r0p8@55087 4525493e-7705-40b1-a816-d608a930855b

---
+++ Workflow/Modules/DBDGenRegisterOutputData.py
@@ -0,0 +1,65 @@
+'''
+Created on Jan 26, 2012
+
+@author: Stephane Poss
+'''
+from ILCDIRAC.Workflow.Modules.ModuleBase                  import ModuleBase
+
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+
+from DIRAC import S_OK, S_ERROR, gLogger
+
+import string
+
+class DBDGenRegisterOutputData(ModuleBase):
+  """ Normally, was supposed to be used to produce the DBD gen level files. Dropped in the end.
+  """
+  def __init__(self):
+    ModuleBase.__init__(self)
+    self.version = "DBDGenRegisterOutputData v1"
+    self.log = gLogger.getSubLogger( "DBDGenRegisterOutputData" )
+    self.commandTimeOut = 10 * 60
+    self.enable = True
+    self.fc = FileCatalogClient()
+    self.nbofevents = 0
+    
+  def applicationSpecificInputs(self):
+    if self.workflow_commons.has_key('ProductionOutputData'):
+      self.prodOutputLFNs = self.workflow_commons['ProductionOutputData'].split(";")
+    else:
+      self.prodOutputLFNs = []
+      
+    if self.workflow_commons.has_key('NbOfEvents'):
+      self.nbofevents = self.workflow_commons['NbOfEvents']
+    if self.workflow_commons.has_key('NbOfEvts'):
+      self.nbofevents = self.workflow_commons[ 'NbOfEvts']          
+    return S_OK("Parameters resolved")
+      
+  def execute(self):
+    self.log.info('Initializing %s' % self.version)
+    result = self.resolveInputVariables()
+    if not result['OK']:
+      self.log.error(result['Message'])
+      return result
+    
+    if not self.workflowStatus['OK'] or not self.stepStatus['OK']:
+      self.log.verbose('Workflow status = %s, step status = %s' % (self.workflowStatus['OK'], self.stepStatus['OK']))
+      return S_OK('No registration of output data metadata attempted')
+
+    if not len(self.prodOutputLFNs):
+      self.log.info('No production data found, so no metadata registration to be done')  
+      return S_OK("No files' metadata to be registered")
+    
+    self.log.verbose("Will try to set the metadata for the following files: \n %s" % string.join(self.prodOutputLFNs, "\n"))
+    
+    for files in self.prodOutputLFNs:
+      metadict = {}
+      metadict['NumberOfEvents'] = self.nbofevents
+      path = files
+      
+      res = self.fc.setMetadata(files, metadict)
+      if not res['OK']:
+        self.log.error("Could not register %s for %s"%(metadict, path))
+        return res
+    
+    return S_OK()
