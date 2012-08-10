a5373f59b7567077917ab7bddf2b74415caa4dc1
sposs <sposs@cern.ch>
2012-08-10 09:54:11 +0000

    Release v12r0p9
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v12r0p9@55375 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/WasteCPU.py
@@ -0,0 +1,26 @@
+'''
+Created on Jul 26, 2011
+
+@author: Stephane Poss
+
+So this is the worst piece of code ever NEEDED: just run some CPU intensive code to waste CPU time.
+'''
+import time
+from math import log
+from DIRAC import S_OK, S_ERROR
+
+def WasteCPUCycles(timecut):
+  """ Waste, waste, and waste more CPU.
+  """
+  a = 1e31
+  first = time.clock()
+  while time.clock()-first < timecut:
+    try:
+      a = log(a)
+    except Exception, x:
+      return S_ERROR("Failed to waste %s CPU seconds:%s" % (timecut, str(x)))  
+    if a < 0:
+      a = -a
+    if a == 0:
+      a = 4  
+  return S_OK("Successfully wasted %s seconds" % timecut)    
