ded49a5b7b2b47651f2b0b75be15d836597a883d
sposs <sposs@cern.ch>
2012-08-21 11:45:17 +0000

    Release v13r0p1
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v13r0p1@55631 4525493e-7705-40b1-a816-d608a930855b

---
+++ __init__.py
@@ -0,0 +1,24 @@
+# $HeadURL: svn+ssh://sposs@svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC/__init__.py $
+# $Id: __init__.py 55629 2012-08-21 11:44:45Z sposs $
+"""
+ILCDIRAC package, implements ILC/CLIC production and application specific stuff
+"""
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+# Define Version
+
+majorVersion = 13
+minorVersion = 0
+patchLevel = 1
+preVersion = 0
+    
+version      = "v%sr%s" % ( majorVersion, minorVersion )
+buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
+if patchLevel:
+  version = "%sp%s" % ( version, patchLevel )
+  buildVersion = "%s build %s" % ( buildVersion, patchLevel )
+if preVersion:
+  version = "%s-pre%s" % ( version, preVersion )
+  buildVersion = "%s pre %s" % ( buildVersion, preVersion )
+
