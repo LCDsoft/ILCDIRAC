48c0c61e94453b61aa6411702870e4f1a06aab53
sposs <sposs@cern.ch>
2012-09-06 08:08:18 +0000

    tag v13r1p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/Core@55900 4525493e-7705-40b1-a816-d608a930855b

---
+++ co_2012090601/Utilities/SiDsoft.py
@@ -0,0 +1,19 @@
+'''
+
+Install SiD software
+
+Created on Apr 7, 2010
+
+@author: sposs
+'''
+from DIRAC import S_OK
+
+def install(app, config, area):
+  """ Install SiD Soft, not used, could be removed
+  """
+  return S_OK()
+
+def remove():
+  """ Remove application.
+  """
+  pass
