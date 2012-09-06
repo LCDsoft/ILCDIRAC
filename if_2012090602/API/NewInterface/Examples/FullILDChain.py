125734d8316dc66713f9f098efabae9dcf509a35
sposs <sposs@cern.ch>
2012-09-06 09:01:55 +0000

    tag v13r1p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/Interfaces@55906 4525493e-7705-40b1-a816-d608a930855b

---
+++ if_2012090602/API/NewInterface/Examples/FullILDChain.py
@@ -0,0 +1,73 @@
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+
+from ILCDIRAC.Interfaces.API.DiracILC import *
+from ILCDIRAC.Interfaces.API.NewInterface.Applications import *
+from ILCDIRAC.Interfaces.API.NewInterface.UserJob import *
+
+dirac =DiracILC()
+
+#wh.setOutputFile("myfile.stdhep")
+
+j = UserJob()
+
+wh = Whizard(processlist=dirac.getProcessList())
+wh.setEnergy(3000)
+wh.setEvtType("ee_h_mumu")
+wh.setNbEvts(1)
+wh.setEnergy(3000)
+params = {}
+params['USERB1']='F'
+wh.setParameterDict(params)
+wh.setModel("sm")
+res = j.append(wh)
+if not res['OK']:
+    print res['Message']
+    exit()
+
+
+mo = Mokka()
+mo.getInputFromApp(wh)
+mo.setVersion("0706P08")
+mo.setSteeringFile("clic_ild_cdr.steer")
+mo.setNbEvts(1)
+mo.setOutputFile("somefile.slcio")
+res = j.append(mo)
+if not res['OK']:
+    print res['Message']
+    exit()
+
+
+ov = OverlayInput()
+ov.setDetectorType("ILD")
+ov.setBXOverlay(60)
+ov.setGGToHadInt(3.2)
+ov.setNbSigEvtsPerJob(1)
+res = j.append(ov)
+if not res['OK']:
+    print res['Message']
+    exit()
+
+
+ma = Marlin()
+ma.setVersion("v0111Prod")
+ma.setSteeringFile("clic_ild_cdr_steering_overlay.xml")
+ma.setGearFile("clic_ild_cdr.gear")
+ma.getInputFromApp(mo)
+ma.setDebug(True)
+res = j.append(ma)
+if not res['OK']:
+    print res['Message']
+    exit()
+#print appplication's attributes.
+ma.listAttributes()
+
+j.setName("test")
+j.setOutputSandbox("*.log")
+
+res = dirac.checkparams(j)
+if not res['OK']:
+    print res['Message']
+    sys.exit(1)
+
+
