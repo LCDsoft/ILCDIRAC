b7616a7ff818361a2ab69403916dd871c428aeab
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-10-05 12:15:12 +0000

    updated dirac-install path, and installation instructions

--- Core/scripts/ilcdirac-install.sh
+++ Core/scripts/ilcdirac-install.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-version=$1
-if [ -z "$version" ]; then
-  version=v9r0p4
-fi
-
-wget http://lhcbproject.web.cern.ch/lhcbproject/dist/DIRAC3/dirac-install
+wget -O dirac-install -np  https://raw.github.com/DIRACGrid/DIRAC/master/Core/scripts/dirac-install.py  --no-check-certificate
 chmod +x dirac-install
-./dirac-install -l ILCDIRAC -r $version -i 26
+./dirac-install -V ILCDIRAC
 
 vo=ilc
 setup=ILC-Production
@@ -16,8 +11,7 @@ csserver=dips://volcd01.cern.ch:9135/Configuration/Server
 scripts/dirac-configure -V $vo -S $setup -C $csserver -d --SkipCAChecks
 echo ""
 echo "You might want to run :"
-echo "./dirac-install -l ILCDIRAC -r $version -i 26 -g 2011-06-06"
-echo "$version=v9r0p4 for e.g."
+echo "./dirac-install -V ILCDIRAC -g 2012-02-20"
 echo "to get the grid UI (-g option), if available for your platform"
 echo ""
 echo "To get the proper environment, run source bashrc"
