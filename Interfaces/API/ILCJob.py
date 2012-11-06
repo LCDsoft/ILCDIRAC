7f9c722056e32c6f4680abf653d8728d239cbff2
sposs <sposs@cern.ch>
2012-11-06 21:14:26 +0000

    Release v13r2p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v13r2p2@58289 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/API/ILCJob.py
@@ -0,0 +1,5 @@
+from DIRAC import gLogger
+class ILCJob():
+  def __init__(self):
+    gLogger.error("This class is now obsolete, please move to the new interface. Contact ilc-dirac@cern.ch for assistance if needed.")
+    
