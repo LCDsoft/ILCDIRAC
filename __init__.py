23622838c713e362d8c9399462b8dd0a77e938ca
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2012-11-06 15:53:33 +0000

    tag v13r2p0, for the TarSoft installation

--- __init__.py
+++ __init__.py
@@ -9,8 +9,8 @@ __path__ = extend_path(__path__, __name__)
 # Define Version
 
 majorVersion = 13
-minorVersion = 1
-patchLevel   = 8
+minorVersion = 2
+patchLevel   = 0
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
