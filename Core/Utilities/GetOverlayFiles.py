b043254e93318fc594b0adb3fb6ba9e0a0afb516
sposs <sposs@cern.ch>
2012-11-16 13:31:30 +0000

    add some doc
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@58570 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/GetOverlayFiles.py
+++ Core/Utilities/GetOverlayFiles.py
@@ -1,4 +1,6 @@
 '''
+List the content of the overlay directory given a bakground type. Called from L{PrepareXMLFile}.
+
 Created on Jan 28, 2011
 
 @author: sposs
@@ -6,18 +8,18 @@ Created on Jan 28, 2011
 from DIRAC import gLogger
 import os
 
-def getOverlayFiles(evttype='gghad'):
+def getOverlayFiles(evttype = 'gghad'):
   """ Return the list of files contained in the overlay_BKG folder, where BKG can be anything
   """
   localfiles = []
-  if not os.path.exists("./overlayinput_"+evttype):
-    gLogger.error('overlay directory does not exists')
+  if not os.path.exists( "./overlayinput_"+evttype ):
+    gLogger.error( 'overlay directory does not exists' )
     return localfiles
   curdir = os.getcwd()
-  os.chdir("./overlayinput_"+evttype)
-  listdir = os.listdir(os.getcwd())
+  os.chdir( "./overlayinput_"+evttype )
+  listdir = os.listdir( os.getcwd() )
   for item in listdir:
-    if item.count('.slcio'):
-      localfiles.append(os.getcwd()+os.sep+item)
+    if item.count( '.slcio' ):
+      localfiles.append( os.getcwd()+os.sep+item )
   os.chdir(curdir)
   return localfiles
