3270f426415fdc7c967b782e3af1cc39040bbfd5
sposs <sposs@cern.ch>
2012-12-03 09:09:16 +0000

    Release v16r2p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v16r2p0@59255 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/OutputDataPolicy.py
@@ -0,0 +1,44 @@
+""" OutputDataPolicy generates the output data that will be created by a workflow task
+
+    DIRAC assumes an execute() method will exist during usage.
+"""
+__RCSID__   = "$Id: OutputDataPolicy.py 19570 2010-01-07 08:42:02Z joel $"
+__VERSION__ = "$Revision: 1.40 $"
+
+from DIRAC                                          import gLogger
+from DIRAC.Interfaces.API.Job                       import Job
+from ILCDIRAC.Core.Utilities.ProductionData         import constructProductionLFNs
+
+class OutputDataPolicy:
+  """ This module is called from the TransformationSystem
+  """  
+  def __init__(self, paramDict):
+    self.paramDict = paramDict
+
+  def execute(self):
+    """ Execute it.
+    """
+    jobDescription = self.paramDict['Job']
+    prodID = self.paramDict['TransformationID']
+    jobID = self.paramDict['TaskID']
+    inputData = self.paramDict['InputData']
+    
+    job = Job(jobDescription)
+    commons = job._getParameters()
+    code = job.createCode()
+    outputList = []
+    for line in code.split("\n"):
+      if line.count("listoutput"):
+        outputList += eval(line.split("#")[0].split("=")[-1])
+
+    commons['outputList'] = outputList
+    commons['PRODUCTION_ID'] = prodID
+    commons['JOB_ID'] = jobID
+    if inputData:
+      commons['InputData'] = inputData
+
+    gLogger.debug(commons)
+    result = constructProductionLFNs(commons)
+    if not result['OK']:
+      gLogger.error(result['Message'])
+    return result
