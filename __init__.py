e52d1d4d281061897ba023528209c8ae87d9bc7b
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2013-01-21 08:48:38 +0000

    move to v6r6p10

--- __init__.py
+++ __init__.py
@@ -9,8 +9,8 @@ __path__ = extend_path(__path__, __name__)
 # Define Version
 
 majorVersion = 16
-minorVersion = 5
-patchLevel   = 2
+minorVersion = 6
+patchLevel   = 0
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
