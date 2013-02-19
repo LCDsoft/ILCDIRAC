cd01cc93b8cc34b6a0b843dca9211fb01b2d61c0
sposs <sposs@cern.ch>
2013-02-19 16:34:39 +0000

    tag v16r8p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/Interfaces@61854 4525493e-7705-40b1-a816-d608a930855b

---
+++ if_2013021901/API/Examples/SIDChain/bannedSites.py
@@ -0,0 +1,14 @@
+bannedSites = [
+'LCG.RAL-LCG2.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.Bristol.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.UKI-NORTHGRID-LANCS-HEP.uk',	# firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.UKI-NORTHGRID-LIV-HEP.uk', # firewall does not allow html downloads, conflicts with lcsim downloading detector descriptions
+'LCG.Brunel.uk',
+'LCG.Manchester.uk',	# crashes running lcsim
+'LCG.Weizmann.il',	# crashes running lcsim
+'LCG.TECHNION.il',	# crashes running lcsim
+'LCG.NIPNE.ro',		# crashes running slic
+'LCG.Tau.il',		# crashes running slic
+'LCG.Liverpool.uk',	# crashes running lcsim
+'LCG.DESYZN.de', # crashes running lcsim
+]
