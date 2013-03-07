0e99f73da57e475226cadaa4cd8a54b2bb710fef
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2013-03-07 13:19:13 +0000

    Create proper python scripts

--- Interfaces/scripts/dirac-repo-retrieve-jobs-output.py
+++ Interfaces/scripts/dirac-repo-retrieve-jobs-output.py
@@ -1,52 +1,60 @@
 '''
+Retrieve the output sandboxes of jobs created using the API, stored in the repository file
+
 Created on Mar 24, 2010
 
 @author: sposs
 '''
 from DIRAC.Core.Base import Script
-import sys
-
-outputdata = False
-repoLocation = None
-
-Script.registerSwitch( 'a:', 'outputdata', 'retrieve also the outputdata')
-Script.registerSwitch( 'r:', 'repository=', 'repository file to use')
-Script.setUsageMessage( sys.argv[0] + '-r <repo location> (-a)')
-
-Script.parseCommandLine( ignoreErrors = False )
-switches = Script.getUnprocessedSwitches()
-for switch in switches:
-  opt = switch[0]
-  arg = switch[1]
-  if opt in ('r', 'repository'):
-    repoLocation = arg
-  else:
-    Script.showHelp()
-  if opt in ('a', 'outputdata'):
-    outputdata = True  
-
-if not repoLocation:
-  Script.showHelp()
-  sys.exit(1)
-    
-import DIRAC
+from DIRAC import S_OK, E_RROR, exit as dexit
 from DIRAC.Interfaces.API.Dirac import Dirac
 
-dirac = Dirac(True, repoLocation)
-
-exitCode = 0
-res = dirac.monitorRepository(False)
-if not res['OK']:
-  print "Failed because %s" % res['Message']
-  DIRAC.exit(1)
+class Params(object):
+  def __init__(self):
+    self.outputdata = False
+    self.repo = ''
+    
+  def setOuputData(self, opt):
+    self.outputdata = True
+    return S_OK()
   
-res = dirac.retrieveRepositorySandboxes()
-if not res['OK']:
-  print "Failed because %s" % res['Message']
-  DIRAC.exit(1)
-if outputdata:
-  res = dirac.retrieveRepositoryData()
+  def setRepo(self, opt):
+    self.repo = opt
+    return S_OK()
+  
+  def registerSwitches(self):
+    Script.registerSwitch( 'd', 'outputdata', 'retrieve also the outputdata')
+    Script.registerSwitch( 'r:', 'repository=', 'repository file to use')
+    Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
+                                        '\nUsage:',
+                                        '  %s [option|cfgfile] ...\n' % Script.scriptName ] )  )
+    
+if __name__=="__main__":
+  outputdata = False
+  repoLocation = None
+  clip = Params()
+  clip.registerSwitches()
+  Script.parseCommandLine( ignoreErrors = False )
+  repoLocation = clip.repo
+  if not repoLocation:
+    Script.showHelp()
+    dexit(1)
+  
+  dirac = Dirac(True, repoLocation)
+  
+  exitCode = 0
+  res = dirac.monitorRepository(False)
   if not res['OK']:
     print "Failed because %s" % res['Message']
-    DIRAC.exit(1)
-DIRAC.exit(exitCode)
+    dexit(1)
+    
+  res = dirac.retrieveRepositorySandboxes()
+  if not res['OK']:
+    print "Failed because %s" % res['Message']
+    dexit(1)
+  if clip.outputdata:
+    res = dirac.retrieveRepositoryData()
+    if not res['OK']:
+      print "Failed because %s" % res['Message']
+      exit(1)
+  dexit(exitCode)
