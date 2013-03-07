189a1b3091ce28dc6353fb742f451a086e9743bd
sposs <sposs@cern.ch>
2013-03-07 14:20:22 +0000

    Make it a new style class
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@62825 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/API/ILCJob.py
+++ Interfaces/API/ILCJob.py
@@ -2,7 +2,7 @@
 Forget this, it should NOT be used
 """
 from DIRAC import gLogger
-class ILCJob():
+class ILCJob(object):
   def __init__(self):
     gLogger.error("This class is now obsolete, please move to the new interface. Contact ilc-dirac@cern.ch for assistance if needed.")
     
