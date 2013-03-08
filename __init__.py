633653849a64ef6f865937e007cae2bdec2879ce
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2013-03-08 13:11:49 +0000

    base on v6r7p15

--- __init__.py
+++ __init__.py
@@ -8,9 +8,9 @@ __path__ = extend_path(__path__, __name__)
 
 # Define Version
 
-majorVersion = 16
-minorVersion = 9
-patchLevel   = 2
+majorVersion = 17
+minorVersion = 1
+patchLevel   = 0
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
