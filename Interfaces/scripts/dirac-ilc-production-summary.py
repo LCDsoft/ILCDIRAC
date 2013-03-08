f7f7fcef9976e813361938c02e4a2cab767b966b
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2013-03-08 15:12:41 +0000

    Final version of the production summary page

--- Interfaces/scripts/dirac-ilc-production-summary.py
+++ Interfaces/scripts/dirac-ilc-production-summary.py
@@ -1,38 +1,46 @@
-'''
-Created on Mar 7, 2011
-
-@author: sposs
-'''
+"""
+Prepare the production summary tables
+"""
 from DIRAC.Core.Base import Script
-Script.parseCommandLine()
-args = Script.getPositionalArgs()
 
-import os
-from DIRAC.TransformationSystem.Client.TransformationClient   import TransformationClient
+import os,string
+from DIRAC.TransformationSystem.Client.TransformationClient         import TransformationClient
 
-from ILCDIRAC.Core.Utilities.ProcessList                      import ProcessList
-from DIRAC                                                    import gConfig
-from DIRAC.Resources.Catalog.FileCatalogClient                import FileCatalogClient
+from ILCDIRAC.Core.Utilities.ProcessList import ProcessList
 
-fc = FileCatalogClient()
+from ILCDIRAC.Core.Utilities.HTML import *
+from DIRAC import gConfig, exit as dexit
 
-processlist = gConfig.getValue('/LocalSite/ProcessListPath')
-prl = ProcessList(processlist)
-processesdict = prl.getProcessesDict()
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
 
-trc = TransformationClient()
-prodids = []
-if not len(args):
-  prodtype = 'MCGeneration'
-  res = trc.getTransformations( {'Type' : prodtype} )
-  if res['OK']:
-    for transfs in res['Value']:
-      prodids.append(transfs['TransformationID'])
+from DIRAC.Core.Utilities import DEncode
 
-else:
-  prodids = args
+
+def getFileInfo(lfn):
+  """ Retrieve the file info
+  """
+  fc = FileCatalogClient()
+  lumi = 0
+  nbevts = 0
+  res  = fc.getFileUserMetadata(lfn)
+  if not res['OK']:
+    print "Failed to get metadata of %s"%lfn
+  if res['Value'].has_key('Luminosity'):   
+    lumi += float(res['Value']['Luminosity'])
+  addinfo = None
+  if 'AdditionalInfo' in res['Value']:
+    addinfo = res['Value']['AdditionalInfo']
+    if addinfo.count("{"):
+      addinfo = eval(addinfo)
+    else:
+      addinfo = DEncode.decode(addinfo)[0]
+  if "NumberOfEvents" in res['Value'].keys():
+    nbevts += int(res['Value']['NumberOfEvents'])
+  return (float(lumi),int(nbevts),addinfo)
 
 def translate(detail):
+  """ Replace whizard naming convention by human conventions
+  """
   detail = detail.replace('e1','e-')
   detail = detail.replace('E1','e+')
   detail = detail.replace('e2','mu-')
@@ -54,6 +62,8 @@ def translate(detail):
   detail = detail.replace('B','bbar')
   detail = detail.replace('Z0','Z')
   detail = detail.replace('Z','Z0')
+  #detail = detail.replace('ql','u:d:s:c:b:U:D:S:C:B')
+  detail = detail.replace('gghad','gamma gamma -> hadrons')
   detail = detail.replace(',','')
   detail = detail.replace('n N','nu nub')
   detail = detail.replace('se--','seL-')
@@ -63,45 +73,224 @@ def translate(detail):
   detail = detail.replace(' H ->',', H ->')
   detail = detail.replace(' Z0 ->',', Z0 ->')
   detail = detail.replace(' W ->',', W ->')
-    
+  
   return detail
 
-metadata = []
+class Params(object):
+  def __init__(self):
+    self.prod = ''
+    self.full_det = False
+    self.verbose = False
+    self.ptypes = ['MCGeneration','MCSimulation','MCReconstruction',"MCReconstruction_Overlay"]
+    self.statuses = ['Active','Stopped','Completed','Archived']
+  def setProdID(self, opt):
+    self.prod = opt
+    return S_OK()
+  def setFullDetail(self,opt):
+    self.full_det = True
+    return S_OK()
+  def setVerbose(self, opt):
+    self.verbose = True
+    return S_OK()
+  def setProdTypes(self, opt):
+    self.ptypes = opt.split(",")
+    return S_OK()
+  def setStatuses(self, opt):
+    self.statuses = opt.split(",")
+    return S_OK()
 
-for prodID in prodids:
-  meta = {}
-  meta['ProdID'] = int(prodID)
+  def registerSwitch(self):
+    Script.registerSwitch("P:", "prod=", "production", self.setProdID)
+    Script.registerSwitch("f", "full_detail", "full detail", self.setFullDetail)
+    Script.registerSwitch("v", "verbose", "verbose output", self.setVerbose)
+    Script.registerSwitch("t:", "types=", "Production Types, comma separated", self.setProdTypes)
+    Script.registerSwitch("S:", "Statuses=", "Statuses, comma separated", self.setStatuses)
+    Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                        '\nUsage:',
+                                        '  %s [option|cfgfile] ...\n' % Script.scriptName ] ) )
 
-  res = fc.findFilesByMetadata(meta)  
-  if not res['OK']:
-    print res['Message']
-    continue
-  lfns = res['Value']
-  nb_files = len(lfns)
-  path = ""
-  if not len(lfns):
-    print "no files found"
-    continue
-  path = os.path.dirname(lfns[0])
-  res = fc.getDirectoryMetadata(path)
-  dirmeta = {}
-  dirmeta['nb_files'] = nb_files
-  dirmeta.update(res['Value'])
-  if not dirmeta.has_key('Luminosity'):
-    dirmeta['Luminosity'] = 0
-  if not dirmeta.has_key('NumberOfEvents'):
-    dirmeta['NumberOfEvents'] = 0
-  #print processesdict[dirmeta['EvtType']]
-  detail = processesdict[dirmeta['EvtType']]['Detail']
+if __name__=="__main__":
+  clip = Params()
+  clip.registerSwitch()
+  Script.parseCommandLine()
+  prod = clip.prod
+  full_detail = clip.full_det
+  fc = FileCatalogClient()
+  
+  processlist = gConfig.getValue('/LocalSite/ProcessListPath')
+  prl = ProcessList(processlist)
+  processesdict = prl.getProcessesDict()
+  
+  trc = TransformationClient()
+  prodids = []
+  if not prod:
+   conddict = {}
+   conddict['Status'] = clip.statuses
+   if clip.ptypes:
+     conddict['Type'] = clip.ptypes
+   res = trc.getTransformations( conddict )
+   if res['OK']:
+     for transfs in res['Value']:
+       prodids.append(transfs['TransformationID'])
+  else:
+    prodids.append(prod)
 
-  dirmeta['detail'] = translate(detail)
-  metadata.append(dirmeta)
+  metadata = []
+  
+  for prodID in prodids:
+    if prodID<700:
+      continue
+    meta = {}
+    meta['ProdID']=int(prodID)
+    res = trc.getTransformation(prodID)
+    if not res['OK']:
+      print "Error getting transformation %s" % prodID 
+      continue
+    prodtype = res['Value']['Type']
+    proddetail = res['Value']['Description']
+    if prodtype == 'MCReconstruction' or prodtype == 'MCReconstruction_Overlay' :
+      meta['Datatype']='DST'
+    if prodtype == 'MCGeneration':
+      meta['Datatype']='gen'
+    if prodtype == 'MCSimulation':
+      meta['Datatype']='SIM'
+    res = fc.findFilesByMetadata(meta)  
+    if not res['OK']:
+      print res['Message']
+      continue
+    lfns = res['Value']
+    nb_files = len(lfns)
+    path = ""
+    if not len(lfns):
+        print "no files found"
+        continue
+    path = os.path.dirname(lfns[0])
+    res = fc.getDirectoryMetadata(path)
+    dirmeta = {}
+    dirmeta['proddetail'] = proddetail    
+    dirmeta['nb_files']=nb_files
+    dirmeta.update(res['Value'])
+    lumi  = 0.
+    nbevts = 0
+    addinfo = None
+    if not full_detail:
+      lfn  = lfns[0]
+      res = getFileInfo(lfn)
+      nbevts = res[1]*len(lfns)
+      lumi = res[0]*len(lfns)
+      addinfo = res[2]
+    else:
+      for lfn in lfns:
+        res = getFileInfo(lfn)
+        lumi += res[0]
+        nbevts += res[1]
+        addinfo = res[2]
+        
+    if nbevts:
+      dirmeta['NumberOfEvents']=nbevts
+    if not lumi:
+      dirmeta['Luminosity']=0
+      dirmeta['CrossSection']=0
+    else:
+      if nbevts:
+        dirmeta['CrossSection']=nbevts/lumi
+      else:
+        dirmeta['CrossSection']=0
+    if addinfo:
+      if 'xsection' in addinfo:
+        if 'xsection' in addinfo['xsection']:
+          dirmeta['CrossSection']=addinfo['xsection']['xsection']
+                
+    if not dirmeta.has_key('NumberOfEvents'):
+      dirmeta['NumberOfEvents']=0
+    #print processesdict[dirmeta['EvtType']]
+    dirmeta['detail']=''
+    if processesdict.has_key(dirmeta['EvtType']):
+      if processesdict[dirmeta['EvtType']].has_key('Detail'):
+        detail = processesdict[dirmeta['EvtType']]['Detail']
+        
+    else:
+      detail=dirmeta['EvtType']
+  
+  
+    if not prodtype == 'MCGeneration':
+      res = trc.getTransformationInputDataQuery(prodID)
+      if res['OK']:
+        if res['Value'].has_key('ProdID'):
+          dirmeta['MomProdID']=res['Value']['ProdID']
+    if not dirmeta.has_key('MomProdID'):
+      dirmeta['MomProdID']=0
+    dirmeta['detail']= translate(detail)
+    metadata.append(dirmeta)
+  
+  detectors = {}
+  detectors['ILD'] = []
+  detectors['SID'] = []
+  detectors['sid'] = []
+  detectors['gen']=[]
+  for channel in metadata:
+    if not channel.has_key('DetectorType'):
+      detectors['gen'].append((channel['detail'],channel['Energy'],channel['ProdID'],channel['nb_files'],channel['NumberOfEvents']/channel['nb_files'],channel['NumberOfEvents'],
+                                 channel['CrossSection'],str(channel['proddetail']))
+                                )
+    else:
+      if not channel['DetectorType'] in detectors:
+        print "This is unknown detector", channel['DetectorType']
+        continue
+      detectors[channel['DetectorType']].append((channel['detail'],channel['Energy'],channel['DetectorType'],channel['ProdID'],channel['nb_files'],channel['NumberOfEvents']/channel['nb_files'],
+                                                     channel['NumberOfEvents'],channel['CrossSection'],channel['MomProdID'],str(channel['proddetail'])))
+  
+  of = file("tables.html","w")
+  of.write("""<!DOCTYPE html>
+<html>
+ <head>
+<title> Production summary </title>
+</head>
+<body>
+""")
+  if len(detectors['gen']):           
+    of.write("<h1>gen prods</h1>\n")
+    t = Table(header_row = ('Channel', 'Energy','ProdID','Tasks','Average Evts/task','Statistics','Cross Section (fb)','Comment'))
+    for item in detectors['gen']:
+      t.rows.append( item )
+    of.write(str(t))
+    if clip.verbose:
+      print "Gen prods"
+      print str(t)
 
-for channel in metadata:
-  print "<tr> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> </tr>" % (channel['detail'],
-                                                                                                channel['Energy'],
-                                                                                                channel['ProdID'],
-                                                                                                channel['nb_files'],
-                                                                                                channel['NumberOfEvents'],
-                                                                                                channel['nb_files'] * channel['NumberOfEvents'],
-                                                                                                channel['nb_files'] * channel['Luminosity'])
+  if len(detectors['ILD']):           
+    of.write("<hr>\n<h1>ILD prods</h1>\n")
+    t = Table(header_row = ('Channel', 'Energy','Detector','ProdID','Number of Files','Events/File','Statistics','Cross Section (fb)','Origin ProdID','Comment'))
+    for item in detectors['ILD']:
+      t.rows.append( item )
+    of.write(str(t))
+    if clip.verbose:
+      print "ILC CDR prods"
+      print str(t)
+  
+  if len(detectors['SID']):           
+    of.write("<hr>\n<h1>SID prods</h1>\n")
+    t = Table(header_row = ('Channel', 'Energy','Detector','ProdID','Number of Files','Events/File','Statistics','Cross Section (fb)','Origin ProdID','Comment'))
+    for item in detectors['SID']:
+      t.rows.append( item )
+    of.write(str(t))
+    if clip.verbose:
+      print "SID CDR prods"
+      print str(t)
+
+  if len(detectors['sid']):           
+    of.write("<hr>\n<h1>sid dbd prods</h1>\n")
+    t = Table(header_row = ('Channel', 'Energy','Detector','ProdID','Number of Files','Events/File','Statistics','Cross Section (fb)','Origin ProdID','Comment'))
+    for item in detectors['sid']:
+      t.rows.append( item )
+    of.write(str(t))
+    if clip.verbose:
+      print "sid DBD prods" 
+      print str(t)
+  
+  of.write("""
+</body>
+</html>
+""")
+  of.close()   
+  dexit(0)
