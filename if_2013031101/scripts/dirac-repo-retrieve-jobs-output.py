4fcbcd09cbb5c308726eaee8f505b674071d0c30
sposs <sposs@cern.ch>
2013-03-11 16:57:12 +0000

    v17r1p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/Interfaces@63078 4525493e-7705-40b1-a816-d608a930855b

---
+++ if_2013031101/scripts/dirac-repo-retrieve-jobs-output.py
@@ -0,0 +1,59 @@
+'''
+Retrieve the output sandboxes of jobs created using the API, stored in the repository file
+
+Created on Mar 24, 2010
+
+@author: sposs
+'''
+from DIRAC.Core.Base import Script
+from DIRAC import S_OK, E_RROR, exit as dexit
+from DIRAC.Interfaces.API.Dirac import Dirac
+
+class Params(object):
+  def __init__(self):
+    self.outputdata = False
+    self.repo = ''
+    
+  def setOuputData(self, opt):
+    self.outputdata = True
+    return S_OK()
+  
+  def setRepo(self, opt):
+    self.repo = opt
+    return S_OK()
+  
+  def registerSwitches(self):
+    Script.registerSwitch( 'd', 'outputdata', 'retrieve also the outputdata', self.setOuputData)
+    Script.registerSwitch( 'r:', 'repository=', 'repository file to use', self.setRepo)
+    Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
+                                        '\nUsage:',
+                                        '  %s [option|cfgfile] ...\n' % Script.scriptName ] )  )
+    
+if __name__=="__main__":
+  repoLocation = ''
+  clip = Params()
+  clip.registerSwitches()
+  Script.parseCommandLine( ignoreErrors = False )
+  repoLocation = clip.repo
+  if not repoLocation:
+    Script.showHelp()
+    dexit(1)
+  
+  dirac = Dirac(True, repoLocation)
+  
+  exitCode = 0
+  res = dirac.monitorRepository(False)
+  if not res['OK']:
+    print "Failed because %s" % res['Message']
+    dexit(1)
+    
+  res = dirac.retrieveRepositorySandboxes()
+  if not res['OK']:
+    print "Failed because %s" % res['Message']
+    dexit(1)
+  if clip.outputdata:
+    res = dirac.retrieveRepositoryData()
+    if not res['OK']:
+      print "Failed because %s" % res['Message']
+      exit(1)
+  dexit(exitCode)
