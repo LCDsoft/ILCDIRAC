4e72b1f6c09e7846aeee8d6ea4f25f1fb8a7ffda
sposs <sposs@cern.ch>
2013-03-11 17:05:21 +0000

    Release v17r1p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v17r1p2@63088 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/API/NewInterface/Examples/ConditionalOverlay.py
@@ -0,0 +1,67 @@
+from DIRAC.Core.Base import Script
+
+Script.parseCommandLine()
+
+from ILCDIRAC.Interfaces.API.NewInterface.Applications import OverlayInput, Marlin
+from ILCDIRAC.Interfaces.API.NewInterface.UserJob import UserJob
+from ILCDIRAC.Interfaces.API.DiracILC import DiracILC
+
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+fc = FileCatalogClient()
+
+meta={}
+meta["ProdID"]=651
+
+res = fc.findFilesByMetadata(meta)
+if not res['OK']:
+    print "Found no files"
+    exit()
+
+lfns = res['Value']    
+print "found %s files"%len(lfns)
+
+
+ovi = OverlayInput() 
+ovi.setEnergy(500.)
+ovi.setBXOverlay(300)
+ovi.setGGToHadInt(0.3)
+ovi.setNbSigEvtsPerJob(10)
+ovi.setDetectorType("ILD")
+
+overlay = [True,False]
+
+for ov in overlay:
+  d = DiracILC(True,"repo_overlay_%s.rep"%ov)
+  for lfn in lfns:
+    j = UserJob()
+    steeringf = "clic_ild_cdr_steering.xml"
+    if ov:
+      steeringf = "clic_ild_cdr_steering_overlay.xml"
+      res = j.append(ovi)
+      if not res['OK']:
+        print res['Message']
+        continue
+    ma = Marlin() 
+    ma.setVersion("v0111Prod")
+    ma.setGearFile("clic_ild_cdr.gear")
+    ma.setSteeringFile(steeringf)
+    ma.setInputFile("LFN:"+lfn)
+    ma.setNbEvts(10)
+    ma.setEnergy(500.)
+    ma.setOutputRecFile("myrec_overlay_%s.slcio"%ov)
+    ma.setOutputDstFile("mydst_overlay_%s.slcio"%ov)
+    res =j.append(ma)
+    if not res['OK']:
+      print res['Message']
+      exit()
+
+    j.setCPUTime(86400)
+    j.setOutputData("myrec_overlay_%s.slcio"%ov,"some/path")
+    j.setName("SomeName")
+    j.setJobGroup("SomeGroup")
+    res = d.checkparams(j)
+    if not res['OK']:
+      print res['Message']
+      exit()
+
+    j.submit(d)
