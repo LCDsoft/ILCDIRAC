733cc57d7c1ffe0cc4ce8f611e3abc581eea78ff
sposs <sposs@cern.ch>
2013-03-18 07:06:40 +0000

    Release v17r2p1
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v17r2p1@63468 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/scripts/dirac-ilc-show-software.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+'''
+Get all software registered in the Configuration service
+
+Created on Dec 17, 2010
+
+@author: sposs
+'''
+from DIRAC.Core.Base import Script
+
+if __name__=="__main__":
+  Script.parseCommandLine()
+  from DIRAC import gConfig, gLogger
+  base = '/Operations/Defaults/AvailableTarBalls'
+
+  platforms = gConfig.getSections(base)
+  
+  for platform in platforms['Value']:
+    gLogger.notice("For platform %s, here are the available software" % platform)
+    apps = gConfig.getSections(base + "/" + platform)
+    for app in apps['Value']:
+      gLogger.notice("   - %s" % app)
+      versions = gConfig.getSections(base + "/" + platform + "/" + app)
+      for vers in  versions['Value']:
+        gLogger.notice("     * %s" % vers)
+        depsb = gConfig.getSections(base + "/" + platform + "/" + app + "/" + vers)
+        if len(depsb['Value']):
+          gLogger.notice("       Depends on")
+          deps = gConfig.getSections(base + "/" + platform + "/" + app + "/" + vers + "/Dependencies")
+          for dep in deps['Value']:
+            depversions = gConfig.getOption(base + "/" + platform + "/" + app + "/" + vers + "/Dependencies/" + dep + "/version")
+            gLogger.notice("         %s %s" % (dep, depversions['Value']))
+                      
+      if not len(versions['Value']):
+        gLogger.notice("      No version available")
+
