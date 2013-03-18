6d4e9a7c139c3573469b68fcf9426c94c05f57ac
sposs <sposs@cern.ch>
2013-03-18 13:24:29 +0000

    Release v17r2p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v17r2p2@63496 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/FindSteeringFileDir.py
@@ -0,0 +1,36 @@
+'''
+Otain the related steering file directory given a certain 
+software name, version and system config
+
+Created on Feb 10, 2012
+
+@author: Stephane Poss
+@since: Feb 10, 2012
+'''
+
+from DIRAC import S_OK, S_ERROR
+from ILCDIRAC.Core.Utilities.CombinedSoftwareInstallation  import getSoftwareFolder
+from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+from ILCDIRAC.Core.Utilities.TARsoft import check
+
+def getSteeringFileDirName(systemConfig, application, applicationVersion):
+  """ Locate the path of the steering file directory assigned to the specified application
+  """
+  ops = Operations()
+  version = ops.getValue('/AvailableTarBalls/%s/%s/%s/Dependencies/steeringfiles/version' % (systemConfig, 
+                                                                                            application,
+                                                                                            applicationVersion), '')
+  if not version: 
+    return S_ERROR("Could not find attached SteeringFile version")
+  TarBall = ops.getValue('/AvailableTarBalls/%s/steeringfiles/%s/TarBall' % (systemConfig, version), '')
+  if not TarBall:
+    return S_ERROR("Could not find tar ball for SteeringFile")
+  mydir = TarBall.replace(".tgz", "").replace(".tar.gz", "")
+  res = getSoftwareFolder(mydir)
+  if not res['OK']:
+    return res
+  mySoftDir = res['Value']
+  res = check('steeringfiles.%s'%version,'.',[mySoftDir])##check that all the files are there: software is not corrupted.
+  if not res['OK']:
+    return res
+  return S_OK(mySoftDir)
