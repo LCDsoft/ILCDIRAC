4ee7e8093e70e7faec03db9659684f6aa5563753
sposs <sposs@cern.ch>
2013-03-25 12:41:34 +0000

    Release v17r3p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v17r3p2@63703 4525493e-7705-40b1-a816-d608a930855b

---
+++ ProcessProductionSystem/Agent/ProductionSummaryAgent.py
@@ -0,0 +1,32 @@
+#####################################################################
+# $HeadURL: $
+#####################################################################
+'''
+Production Summary agent: writes out every day the production status 
+
+Created on Apr 8, 2011
+
+@author: Stephane Poss
+'''
+__RCSID__ = "$ Id: $"
+
+from DIRAC                                                                import S_OK, gMonitor
+from DIRAC.Core.Base.AgentModule                                          import AgentModule
+from DIRAC.TransformationSystem.Client.TransformationClient               import TransformationClient
+from DIRAC.Resources.Catalog.FileCatalogClient                            import FileCatalogClient
+
+AGENT_NAME = 'Transformation/ProductionSummaryAgent'
+
+class ProductionSummaryAgent( AgentModule ):
+  """ Agent to produce the summary table that should be available in the twiki
+  """
+  def initialize(self):
+    self.pollingTime = self.am_getOption('PollingTime', 86400)
+    gMonitor.registerActivity("Iteration", "Agent Loops", AGENT_NAME, "Loops/min", gMonitor.OP_SUM)
+    self.transClient = TransformationClient('TransformationDB')
+    self.fc = FileCatalogClient()
+    return S_OK()
+
+  ##############################################################################
+  def execute(self):
+    return S_OK()
