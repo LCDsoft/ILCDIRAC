158911877fb793b443e1eb081ff2653e69f58b39
sposs <sposs@cern.ch>
2013-03-26 16:49:18 +0000

    Remove most of the content as it is in UserJob
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@63798 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/API/NewInterface/ILDUserJob.py
+++ Interfaces/API/NewInterface/ILDUserJob.py
@@ -6,25 +6,10 @@ User Job class. Used to define (guess what?) user jobs!
 @author: Remi Ete
 @author: Ching Bon Lam
 '''
-
-from ILCDIRAC.Interfaces.API.NewInterface.UserJob import UserJob
-from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
-from DIRAC import S_OK
-
-
-class ILDUserJob(UserJob):
+from DIRAC import gLogger
+class ILDUserJob(object):
   """ User job class. To be used by users, not for production.
   """
   def __init__(self, script = None):
-    super(ILDUserJob, self).__init__( script )
-    self.type = 'User'
-    self.diracinstance = None
+    gLogger.notice("Use UserJob instead")
     
-  def setILDConfig(self,Version):
-    """ Define the Configuration package to obtain
-    """
-    appName = 'ildconfig'
-    self._addSoftware(appName, Version)
-    
-    self._addParameter( self.workflow, 'ILDConfigPackage', 'JDL', appName+Version, 'ILDConfig package' )
-    return S_OK()
