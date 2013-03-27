1a1f8c0aa50a82c7c232475914648a52c1f099db
sposs <sposs@cern.ch>
2013-03-27 16:43:35 +0000

    Release v17r3p6
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v17r3p6@63878 4525493e-7705-40b1-a816-d608a930855b

---
+++ TransformationSystem/Client/Transformation.py
@@ -0,0 +1,12 @@
+"""
+This class is needed to get the Limited plugin support.
+"""
+
+from DIRAC.TransformationSystem.Client.Transformation import Transformation as DT
+
+class Transformation(DT):
+  def __init__(self, transID = 0, transClient = None):
+    super( Transformation, self ).__init__(transID = 0, transClient = None)
+    self.supportedPlugins += ['Limited']
+  def _checkLimitedPlugin( self ):
+    return self._checkStandardPlugin()
