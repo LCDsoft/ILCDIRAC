4e53664181697c0a119b7e680d26f7cca071e49c
sposs <sposs@cern.ch>
2013-03-28 23:28:36 +0000

    Quite a lot of changes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/trunk/ILCDIRAC@63905 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-ilc-get-info.py
+++ Interfaces/scripts/dirac-ilc-get-info.py
@@ -29,4 +29,104 @@ if __name__ == "__main__":
   clip.registerSwitch()
   Script.parseCommandLine(script = True)
   
+  if not clip.prodid and not clip.file:
+    Script.showHelp()
+    dexit(1)
+  
+  from DIRAC import gLogger
+  import os
+  
+  from DIRAC.TransformationSystem.Client.TransformationClient import TransformationClient
+  tc = TransformationClient()  
+
+  from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+  fc = FileCatalogClient()
+  fmeta = {}
+
+  if clip.prodid:
+    res = tc.getTransformation(clip.prodid)
+    if not res['OK']:
+      gLogger.error(res['Message'])
+      dexit(1)
+    trans = res['Value']
+    #do something with transf
+    res = fc.findDirectoriesByMetadata({'ProdID':clip.prodid})
+    if res['OK']:
+      if len(res['Value'].values()):
+        for dirs in res['Value'].values():
+          res = fc.getDirectoryMetadata(dirs)
+          if res['OK']:
+            fmeta.update(res['Value'])
+          else:
+            gLogger.warn("Failed to get dir metadata")
+          res = fc.listDirectory(dirs)
+          if not res['OK']:
+            continue
+          content = res['Value']['Successful'][dirs]
+          if content["Files"]:
+            for f_ex in content["Files"].keys():
+              res = fc.getFileUserMetadata(f_ex)
+              if res['OK']:
+                fmeta.update(res['Value'])
+                break
+          
+            
+      
+
+  
+  if clip.file:
+    f = clip.file
+    if f.count("/"):
+      fpath = os.path.dirname(f)
+      res = fc.getDirectoryMetadata(fpath)
+      if not res['OK']:
+        gLogger.error(res['Message'])
+        dexit(0)
+      fmeta.update(res['Value'])
+      res = fc.getFileUserMetadata(f)
+      if not res['OK']:
+        gLogger.error(res['Message'])
+        dexit(1)
+      fmeta.update(res['Value'])
+    else:
+      ext = f.split(".")[-1]
+      fitems = []
+      [fitems.extend(i.split('_')) for i in a.split('.')[:-1]]
+      pid = ''
+      if ext == 'stdhep':
+        pid = fitems[fitems.index('gen')+1]
+        print pid
+      if ext == 'slcio':
+        if 'rec' in fitems:
+          pid = fitems[fitems.index('rec')+1]
+        elif 'dst' in fitems:
+          pid = fitems[fitems.index('dst')+1]
+        elif 'sim' in fitems:
+          pid = fitems[fitems.index('sim')+1]
+        else:
+          gLogger.error("This file does not follow the ILCDIRAC production conventions!")
+          gLogger.error("Please specify a prod ID directly or check the file.")
+          dexit(0)
+        print pid
+      
+      res = fc.findDirectoriesbyMetadata({'ProdID':int(pid)})
+      if not res['OK']:
+        gLogger.error(res['Message'])
+        dexit(1)
+      dir_ex = res['Value'].values()[0]
+      res = fc.getDirectoryMetadata(dir_ex)
+      if not res['OK']:
+        gLogger.error(res['Message'])
+      else:
+        fmeta.update(res['Value'])
+      
+      res = tc.getTransformation(pid)
+      if not res['OK']:
+        gLogger.error(res['Message'])
+        gLogger.error('Will proceed anyway')
+      else:
+        trans = res['Value']
+        
+  gLogger.notice(str(fmeta))
+      
   dexit(0)
