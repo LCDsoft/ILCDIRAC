f3d18ff115073aad53309e7b35f2a92331de6606
sposs <sposs@cern.ch>
2013-04-03 08:19:29 +0000

    Release v18r0p0
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/ILCDIRAC/tags/ILCDIRAC/v18r0p0@63968 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/__init__.py
@@ -0,0 +1,9 @@
+# $HeadURL$
+# $Id$
+"""
+All Core utilities (scripts and classes) that are used here and there
+
+This contains the LCD specific Core.
+   
+Created by S Poss, 2010/01/13
+"""
---
+++ Core/scripts/ilcdirac-install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+wget -O dirac-install -np  https://raw.github.com/DIRACGrid/DIRAC/master/Core/scripts/dirac-install.py  --no-check-certificate
+chmod +x dirac-install
+./dirac-install -V ILCDIRAC
+
+source bashrc
+dirac-proxy-init -x
+
+vo=ilc
+setup=ILC-Production
+csserver=dips://volcd01.cern.ch:9135/Configuration/Server
+
+dirac-configure -V $vo -S $setup -C $csserver -d --SkipCAChecks
+echo ""
+echo "To get the proper environment, run source bashrc"
+echo ""
+echo "You can now obtain a dirac proxy by running"
+echo "dirac-proxy-init"
