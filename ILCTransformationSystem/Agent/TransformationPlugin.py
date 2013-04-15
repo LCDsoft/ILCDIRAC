1cff49b88e8e91f6fa0f3efa9a32e9ac37688371
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2013-04-15 14:52:30 +0000

    Add the 2 new plugins: Sliced and SlicedLimited

--- ILCTransformationSystem/Agent/TransformationPlugin.py
+++ ILCTransformationSystem/Agent/TransformationPlugin.py
@@ -41,3 +41,44 @@ class TransformationPlugin(DTP):
         break
     return S_OK( newTasks )
     
+  def _Sliced(self):
+    """ 
+    Handle the slicing, in fact do nothing particular
+    """
+    
+    lfns = self.data
+    #self.data is the dict of replicas for each file d[lfn]=[replicas]
+    newTasks = []
+    for lfn in lfns.keys():
+      newTasks.append( ( '', [lfn] ) )
+
+    return S_OK( newTasks)  
+  
+  def _SlicedLimited(self):
+    """
+    For the Sliced productions
+    Limit the number of tasks created to the MaxNumberOfTasks 
+    Get the total number of tasks submitted
+    Check if that's bigger than the MaxNumberOfTasks
+    Extend by the number of tasks if needed to reach MaxNumberOfTasks
+    """
+    max_tasks = self.params['MaxNumberOfTasks']
+    res = self.transClient.getCounters( 'TransformationFiles', ['Status'], 
+                                        {'TransformationID':self.params['TransformationID']} )
+    if not res['OK']:
+      return res
+    total_used = 0
+    for statustup in res['Value']:
+      if statustup[0]['Status'] in ['Assigned', 'Processed']:
+        total_used += statustup[1]
+    if total_used >= max_tasks and max_tasks > 0:
+      return S_ERROR('Too many tasks for this transformation')
+    lfns = self.data
+    newTasks = []
+    for lfn in lfns.keys():
+      newTasks.append( ( '', lfn ) )
+      total_used += 1
+      if total_used >= max_tasks and max_tasks > 0:
+        break
+    return S_OK( newTasks )
+    
