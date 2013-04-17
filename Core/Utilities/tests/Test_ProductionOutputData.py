88d27e26dd818d59f48657d870dfe86c7044f4f9
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2013-04-17 08:44:45 +0000

    Add a couple of tests

---
+++ Core/Utilities/tests/Test_ProductionOutputData.py
@@ -0,0 +1,66 @@
+import unittest
+from ILCDIRAC.Core.Utilities.ProductionData import constructProductionLFNs
+
+class ProductionOutputDataTestCase( unittest.TestCase ):
+  """ Base class for the test cases
+  """
+  def setUp( self ):
+    pass
+  def tearDown( self ):
+    pass
+  
+  def test_contructProductionLFNBad(self):
+    commons = {}
+    result = constructProductionLFNs(commons)
+    self.assertEqual( result['OK'], False)
+    
+  def test_contructProductionLFNstdhep(self):
+    commons = {}
+    commons['PRODUCTION_ID'] = 12345
+    commons['JOB_ID'] = 1234
+    commons['outputList'] = [{'outputFile':"something_gen.stdhep",
+                              'outputPath':'/ilc/prod/clic/test/gen'}]
+    result = constructProductionLFNs(commons)
+
+    self.assertEqual( result['OK'], True)
+
+
+  def test_contructProductionLFNsim(self):
+    commons = {}
+    commons['PRODUCTION_ID'] = 12345
+    commons['JOB_ID'] = 1234
+    commons['outputList'] = [{'outputFile':"something_sim.slcio",
+                              'outputPath':'/ilc/prod/clic/test/SIM'}]
+    result = constructProductionLFNs(commons)
+    self.assertEqual( result['OK'], True)
+    
+  def test_contructProductionLFNrec(self):
+    commons = {}
+    commons['PRODUCTION_ID'] = 12345
+    commons['JOB_ID'] = 1234
+    commons['outputList'] = [{'outputFile':"something_rec.slcio",
+                              'outputPath':'/ilc/prod/clic/test/REC'},
+                             {'outputFile':"something_dst.slcio",
+                              'outputPath':'/ilc/prod/clic/test/DST'}]
+    result = constructProductionLFNs(commons)
+    self.assertEqual( result['OK'], True)
+
+  def test_contructProductionLFNoutput(self):
+    commons = {}
+    commons['PRODUCTION_ID'] = 12345
+    commons['JOB_ID'] = 1234
+    commons['outputList'] = [{'outputFile':"something_gen.stdhep",
+                              'outputPath':'/ilc/prod/clic/test/gen'}]
+    result = constructProductionLFNs(commons)
+    res = {'ProductionOutputData' : ["/ilc/prod/clic/test/gen/00012345/001/something_gen_12345_1234.stdhep"], 
+           'LogFilePath' : ["/ilc/prod/clic/test/gen/00012345/LOG/001"],
+            'LogTargetPath' : ["/ilc/prod/clic/test/gen/00012345/LOG/00012345_1234.tar"], 
+            'DebugLFNs' : []}
+    for key in res.keys():
+      self.assertEqual( result['Value'][key], res[key])
+        
+    
+if __name__ == '__main__':
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase( ProductionOutputDataTestCase )
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
+  
---
+++ Core/Utilities/tests/Test_ResolvePathsandNames.py
@@ -0,0 +1,43 @@
+import unittest
+
+from ILCDIRAC.Core.Utilities.resolvePathsAndNames import getProdFilename, resolveIFpaths
+import os, tempfile, shutil
+
+class ResolvePathsAndNamesTests( unittest.TestCase ):
+  def setUp(self):
+    """
+    Make fake files for the test
+    """
+    self.inputfiles = ["toto_gen_12345_123.txt"]
+    self.dir = tempfile.mkdtemp(dir = ".")
+    self.realloc = os.path.join(self.dir,"toto_gen_12345_123.txt")
+    inputf = file(self.realloc,"w")
+    inputf.write("fake file")
+    inputf.close()
+    
+  def tearDown(self):
+    """ Remove the fake files
+    """
+    try:
+      bd = os.path.dirname(self.realloc)
+      os.remove(self.realloc)
+      shutil.rmtree(bd)
+    except:
+      print "failed to remove file"
+    
+  def test_getnames(self):
+    res = getProdFilename("toto_gen.stdhep", 12345,123)
+    self.assertEqual(res,'toto_gen_12345_123.stdhep')
+  
+  def test_resolvepaths(self):
+    res = resolveIFpaths(self.inputfiles)
+    self.assertTrue('OK' in res)
+    self.assertEqual(res['OK'], True, res)
+    self.assertTrue('Value' in res, res.keys() )
+    self.assertEqual(res['Value'], [ os.path.abspath(self.realloc) ])
+    
+    
+if __name__ == "__main__":
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase( ResolvePathsAndNamesTests )
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
+  
