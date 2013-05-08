3aa59f9acde29939a89be3c16c5d2378f018d281
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2013-05-08 13:08:35 +0000

    Add script to list the members of the ILC VO.

---
+++ Core/scripts/dirac-ilc-list-users.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+""" List the VO members
+"""
+
+def setUser(opt):
+  from DIRAC import S_OK
+  global username
+  username = opt
+  return S_OK()
+
+if __name__ == "__main__":
+  from DIRAC.Core.Base import Script
+  username = None
+  Script.registerSwitch("u:", "UserName", "Family name of the user", setUser)  
+  Script.parseCommandLine()
+  
+  from DIRAC.Core.Security.VOMSService import VOMSService
+  from DIRAC import gLogger, exit as dexit
+  v = VOMSService()
+  res = v.admListMembers()
+  if not res['OK']:
+    gLogger.error(res['Message'])
+    dexit(1)
+  users = res['Value']
+  for user in users:
+    if not username:
+      gLogger.notice("%s, %s, %s" % (user['DN'], user['CA'], user['mail']))
+    else:
+      if user['DN'].lower().count(username.lower()):
+        gLogger.notice("%s, %s, %s" % (user['DN'], user['CA'], user['mail']))
+        break 
