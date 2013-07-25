fc319fe718ed8650466341f247a0af3eed925dce
Andre Sailer <andre.philippe.sailer@cern.ch>
2013-07-25 16:40:15 +0000

    make new tag

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ __path__ = extend_path(__path__, __name__)
 
 majorVersion = 19
 minorVersion = 1
-patchLevel   = 7
+patchLevel   = 8
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
