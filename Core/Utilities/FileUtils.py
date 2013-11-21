ad4bd001d3e0968568a2bd06e25587bd2fd61df4
Stephane Guillaume Poss <stephane.guillaume.poss@cern.ch>
2013-11-21 12:07:57 +0000

    use reg exp to prevent users from copying everything

--- Core/Utilities/FileUtils.py
+++ Core/Utilities/FileUtils.py
@@ -6,7 +6,7 @@ Several file utilities
 '''
 __RCSID__ = "$Id$"
 
-import os, shutil, glob
+import os, shutil, glob, re
 from distutils import dir_util, errors
 
 from DIRAC import S_OK, S_ERROR, gLogger
@@ -70,13 +70,18 @@ def upload(path, appTar):
 def fullCopy(srcdir, dstdir, item):
   """ Copy the item from srcdir to dstdir, creates missing directories if needed
   """
-  item = item.lstrip("/").rstrip("/")
+  item = item.rstrip().lstrip().lstrip("./").rstrip("/")
   srcdir = srcdir.rstrip("/")
   dstdir = dstdir.rstrip("/")
+  if not re.match(r"(.*)[a-zA-Z0-9]+(.*)", item):#we want to have explicit elements
+    gLogger.error("You try to get all files, that cannot happen")
+    return S_OK()
   src = os.path.join(srcdir, item)
   items = glob.glob(src)
   if not items:
+    gLogger.error("No items found matching", src)
     return S_ERROR("No items found!")
+  
   for item in items:
     item = item.lstrip(srcdir).lstrip("/")
     dst = os.path.join(dstdir, item)
