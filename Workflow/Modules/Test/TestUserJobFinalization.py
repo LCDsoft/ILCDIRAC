f0e7f9922be099d11875034092f97730b2b1ee1f
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-08-01 14:50:11 +0200

    TestUserJobFinalization, can use instance attributes instead of workflow_commons again

--- Workflow/Modules/Test/TestUserJobFinalization.py
+++ Workflow/Modules/Test/TestUserJobFinalization.py
@@ -17,12 +17,13 @@ MYUJF.workflow_commons['TotalSteps'] = 1
 MYUJF.workflow_commons['Owner'] = 'sailer'
 MYUJF.workflow_commons['VO'] = 'ilc'
 MYUJF.workflow_commons['UserOutputSE'] = ""
-MYUJF.workflow_commons['IgnoreAppError'] = True
-MYUJF.workflow_commons['UserOutputData'] = ['file1.txt','file2.txt']
 MYUJF.step_commons['STEP_NUMBER'] = 1
 MYUJF.enable = False
 MYUJF.log.setLevel("DEBUG")
 MYUJF.workflow_commons["UserOutputSE"] = "CERN-SRM;CERN-DIP-3"
+
+MYUJF.ignoreapperrors = True
+MYUJF.userOutputData  = ['file1.txt','file2.txt']
 
 RESULT = MYUJF.execute()
 if 'Message' in RESULT:
