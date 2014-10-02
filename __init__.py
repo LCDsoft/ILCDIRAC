ffe80765a308e4ddccf8253334e7b4c103d648f7
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-10-02 18:45:59 +0200

    Add new release versions v22r0p0 and v21r5p2

--- __init__.py
+++ __init__.py
@@ -8,9 +8,9 @@ __path__ = extend_path(__path__, __name__)
 
 # Define Version
 
-majorVersion = 21
-minorVersion = 5
-patchLevel   = 1
+majorVersion = 22
+minorVersion = 0
+patchLevel   = 0
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
