996ec94f38647df0a548d3cab31e9b0f14505dd0
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-10-17 19:02:05 +0200

    Remove ILCJob, has been deprecated long enough

--- Interfaces/API/ILCJob.py
+++
@@ -1,8 +0,0 @@
-"""
-Forget this, it should NOT be used
-"""
-from DIRAC import gLogger
-class ILCJob(object):
-  def __init__(self):
-    gLogger.error("This class is now obsolete, please move to the new interface. Contact ilc-dirac@cern.ch for assistance if needed.")
-    
