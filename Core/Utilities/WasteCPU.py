08ea4796ba5b666c736f6cc16583e437128f8f41
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-10-20 14:42:42 +0200

    Core/Utilities/WasteCPU: pylint

--- Core/Utilities/WasteCPU.py
+++ Core/Utilities/WasteCPU.py
@@ -4,6 +4,7 @@ This is the worst piece of code ever NEEDED: just run some CPU intensive code to
 @author: Stephane Poss
 @since: Jul 26, 2011
 '''
+__RCSID__ = "$Id$"
 import time
 from math import log
 from DIRAC import S_OK, S_ERROR
@@ -11,15 +12,15 @@ from DIRAC import S_OK, S_ERROR
 def WasteCPUCycles(timecut):
   """ Waste, waste, and waste more CPU.
   """
-  a = 1e31
+  number = 1e31
   first = time.clock()
   while time.clock()-first < timecut:
     try:
-      a = log(a)
-    except Exception, x:
-      return S_ERROR("Failed to waste %s CPU seconds:%s" % (timecut, str(x)))  
-    if a < 0:
-      a = -a
-    if a == 0:
-      a = 4  
-  return S_OK("Successfully wasted %s seconds" % timecut)    
+      number = log(number)
+    except ValueError as x:
+      return S_ERROR("Failed to waste %s CPU seconds:%s" % (timecut, str(x)))
+    if number < 0:
+      number = -number
+    if number == 0:
+      number = 4
+  return S_OK("Successfully wasted %s seconds" % timecut)
