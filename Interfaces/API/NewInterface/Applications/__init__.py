f193cb1f1303e01723f48f0675ec2bc06ebc6192
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 20:34:17 +0100

    Move RootMacro to its own file

--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -3,9 +3,10 @@ Applications module
 """
 __RCSID__ = "$Id"
 
-__all__ = ['GenericApplication', 'GetSRMFile', '_Root', 'RootScript']
+__all__ = ['GenericApplication', 'GetSRMFile', '_Root', 'RootScript', 'RootMacro']
 
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GenericApplication import GenericApplication
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GetSRMFile import GetSRMFile
 from ILCDIRAC.Interfaces.API.NewInterface.Applications._Root import _Root
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.RootScript import RootScript
+from ILCDIRAC.Interfaces.API.NewInterface.Applications.RootMacro import RootMacro
