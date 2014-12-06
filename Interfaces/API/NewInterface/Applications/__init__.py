41b24a1af831be860c4a03c54c939e03b177107a
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 20:38:22 +0100

    Move Pythia to its own file

--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -4,7 +4,7 @@ Applications module
 __RCSID__ = "$Id"
 
 __all__ = ['GenericApplication', 'GetSRMFile', '_Root', 'RootScript', 'RootMacro',
-           'Whizard']
+           'Whizard', 'Pythia']
 
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GenericApplication import GenericApplication
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GetSRMFile import GetSRMFile
@@ -12,3 +12,4 @@ from ILCDIRAC.Interfaces.API.NewInterface.Applications._Root import _Root
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.RootScript import RootScript
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.RootMacro import RootMacro
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.Whizard import Whizard
+from ILCDIRAC.Interfaces.API.NewInterface.Applications.Pythia import Pythia
