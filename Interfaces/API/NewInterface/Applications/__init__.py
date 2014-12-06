5ec20698ef07bc7ae07115ae56e53ac8dfea62b4
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 20:36:29 +0100

    Move Whizard to its own file

--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -3,10 +3,12 @@ Applications module
 """
 __RCSID__ = "$Id"
 
-__all__ = ['GenericApplication', 'GetSRMFile', '_Root', 'RootScript', 'RootMacro']
+__all__ = ['GenericApplication', 'GetSRMFile', '_Root', 'RootScript', 'RootMacro',
+           'Whizard']
 
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GenericApplication import GenericApplication
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GetSRMFile import GetSRMFile
 from ILCDIRAC.Interfaces.API.NewInterface.Applications._Root import _Root
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.RootScript import RootScript
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.RootMacro import RootMacro
+from ILCDIRAC.Interfaces.API.NewInterface.Applications.Whizard import Whizard
