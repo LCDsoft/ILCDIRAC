55daa560abbd1cf8b9c9bd2a908f30c42c109aa5
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 21:52:51 +0100

    Move Applications documentation to __init__

--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -1,5 +1,37 @@
 """
-Applications module
+This module contains the definition of the different applications that can
+be used to create jobs.
+
+Example usage:
+
+>>> from ILCDIRAC.Interfaces.API.NewInterface.Applications import *
+>>> from ILCDIRAC.Interfaces.API.NewInterface.UserJob import *
+>>> from ILCDIRAC.Interfaces.API.DiracILC import DiracILC
+>>> dirac = DiracILC()
+>>> job = UserJob()
+>>> ga = GenericApplication()
+>>> ga.setScript("myscript.py")
+>>> ga.setArguments("some arguments")
+>>> ga.setDependency({"mokka":"v0706P08","marlin":"v0111Prod"})
+>>> job.append(ga)
+>>> job.submit(dirac)
+
+It's also possible to set all the application's properties in the constructor
+
+>>> ga = GenericApplication({"Script":"myscript.py", "Arguments":"some arguments", \
+         "Dependency":{"mokka":"v0706P08","marlin":"v0111Prod"}})
+
+but this is more an expert's functionality.
+
+Running:
+
+>>> help(GenericApplication)
+
+prints out all the available methods.
+
+@author: Stephane Poss
+@author: Remi Ete
+@author: Ching Bon Lam
 """
 __RCSID__ = "$Id"
 
