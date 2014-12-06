c1bc6be04c892459f0f37b0028918fc44742acff
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 20:24:38 +0100

    Move _Root to its own file

--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -3,7 +3,8 @@ Applications module
 """
 __RCSID__ = "$Id"
 
-__all__ = ['GenericApplication', 'GetSRMFile']
+__all__ = ['GenericApplication', 'GetSRMFile', '_Root']
 
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GenericApplication import GenericApplication
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GetSRMFile import GetSRMFile
+from ILCDIRAC.Interfaces.API.NewInterface.Applications._Root import _Root
