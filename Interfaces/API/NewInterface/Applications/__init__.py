07c6814830e3f13c6b6528a1b3ca7f0d36d87b45
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 20:22:31 +0100

    Move GetSRMFile to its own file

--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -3,6 +3,7 @@ Applications module
 """
 __RCSID__ = "$Id"
 
-__all__ = ['GenericApplication']
+__all__ = ['GenericApplication', 'GetSRMFile']
 
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GenericApplication import GenericApplication
+from ILCDIRAC.Interfaces.API.NewInterface.Applications.GetSRMFile import GetSRMFile
