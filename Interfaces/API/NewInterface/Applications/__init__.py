ffd87ebba11791cb1bf5c3e7eb25e2e88591d8bc
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 20:57:49 +0100

    Mova Mokka to its own file

--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -4,7 +4,8 @@ Applications module
 __RCSID__ = "$Id"
 
 __all__ = ['GenericApplication', 'GetSRMFile', '_Root', 'RootScript', 'RootMacro',
-           'Whizard', 'Pythia', 'PostGenSelection', 'StdhepCut', 'StdhepCutJava']
+           'Whizard', 'Pythia', 'PostGenSelection', 'StdhepCut', 'StdhepCutJava',
+           'Mokka']
 
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GenericApplication import GenericApplication
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GetSRMFile import GetSRMFile
@@ -16,3 +17,4 @@ from ILCDIRAC.Interfaces.API.NewInterface.Applications.Pythia import Pythia
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.PostGenSelection import PostGenSelection
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.StdhepCut import StdhepCut
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.StdhepCutJava import StdhepCutJava
+from ILCDIRAC.Interfaces.API.NewInterface.Applications.Mokka import Mokka
