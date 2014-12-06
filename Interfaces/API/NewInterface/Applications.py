f7ed2d46deac012e68532c5dbe530c3512e446e5
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 20:31:14 +0100

    Move RootScript to its own file

--- Interfaces/API/NewInterface/Applications.py
+++ Interfaces/API/NewInterface/Applications.py
@@ -52,43 +52,6 @@ import types, os
 __RCSID__ = "$Id$"
 
 #################################################################
-#            Root Script Application: use a script in the 
-#                    Root application framework
-#################################################################  
-class RootScript(_Root):
-  """ Run a script (root executable or shell) in the root application environment. 
-  
-  Example:
-  
-  >>> rootsc = RootScript()
-  >>> rootsc.setScript("myscript.exe")
-  >>> rootsc.setArguments("some command line arguments")
-  
-  The ExtraCLIArguments is not used here, only use the Arguments
-  """
-  def __init__(self, paramdict = None):
-    self.script = None
-    super(RootScript, self).__init__( paramdict )
-    self._modulename = "RootExecutableAnalysis"
-    self.appname = 'root'
-    self._moduledescription = 'Root application script'
-      
-      
-  def setScript(self, executable):
-    """ Define executable to use
-    
-    @param executable: Script to run on. Can be shell or root executable. Must be a local file.
-    @type executable: string
-    """
-    self._checkArgs( { 'executable' : types.StringTypes } )
-
-    self.Script = executable
-    if os.path.exists(executable) or executable.lower().count("lfn:"):
-      self.inputSB.append(executable)
-    return S_OK()
-    
-
-#################################################################
 #            Root Macro Application: use a macro in the 
 #                   Root application framework
 #################################################################  
--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -3,8 +3,9 @@ Applications module
 """
 __RCSID__ = "$Id"
 
-__all__ = ['GenericApplication', 'GetSRMFile', '_Root']
+__all__ = ['GenericApplication', 'GetSRMFile', '_Root', 'RootScript']
 
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GenericApplication import GenericApplication
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GetSRMFile import GetSRMFile
 from ILCDIRAC.Interfaces.API.NewInterface.Applications._Root import _Root
+from ILCDIRAC.Interfaces.API.NewInterface.Applications.RootScript import RootScript
