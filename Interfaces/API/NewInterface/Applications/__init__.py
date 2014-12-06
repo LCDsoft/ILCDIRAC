d6dbbbd4709f0067d861b45abc4199553524b40a
Andre Sailer <andre.philippe.sailer@cern.ch>
2014-12-06 20:53:34 +0100

    Move StdhepCut to its own file

--- Interfaces/API/NewInterface/Applications/__init__.py
+++ Interfaces/API/NewInterface/Applications/__init__.py
@@ -4,7 +4,7 @@ Applications module
 __RCSID__ = "$Id"
 
 __all__ = ['GenericApplication', 'GetSRMFile', '_Root', 'RootScript', 'RootMacro',
-           'Whizard', 'Pythia', 'PostGenSelection']
+           'Whizard', 'Pythia', 'PostGenSelection', 'StdhepCut']
 
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GenericApplication import GenericApplication
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.GetSRMFile import GetSRMFile
@@ -14,3 +14,4 @@ from ILCDIRAC.Interfaces.API.NewInterface.Applications.RootMacro import RootMacr
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.Whizard import Whizard
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.Pythia import Pythia
 from ILCDIRAC.Interfaces.API.NewInterface.Applications.PostGenSelection import PostGenSelection
+from ILCDIRAC.Interfaces.API.NewInterface.Applications.StdhepCut import StdhepCut
