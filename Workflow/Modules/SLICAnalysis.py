ad77fc0f20cdb3158444595146d4376aeca25ba3
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-01-28 16:03:50 +0100

    Add newline to seperate unconnected statements

--- Workflow/Modules/SLICAnalysis.py
+++ Workflow/Modules/SLICAnalysis.py
@@ -135,6 +135,7 @@ class SLICAnalysis(ModuleBase):
     if not os.path.exists(self.detectorModel + ".zip"):
       self.log.error('Detector model %s was not found neither locally nor on the web, exiting' % self.detectorModel)
       return S_ERROR('Detector model %s was not found neither locally nor on the web, exiting' % self.detectorModel)
+
     try:
       unzip_file_into_dir(open(self.detectorModel + ".zip"), os.getcwd())
     except (RuntimeError, OSError): #RuntimeError is for zipfile
