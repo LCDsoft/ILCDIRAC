26d31e042bdfa61f873477233483184d4675b93a
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-08-14 18:43:03 +0200

    PrepareLibs: make pylint compliant

--- Core/Utilities/PrepareLibs.py
+++ Core/Utilities/PrepareLibs.py
@@ -4,10 +4,12 @@ Remove any system library provided in the application tar ball
 :author: sposs
 :since: Jan 26, 2011
 '''
+__RCSID__ = "$Id$"
+
 from DIRAC import gLogger
 import os
 
-files_to_remove = ["libc.so","libc-2.5","libm.so","libpthread.so","libdl.so", "libstdc++.so", "libgcc_s.so.1"]
+FILES_TO_REMOVE = ["libc.so","libc-2.5","libm.so","libpthread.so","libdl.so", "libstdc++.so", "libgcc_s.so.1"]
 
 def removeLibc(libraryPath):
   """ Remove libraries that can be problematic, like libc.so
@@ -17,22 +19,22 @@ def removeLibc(libraryPath):
   """
 
   gLogger.debug("RemoveLibC: Trying to remove these libraries:")
-  gLogger.debug("RemoveLibC - "+ "\nRemoveLibC - ".join(files_to_remove) )
+  gLogger.debug("RemoveLibC - "+ "\nRemoveLibC - ".join(FILES_TO_REMOVE) )
 
   curdir = os.getcwd()
   try:
     os.chdir(libraryPath)
-  except:
-    return True  
+  except OSError:
+    return True
   listlibs = os.listdir(os.getcwd())
   for lib in listlibs:
-    for lib_to_remove in files_to_remove:
+    for lib_to_remove in FILES_TO_REMOVE:
       if lib.count(lib_to_remove):
         try:
           libraryPath = os.getcwd() + os.sep + lib
           gLogger.info("RemoveLibC: Trying to remove: %s" % libraryPath)
           os.remove(libraryPath)
-        except:
+        except OSError:
           gLogger.error("RemoveLibC: Could not remove", lib)
           os.chdir(curdir)
           return False
@@ -41,17 +43,17 @@ def removeLibc(libraryPath):
 
 def getLibsToIgnore():
   """ :returns: static list of system libraries """
-  return files_to_remove
+  return FILES_TO_REMOVE
 
 if __name__ == "__main__":
   import sys
   if not len(sys.argv)>1:
     print "You need to pass the path"
     exit(1)
-  path = sys.argv[1]
-  print "Will remove from %s the files that look like %s" % (path, getLibsToIgnore())  
+  PATH = sys.argv[1]
+  print "Will remove from %s the files that look like %s" % (PATH, getLibsToIgnore())
   
-  if not removeLibc(path):
+  if not removeLibc(PATH):
     print "Could not clean libs"
     exit(1)
   exit(0)
