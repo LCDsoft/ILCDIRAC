2c76ec4652f218f398e5f34540099a9b5d72bc75
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-10-16 16:50:44 +0200

    ILCDIRAC-580: Add CreateDoc folder with all the things needed to create the sphinx documentation

---
+++ MakeDoc.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+
+import os
+
+BASEPATH = "DOC"
+ILCDIRACPATH = os.path.join(os.environ.get("DIRAC"), "ILCDIRAC" )
+DIRACPATH = os.environ.get("DIRAC")
+
+ORIGDIR = os.getcwd()
+
+BASEPATH = os.path.join( ORIGDIR, BASEPATH )
+
+
+def mkdir( folder ):
+  """create a folder, ignore if it exists"""
+  try:
+    os.mkdir( os.path.join(os.getcwd(),folder) )
+  except OSError as e:
+    print Exception,str(e)
+
+def mkRest( filename, modulename, fullmodulename, subpackages=None, modules=None ):
+  """make a rst file for filename"""
+  if modulename == "scripts":
+    modulefinal = fullmodulename.split(".")[-2]+" Scripts"
+  else:
+    modulefinal = modulename
+  with open(filename, 'w') as rst:
+    lines = []
+    lines.append("%s" % modulefinal)
+    lines.append("="*len(modulefinal))
+    lines.append(".. module:: %s " % fullmodulename )
+    lines.append("" )
+    # lines.append(".. automodule:: %s" % fullmodulename )
+    # lines.append("   :members:" )
+    # lines.append("   :undoc-members:" )
+    # lines.append("   :inherited-members:" )
+    # lines.append("   :show-inheritance:" )
+
+    # if subpackages or modules:
+    #   lines.append(".. autosummary::")
+    #   lines.append("")
+
+    # if subpackages:
+    #   for package in subpackages:
+    #     lines.append("   %s/%s" % (package,package.split("/")[-1] ) )
+    #     #lines.append("   %s " % (package, ) )
+    #     lines.append("")
+    # if modules:
+    #   for module in modules:
+    #     lines.append("   %s" % (module.split("/")[-1],) )
+    #     #lines.append("   %s " % (package, ) )
+    #     lines.append("")
+    
+    if subpackages or modules:
+      lines.append(".. toctree::")
+      lines.append("   :maxdepth: 1")
+      lines.append("")
+
+    if subpackages:
+      for package in subpackages:
+        lines.append("   %s/%s.rst" % (package,package.split("/")[-1] ) )
+        #lines.append("   %s " % (package, ) )
+
+    if modules:
+      for module in sorted(modules):
+        lines.append("   %s.rst" % (module.split("/")[-1],) )
+        #lines.append("   %s " % (package, ) )
+        
+    rst.write("\n".join(lines))
+
+    
+def mkModuleRest( classname, fullclassname ):
+  """ create rst file for class"""
+  filename = classname+".rst"
+  with open(filename, 'w') as rst:
+    lines = []
+    lines.append("%s" % classname)
+    lines.append("="*len(classname))
+
+    # if "-" not in classname:
+    #   lines.append(".. autosummary::" )
+    #   lines.append("   :toctree: %sGen" % classname )
+    #   lines.append("")
+    #   lines.append("   %s " % fullclassname )
+    #   lines.append("")
+
+    ## These diagrams look aweful, need to install graphiz package and enable extensions
+    # lines.append(".. inheritance-diagram:: %s" % fullclassname )
+    # lines.append("")
+
+    lines.append(".. automodule:: %s" % fullclassname )
+    lines.append("   :members:" )
+    lines.append("   :inherited-members:" )
+    lines.append("   :undoc-members:" )
+    lines.append("   :show-inheritance:" )
+    rst.write("\n".join(lines))
+
+  
+def getsubpackages( abspath, direc):
+  """return list of subpackages with full path"""
+  packages = []
+  for dire in direc:
+    if "test" in dire.lower():
+      continue
+    #print os.path.join( DIRACPATH,abspath,dire, "__init__.py" )
+    if os.path.exists( os.path.join( DIRACPATH,abspath,dire, "__init__.py" ) ):
+      #packages.append( os.path.join( "DOC", abspath, dire) )
+      packages.append( os.path.join( dire) )
+  #print "packages",packages
+  return packages
+
+def getmodules( abspath, direc, files ):
+  """return list of subpackages with full path"""
+  packages = []
+  for filename in files:
+    if "test" in filename.lower():
+      continue
+    if filename != "__init__.py":
+      packages.append( filename.split(".py")[0] )
+
+  return packages
+
+
+def createDoc():
+  """create the rst files for all the things we want them  for"""
+  mkdir(BASEPATH)
+  os.chdir(BASEPATH)
+
+  for root,direc,files in os.walk(ILCDIRACPATH):
+    files = [ _ for _ in files if _.endswith(".py") ]
+    if "__init__.py" not in files:
+      continue
+    if "test" in root.lower():
+      continue
+    #print root, direc, files
+    modulename = root.split("/")[-1]
+    abspath = root.split(DIRACPATH)[1].strip("/")
+    fullmodulename = ".".join(abspath.split("/"))
+    packages = getsubpackages(abspath,direc)
+    if abspath:
+      mkdir( abspath )
+      os.chdir( abspath )
+    mkRest( modulename+".rst", modulename, fullmodulename, subpackages=packages, modules=getmodules(abspath, direc, files) )
+
+    for filename in files:
+      if "test" in filename.lower():
+        continue
+      if "__init__.py" == filename:
+        continue
+      if not filename.endswith(".py"):
+        continue
+      fullclassname = ".".join(abspath.split("/")+[filename])
+      mkModuleRest( filename.split(".py")[0], fullclassname.split(".py")[0] )
+
+    os.chdir(BASEPATH)
+
+
+
+    
+# for Module in Interfaces Workflow RequestManagement ILCTransformationSystem WorkloadManagementSystem DataManagementSystem
+# do
+
+# done
+
+
+# #sphinx-apidoc -T -F -H ILCDIRAC -V 23.0 -R 10 -e -M -d 1 -f -o source/rsts/Interfaces ../Interfaces/API
+# sphinx-apidoc -T -F -H ILCDIRAC -V 23.0 -R 10 -e -M -d 1 -f -o source/rsts/ILCDIRAC ../
+
+
+# rm source/rsts/ILCDIRAC/ILCDIRAC.Interfaces.API.NewInterface.Tests.ProductionJobTests.rst
+
+# #sphinx-apidoc -F -H Interfaces -V 23.0 -R 10 -e -M -d 1 -f -o source/rsts/Interfaces ../Interfaces/API/NewInterface/
+
+if __name__ == "__main__":
+  createDoc()
---
+++ source/index.rst
@@ -0,0 +1,66 @@
+.. ILCDIRAC documentation master file, created by
+   sphinx-quickstart on Mon Oct 12 19:49:21 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ILCDIRAC Documentation
+######################
+
+Welcome to the ILCDIRAC Documentation. 
+
+Interfaces for User Jobs
+========================
+
+If you are looking for how to submit jobs
+for Linear Collider Software please look at the
+:mod:`~ILCDIRAC.Interfaces.API.NewInterface.UserJob` class and the
+:mod:`~ILCDIRAC.Interfaces.API.NewInterface.Applications` modules and finally at
+the :mod:`~ILCDIRAC.Interfaces.API.DiracILC` class
+
+.. toctree::
+   :maxdepth: 1
+
+   DOC/ILCDIRAC/Interfaces/API/NewInterface/Applications/Applications.rst
+   DOC/ILCDIRAC/Interfaces/API/NewInterface/UserJob.rst
+   DOC/ILCDIRAC/Interfaces/API/DiracILC.rst
+
+Scripts
+-------
+
+Some scripts of interest to the casual user are part of the :mod:`Interfaces <ILCDIRAC.Interfaces.scripts>` and
+:mod:`Core <ILCDIRAC.Core.scripts>` modules.
+
+.. toctree::
+   :maxdepth: 2
+
+   DOC/ILCDIRAC/Interfaces/scripts/scripts.rst
+   DOC/ILCDIRAC/Core/scripts/scripts.rst
+
+Production Scripts
+==================
+
+Some scripts used to control productions are available for production managers
+
+.. toctree::
+   :maxdepth: 2
+
+   DOC/ILCDIRAC/ILCTransformationSystem/scripts/scripts.rst
+
+Contents:
+=========
+
+This is the full table of contents for the documentation
+
+.. toctree::
+   :maxdepth: 2
+
+   /DOC/ILCDIRAC/ILCDIRAC
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
