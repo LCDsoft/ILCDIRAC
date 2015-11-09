d51a1e1e43ef4fe8c88a46b3da70d543395ff9ee
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-11-09 14:33:23 +0100

    update patchSphinx for new sphinx version

--- patchSphinx.sh
+++ patchSphinx.sh
@@ -9,21 +9,21 @@ SPHINXEXT="$SPHINXBASE/lib/python2.7/site-packages/sphinx/ext"
 AUTODOC=$SPHINXEXT/autodoc.py
 
 cat > autodoc.patch << EOF
---- /home/sailer/software/DIRAC/DiracDevV6r12/Linux_x86_64_glibc-2.12/lib/python2.7/site-packages/sphinx/ext/autodoc.py~ 2015-11-02 17:25:01.634787510 +0100
-+++ /home/sailer/software/DIRAC/DiracDevV6r12/Linux_x86_64_glibc-2.12/lib/python2.7/site-packages/sphinx/ext/autodoc.py  2015-11-02 17:25:50.083781673 +0100
+--- /home/sailer/software/DIRAC/DiracDevV6r13/Linux_x86_64_glibc-2.12/lib/python2.7/site-packages/sphinx/ext/autodoc.py~        2015-11-09 13:53:34.104991094 +0100
++++ /home/sailer/software/DIRAC/DiracDevV6r13/Linux_x86_64_glibc-2.12/lib/python2.7/site-packages/sphinx/ext/autodoc.py 2015-11-09 13:57:02.519263402 +0100
 @@ -36,9 +36,9 @@
- 
+
  #: extended signature RE: with explicit module name separated by ::
  py_ext_sig_re = re.compile(
--    r'''^ ([\w.]+::)?             # explicit module name
--            ([\w.]+\.)?           # module and/or class name(s)
--            ([\w]+)  \s*          # thing name
-+    r'''^ ([\w.-]+::)?            # explicit module name
-+            ([\w.-]+\.)?          # module and/or class name(s)
-+            ([\w-]+)  \s*         # thing name
-             (?: \((.*)\)          # optional: arguments
-              (?:\s* -> \s* (.*))? #           return annotation
-             )? $                  # and nothing more
+-    r'''^ ([\w.]+::)?            # explicit module name
+-          ([\w.]+\.)?            # module and/or class name(s)
+-          (\w+)  \s*             # thing name
++    r'''^ ([\w.-]+::)?           # explicit module name
++          ([\w.-]+\.)?           # module and/or class name(s)
++          ([\w-]+)  \s*          # thing name
+           (?: \((.*)\)           # optional: arguments
+            (?:\s* -> \s* (.*))?  #           return annotation
+           )? $                   # and nothing more
 EOF
 
 patch -N $AUTODOC < autodoc.patch
