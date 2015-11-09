6630c88001292e540f6e446d901ed75143974d09
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-11-09 16:18:11 +0100

    Rename ProductionJobTests --> Test_ and remove Script.parseCommandLine

--- Interfaces/API/NewInterface/Tests/Test_ProductionJob.py
+++ Interfaces/API/NewInterface/Tests/Test_ProductionJob.py
@@ -6,20 +6,13 @@ Test user jobfinalization
 """
 __RCSID__ = "$Id$"
 
-
 import unittest
-
 from decimal import Decimal
 from DIRAC import gLogger
-
-from DIRAC.Core.Base import Script
-Script.parseCommandLine()
-
 from ILCDIRAC.Interfaces.API.NewInterface.ProductionJob import ProductionJob
 
 gLogger.setLevel("DEBUG")
 gLogger.showHeaders(True)
-
 
 class ProductionJobTestCase( unittest.TestCase ):
   """ Base class for the ProductionJob test cases
@@ -31,25 +24,25 @@ class ProductionJobTestCase( unittest.TestCase ):
     self.prodJob.energy=250.0
 
   def test_Energy250( self ):
-    """ test  250gev """
+    """ProductionJob getEnergyPath 250gev..........................................................."""
     self.prodJob.energy = Decimal('250.0')
     res = self.prodJob.getEnergyPath()
     self.assertEqual( "250gev/", res )
 
   def test_Energy350( self ):
-    """ test  350gev """
+    """ProductionJob getEnergyPath 350gev..........................................................."""
     self.prodJob.energy = 350.0
     res = self.prodJob.getEnergyPath()
     self.assertEqual( "350gev/", res )
 
   def test_Energy3000( self ):
-    """ test  3tev """
+    """ProductionJob getEnergyPatt 3tev............................................................."""
     self.prodJob.energy = 3000.0
     res = self.prodJob.getEnergyPath()
     self.assertEqual( "3tev/", res )
 
   def test_Energy1400( self ):
-    """ test  1.4tev """
+    """ProductionJob getEnergyPath 1.4tev .........................................................."""
     self.prodJob.energy = 1400.0
     res = self.prodJob.getEnergyPath()
     self.assertEqual( "1.4tev/", res )
