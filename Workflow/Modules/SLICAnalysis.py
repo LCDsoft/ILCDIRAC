8cf2f0e0a114952fefe857141c2526579e6c3ac9
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-12-02 11:20:41 +0100

    SLIC/LCSIM: Move download of detector zip file to Mixin Class

--- Workflow/Modules/SLICAnalysis.py
+++ Workflow/Modules/SLICAnalysis.py
@@ -9,18 +9,18 @@ Run SLIC
 __RCSID__ = "$Id$"
 
 
-import os, urllib
+import os
 from DIRAC.Core.Utilities.Subprocess                      import shellCall
 #from DIRAC.Core.DISET.RPCClient                           import RPCClient
 from ILCDIRAC.Workflow.Modules.ModuleBase                    import ModuleBase
-from ILCDIRAC.Core.Utilities.CombinedSoftwareInstallation import getEnvironmentScript, unzip_file_into_dir
+from ILCDIRAC.Workflow.Utilities.CompactMixin             import CompactMixin
+from ILCDIRAC.Core.Utilities.CombinedSoftwareInstallation import getEnvironmentScript
 from ILCDIRAC.Core.Utilities.PrepareOptionFiles           import prepareMacFile, getNewLDLibs
 from ILCDIRAC.Core.Utilities.resolvePathsAndNames         import resolveIFpaths, getProdFilename
 from ILCDIRAC.Core.Utilities.FindSteeringFileDir          import getSteeringFileDirName
-
 from DIRAC                                                import S_OK, S_ERROR, gLogger
       
-class SLICAnalysis(ModuleBase):
+class SLICAnalysis(CompactMixin, ModuleBase):
   """
   Specific Module to run a SLIC job.
   """
@@ -97,34 +97,10 @@ class SLICAnalysis(ModuleBase):
       return res
     env_script_path = res["Value"]
     
-    #retrieve detector model from web
-    detector_urls = self.ops.getValue('/SLICweb/SLICDetectorModels', [''])
-    if len(detector_urls[0]) < 1:
-      self.log.error('Could not find in CS the URL for detector model')
-      return S_ERROR('Could not find in CS the URL for detector model')
+    retMod = self.getDetectorModel()
+    if not retMod:
+      return retMod
 
-    if not os.path.exists(self.detectorModel + ".zip"):
-      for detector_url in detector_urls:
-        try:
-          urllib.urlretrieve("%s%s" % (detector_url, self.detectorModel + ".zip"), 
-                             self.detectorModel + ".zip")
-        except IOError:
-          self.log.error("Download of detector model failed")
-          continue
-
-    if not os.path.exists(self.detectorModel + ".zip"):
-      self.log.error('Detector model %s was not found neither locally nor on the web, exiting' % self.detectorModel)
-      return S_ERROR('Detector model was not found')
-
-    try:
-      unzip_file_into_dir(open(self.detectorModel + ".zip"), os.getcwd())
-    except (RuntimeError, OSError) as err: #RuntimeError is for zipfile
-      os.unlink(self.detectorModel + ".zip")
-      self.log.error('Failed to unzip detector model: ', str(err))
-      return S_ERROR('Failed to unzip detector model')
-    #unzip detector model
-    #self.unzip_file_into_dir(open(self.detectorModel+".zip"),os.getcwd())
-    
     slicmac = 'slicmac.mac'
     if len(self.InputFile):
       res = resolveIFpaths(self.InputFile)
