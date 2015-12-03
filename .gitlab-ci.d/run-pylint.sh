ca2dcd49b781869005c4afe63fd93443bda86731
Marko Petric <marko.petric@cern.ch>
2015-12-03 16:19:35 +0100

    Fixing pylint

--- .gitlab-ci.d/run-pylint.sh
+++ .gitlab-ci.d/run-pylint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+pip install --upgrade pip
+pip install pylint
 find . -name Examples -prune -o -name .ropeproject -prune -o -name "*.py" -exec pylint -E --rcfile=.gitlab-ci.d/DIRAC.pylint.rc --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
