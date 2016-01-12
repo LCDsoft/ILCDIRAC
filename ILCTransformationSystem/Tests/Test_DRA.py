e9d32eaf998974aa9bc89004b590dd3eb18e85f7
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-01-12 13:50:59 +0100

    TestDRA: Add more tests and improve patches and mocks

--- ILCTransformationSystem/Tests/Test_DRA.py
+++ ILCTransformationSystem/Tests/Test_DRA.py
@@ -1,6 +1,8 @@
 """Test the DataRecoveryAgent"""
 
 import unittest
+import sys
+from StringIO import StringIO
 
 from mock import MagicMock as Mock, patch
 
@@ -9,6 +11,8 @@ from DIRAC import S_OK, S_ERROR
 from ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent import DataRecoveryAgent
 
 __RCSID__ = "$Id$"
+
+TINFOMOCK = Mock()
 
 class TestDRA( unittest.TestCase ):
   """Test the DataRecoveryAgent"""
@@ -20,6 +24,10 @@ class TestDRA( unittest.TestCase ):
   @patch("ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent.ReqClient", new=Mock() )
   def setUp ( self ):
     self.dra = DataRecoveryAgent( agentName="ILCTransformationSystem/DataRecoveryAgent", loadName="TestDRA" )
+    self.dra.reqClient = Mock()
+    self.dra.tClient = Mock()
+    self.dra.fcClient = Mock()
+    self.dra.jobMon = Mock()
 
   def tearDown ( self ):
     pass
@@ -39,11 +47,11 @@ class TestDRA( unittest.TestCase ):
     self.assertFalse( self.dra.enabled )
     self.assertTrue( res['OK'] )
 
-  @patch("DIRAC.TransformationSystem.Client.TransformationClient.TransformationClient.getTransformations",
-         new=Mock( return_value=S_OK( [dict(TransformationID=1234, TransformationName="TestProd12", Type="TestProd")] ) )
-        )
   def test_getEligibleTransformations_success( self ):
     """test for DataRecoveryAgent getEligibleTransformations success................................"""
+    self.dra.tClient.getTransformations = Mock(
+      return_value=S_OK( [dict(TransformationID=1234, TransformationName="TestProd12", Type="TestProd")] ) )
+
     res = self.dra.getEligibleTransformations( status="Active", typeList=['TestProds'] )
     self.assertTrue( res['OK'] )
     self.assertIsInstance( res['Value'], dict )
@@ -52,15 +60,48 @@ class TestDRA( unittest.TestCase ):
     self.assertIsInstance( vals['1234'], tuple )
     self.assertEqual( ("TestProd", "TestProd12"), vals["1234"] )
 
-  @patch("DIRAC.TransformationSystem.Client.TransformationClient.TransformationClient.getTransformations",
-         new=Mock( return_value=S_ERROR( "No can Do" ) ) )
-  def test_getEligibleTransformations_faild( self ):
-    """test for DataRecoveryAgent getEligibleTransformation failure................................."""
+  def test_getEligibleTransformations_failed( self ):
+    """test for DataRecoveryAgent getEligibleTransformations failure................................"""
+    self.dra.tClient.getTransformations = Mock( return_value=S_ERROR( "No can Do" ) )
     res = self.dra.getEligibleTransformations( status="Active", typeList=['TestProds'] )
     self.assertFalse( res['OK'] )
     self.assertEqual( "No can Do", res['Message'] )
-    
+
+  def test_treatProduction1( self ):
+    """test for DataRecoveryAgent treatProduction success1.........................................."""
+    getJobMock = Mock( name = "getJobMOck" )
+    getJobMock.getJobs.return_value = ( Mock( name = "jobsMOck" ), 50, 50 )
+    tinfoMock = Mock( name = "infoMock", return_value = getJobMock )
+    self.dra.checkAllJobs = Mock()
+    with patch("ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent.TransformationInfo", new=tinfoMock ):
+      self.dra.treatProduction( prodID=1234, transName="TestProd12", transType="MCGeneration" ) ##returns None
+
+  def test_treatProduction2( self ):
+    """test for DataRecoveryAgent treatProduction success2.........................................."""
+    getJobMock = Mock( name = "getJobMOck" )
+    getJobMock.getJobs.return_value = ( Mock( name = "jobsMock" ), 50, 50 )
+    tinfoMock = Mock( name = "infoMock", return_value = getJobMock )
+    self.dra.checkAllJobs = Mock()
+    with patch("ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent.TransformationInfo", new=tinfoMock ):
+      self.dra.treatProduction( prodID=1234, transName="TestProd12", transType="MCReconstruction" ) ##returns None
+
+
+  def test_treatProduction3( self ):
+    """test for DataRecoveryAgent treatProduction skip.............................................."""
+    getJobMock = Mock( name = "getJobMOck" )
+    getJobMock.getJobs.return_value = ( Mock( name = "jobsMock" ), 50, 50 )
+    tinfoMock = Mock( name = "infoMock", return_value = getJobMock )
+    self.dra.checkAllJobs = Mock()
+    self.dra.jobCache[1234] = (50, 50)
+    #catch the printout to check path taken
+    out = StringIO()
+    sys.stdout = out
+
+    with patch("ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent.TransformationInfo", new=tinfoMock ):
+      self.dra.treatProduction( prodID=1234, transName="TestProd12", transType="MCReconstruction" ) ##returns None
+    self.assertIn( "Skipping production 1234", out.getvalue().strip().splitlines()[0] )
+
+
 if __name__ == "__main__":
   SUITE = unittest.defaultTestLoader.loadTestsFromTestCase( TestDRA )
   TESTRESULT = unittest.TextTestRunner( verbosity = 2 ).run( SUITE )
-
