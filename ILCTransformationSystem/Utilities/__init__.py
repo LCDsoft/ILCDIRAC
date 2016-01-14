c23e77b3001565026f5a68f7a34dd33f56f3d467
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-01-14 19:06:28 +0100

    Remove utilities from __init__, this caused some problem with Mocking imported Modules

--- ILCTransformationSystem/Utilities/__init__.py
+++ ILCTransformationSystem/Utilities/__init__.py
@@ -1,9 +1,4 @@
 """ILCTransformationSystem Utilities"""
 
-from ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo import TransformationInfo
-from ILCDIRAC.ILCTransformationSystem.Utilities.JobInfo import JobInfo
-
 __RCSID__ = "$Id$"
-
-__all__ = ['TransformationInfo', 'JobInfo']
 
