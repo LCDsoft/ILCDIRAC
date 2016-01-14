677c3a1a58567bb606e3d2ff5bf8c7f9792d4b0e
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-01-14 10:04:13 +0100

    ILCTransformation: remove FileInformation
    File not used any more

--- ILCTransformationSystem/Utilities/__init__.py
+++ ILCTransformationSystem/Utilities/__init__.py
@@ -1,9 +1,9 @@
 """ILCTransformationSystem Utilities"""
 
-__RCSID__ = "$Id$"
-
-__all__ = ['FileInformation', 'TransformationInfo', 'JobInfo']
-
-from ILCDIRAC.ILCTransformationSystem.Utilities.FileInformation import FileInformation
 from ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo import TransformationInfo
 from ILCDIRAC.ILCTransformationSystem.Utilities.JobInfo import JobInfo
+
+__RCSID__ = "$Id$"
+
+__all__ = ['TransformationInfo', 'JobInfo']
+
