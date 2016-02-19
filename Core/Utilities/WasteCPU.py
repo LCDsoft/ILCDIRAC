65ef49a68623e75f57fe986777d7ff84c499a2a0
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-02-19 13:37:48 +0100

    Catch all exception in wasteCPU
    add tests for full coverage

--- Core/Utilities/WasteCPU.py
+++ Core/Utilities/WasteCPU.py
@@ -4,21 +4,26 @@ This is the worst piece of code ever NEEDED: just run some CPU intensive code to
 :author: Stephane Poss
 :since: Jul 26, 2011
 '''
-__RCSID__ = "$Id$"
+
 import time
 from math import log
 from DIRAC import S_OK, S_ERROR
+
+__RCSID__ = "$Id$"
 
 def wasteCPUCycles(timecut):
   """ Waste, waste, and waste more CPU.
   """
   number = 1e31
   first = time.clock()
-  while time.clock()-first < timecut:
-    try:
-      number = log(number)
-    except ValueError as x:
-      return S_ERROR("Failed to waste %s CPU seconds:%s" % (timecut, str(x)))
-    if number <= 0:
-      number = -number + 4
+  try:
+    while time.clock()-first < timecut:
+      try:
+        number = log(number)
+      except ValueError as x:
+        return S_ERROR("Failed to waste %s CPU seconds:%s" % (timecut, str(x)))
+      if number <= 0:
+        number = -number + 4
+  except Exception as e:
+    return S_ERROR("Failed to waste %s CPU seconds, OtherException: %r" % (timecut, e))
   return S_OK("Successfully wasted %s seconds" % timecut)
