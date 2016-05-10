1fca2ce9e9b904a92ba2a2dd9def3b12a9482b65
Jan Ebbing <jan.hendrik.ebbing@cern.ch>
2016-05-10 17:34:59 +0200

    Completed change from nose to pytest, cleanup, add config files for pytest/cov.

--- .gitlab-ci.d/run-unittests.sh
+++ .gitlab-ci.d/run-unittests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export PYTEST_ADDOPTS="-s -v --cov --cov-report html -k test_ --ignore=Interfaces/API/NewInterface/Productions/TestProductionChain.py --ignore Interfaces/API/NewInterface/Tests/Test_FullCVMFSTests.py --ignore Workflow/Modules/Test/Test_SEs.py"
-py.test
+export PYTEST_ADDOPTS=" --ignore Interfaces/API/NewInterface/Tests/Test_FullCVMFSTests.py "
+py.test 
