6f6fd1b2c44c7ed64686810323bdfab41a6ec50e
Jan Ebbing <jan.hendrik.ebbing@cern.ch>
2016-08-09 09:57:45 +0200

    Change scripts to not run CVMFS/SE tests in unittest phase (using marks/tags).

--- .gitlab-ci.d/calculate-complete-coverage.sh
+++ .gitlab-ci.d/calculate-complete-coverage.sh
@@ -3,7 +3,8 @@
 # Note that this script requires a proxy to be executed
 echo "Please check that a valid dirac proxy is available before executing the cvmfs tests."
 source .gitlab-ci.d/set-reportstyle.sh
-py.test
+py.test Workflow/Modules/Test/Test_SEs.py
 export PYTEST_ADDOPTS=$PYTEST_ADDOPTS" --cov-append"
 py.test Interfaces/API/NewInterface/Tests/Test_FullCVMFSTests.py
-py.test Workflow/Modules/Test/Test_SEs.py
+export PYTEST_ADDOPTS=$PYTEST_ADDOPTS" -m 'not integration'"
+py.test
