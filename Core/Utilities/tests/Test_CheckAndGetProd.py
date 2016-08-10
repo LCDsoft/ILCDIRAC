98313558ed229d8e07873ad00e73ed3442789438
Jan Ebbing <jan.hendrik.ebbing@cern.ch>
2016-08-10 13:46:23 +0200

    Refactor CheckAndGetProxy tests to fix weird error - replace @patch with 'with patch'.

--- Core/Utilities/tests/Test_CheckAndGetProd.py
+++ Core/Utilities/tests/Test_CheckAndGetProd.py
@@ -18,81 +18,69 @@ class CheckProxyTest( unittest.TestCase ):
   def tearDown ( self ):
     pass
 
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=0))
-  @patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(return_value=S_OK({"group":"ilc_prod"})))
   def test_success( self ):
     """test for CheckandGetProdProxy: success......................................................."""
-    res = checkAndGetProdProxy()
-    self.assertTrue( res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=0)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(return_value=S_OK({"group":"ilc_prod"}))):
+      res = checkAndGetProdProxy()
+      self.assertTrue( res['OK'] )
 
-
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=0))
-  @patch("%s.getProxyInfo" % MODULE_NAME,
-         new=Mock(side_effect=[S_OK({"group":"ilc_user"}),S_OK({"group":"ilc_prod"})]))
   def test_success_2( self ):
     """test for CheckandGetProdProxy: sucess 2......................................................"""
-    res = checkAndGetProdProxy()
-    self.assertTrue( res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=0)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(side_effect=[S_OK({"group":"ilc_user"}),S_OK({"group":"ilc_prod"})])):
+      res = checkAndGetProdProxy()
+      self.assertTrue( res['OK'] )
 
-    
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=1))
-  @patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(return_value=S_ERROR("No proxy info")))
   def test_failure( self ):
     """test for CheckandGetProdProxy: failure......................................................."""
-    res = checkAndGetProdProxy()
-    self.assertFalse( res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=1)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(return_value=S_ERROR('No proxy info'))):
+      res = checkAndGetProdProxy()
+      self.assertFalse( res['OK'] )
 
-
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=0))
-  @patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(return_value=S_ERROR("message")))
   def test_failure_2( self ):
     """test for CheckandGetProdProxy: semi failure 1................................................"""
-    res = checkAndGetProdProxy()
-    self.assertFalse( res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=0)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(return_value=S_ERROR('message'))):
+      res = checkAndGetProdProxy()
+      self.assertFalse( res['OK'] )
 
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=0))
-  @patch("%s.getProxyInfo" % MODULE_NAME,
-         new=Mock(side_effect=[S_OK({}),S_OK({"group":"ilc_prod"})]))
   def test_failure_3( self ):
     """test for CheckandGetProdProxy: semi failure 2................................................"""
-    res = checkAndGetProdProxy()
-    self.assertTrue( res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=0)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(side_effect=[S_OK({}), S_OK({"group":"ilc_prod"})])):
+      res = checkAndGetProdProxy()
+      self.assertTrue( res['OK'] )
 
-
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=0))
-  @patch("%s.getProxyInfo" % MODULE_NAME,
-         new=Mock(side_effect=[S_ERROR("no proxy info"),S_OK({"group":"ilc_prod"})]))
   def test_failure_4( self ):
     """test for CheckandGetProdProxy: semi failure 3................................................"""
-    res = checkAndGetProdProxy()
-    self.assertTrue( res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=0)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(side_effect=[S_ERROR("no proxy info"),S_OK({"group":"ilc_prod"})])):
+      res = checkAndGetProdProxy()
+      self.assertTrue( res['OK'] )
 
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=0))
-  @patch("%s.getProxyInfo" % MODULE_NAME,
-         new=Mock(side_effect=[S_ERROR("no proxy info"),S_OK({"notgroup":"ilc_user"})]))
   def test_failure_5( self ):
     """test for CheckandGetProdProxy: semi failure 4................................................"""
-    res = checkAndGetProdProxy()
-    self.assertTrue( not res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=0)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(side_effect=[S_ERROR("no proxy info"),S_OK({"notgroup":"ilc_user"})])):
+      res = checkAndGetProdProxy()
+      self.assertTrue( not res['OK'] )
 
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=0))
-  @patch("%s.getProxyInfo" % MODULE_NAME,
-         new=Mock(side_effect=[S_ERROR("no proxy info"),S_OK({"group":"ilc_user"})]))
   def test_failure_6( self ):
     """test for CheckandGetProdProxy: semi failure 5................................................"""
-    res = checkAndGetProdProxy()
-    self.assertTrue( not res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=0)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(side_effect=[S_ERROR("no proxy info"),S_OK({"group":"ilc_user"})])):
+      res = checkAndGetProdProxy()
+      self.assertTrue( not res['OK'] )
 
-    
-  @patch("%s.call" % MODULE_NAME, new=Mock(return_value=0))
-  @patch("%s.getProxyInfo" % MODULE_NAME,
-         new=Mock(side_effect=[S_ERROR("no proxy info"),S_ERROR("Still no proxy")]))
   def test_failure_7( self ):
     """test for CheckandGetProdProxy: semi failure 6................................................"""
-    res = checkAndGetProdProxy()
-    self.assertTrue( not res['OK'] )
+    with patch("%s.call" % MODULE_NAME, new=Mock(return_value=0)), \
+         patch("%s.getProxyInfo" % MODULE_NAME, new=Mock(side_effect=[S_ERROR("no proxy info"),S_ERROR("Still no proxy")])):
+      res = checkAndGetProdProxy()
+      self.assertTrue( not res['OK'] )
 
 if __name__ == "__main__":
   SUITE = unittest.defaultTestLoader.loadTestsFromTestCase( CheckProxyTest )
   TESTRESULT = unittest.TextTestRunner( verbosity = 2 ).run( SUITE )
-
