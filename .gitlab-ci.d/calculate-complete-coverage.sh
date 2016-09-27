ae5422c5c7ef19c77062aade9730c48024b5f0f9
Jan Ebbing <jan.hendrik.ebbing@cern.ch>
2016-09-27 17:30:09 +0200

    Add status report to the script that computes coverage to avoid scrolling.

--- .gitlab-ci.d/calculate-complete-coverage.sh
+++ .gitlab-ci.d/calculate-complete-coverage.sh
@@ -4,7 +4,40 @@
 echo "Please check that a valid dirac proxy is available before executing the cvmfs tests."
 source .gitlab-ci.d/set-reportstyle.sh
 py.test Workflow/Modules/Test/Test_SEs.py
+
+SE_RESULT=""
+if [ $? -eq 0 ]
+then
+  SE_RESULT="Storage unit tests successful"
+else
+  SE_RESULT="Storage unit tests failed! Check what failed."
+fi
+
 export PYTEST_ADDOPTS=$PYTEST_ADDOPTS" --cov-append"
 py.test Interfaces/API/NewInterface/Tests/Test_FullCVMFSTests.py
+
+CVMFS_RESULT=""
+if [ $? -eq 0 ]
+then
+  CVMFS_RESULT="Storage unit tests successful"
+else
+  CVMFS_RESULT="Storage unit tests failed! Check what failed."
+fi
+
 export PYTEST_ADDOPTS=$PYTEST_ADDOPTS" -m 'not integration'"
 py.test
+
+UNIT_RESULT=""
+if [ $? -eq 0 ]
+then
+  UNIT_RESULT="Storage unit tests successful"
+else
+  UNIT_RESULT="Storage unit tests failed! Check what failed."
+fi
+
+echo "########################################################################"
+echo "Reporting results of tests..."
+echo "########################################################################"
+echo $SE_RESULT
+echo $CVMFS_RESULT
+echo $UNIT_RESULT
