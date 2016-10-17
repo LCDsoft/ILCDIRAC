62183e4590996d334053406dda597dbeec762352
Jan Ebbing <jan.hendrik.ebbing@cern.ch>
2016-10-17 13:39:42 +0200

    Merged Test_DDsimInterface into Test_DDSim, add test for no detector model.

--- Interfaces/API/NewInterface/Tests/Test_DDSim.py
+++ Interfaces/API/NewInterface/Tests/Test_DDSim.py
@@ -6,9 +6,9 @@ Test DDSim module
 
 import inspect
 import unittest
-from mock import patch, MagicMock as Mock
+from mock import create_autospec, patch, MagicMock as Mock
 
-from DIRAC import S_OK, S_ERROR
+from DIRAC import gLogger, S_OK, S_ERROR
 from ILCDIRAC.Interfaces.API.NewInterface.Applications import DDSim
 from ILCDIRAC.Tests.Utilities.GeneralUtils import assertEqualsImproved, assertDiracFailsWith, \
   assertDiracSucceeds
@@ -16,6 +16,9 @@ from ILCDIRAC.Tests.Utilities.GeneralUtils import assertEqualsImproved, assertDi
 __RCSID__ = "$Id$"
 
 MODULE_NAME = 'ILCDIRAC.Interfaces.API.NewInterface.Applications.DDSim'
+
+gLogger.setLevel("DEBUG")
+gLogger.showHeaders(True)
 
 #pylint: disable=protected-access
 class DDSimTestCase( unittest.TestCase ):
@@ -107,6 +110,12 @@ class DDSimTestCase( unittest.TestCase ):
     self.assertNotIn( 'nbevts', self.dds.prodparameters )
     self.assertNotIn( 'Process', self.dds.prodparameters )
 
+  def test_checkconsistency_nodetectormodel( self ):
+    self.dds.version = 123
+    self.dds.steeringFile = None
+    self.dds.detectorModel = None
+    assertDiracFailsWith( self.dds._checkConsistency( Mock() ), 'no detectormodel set', self )
+
   def test_checkconsistency_noversion( self ):
     self.dds.version = None
     assertDiracFailsWith( self.dds._checkConsistency( Mock() ), 'no version found', self )
@@ -145,7 +154,115 @@ class DDSimTestCase( unittest.TestCase ):
       for keyword in [ 'detectorType', 'slic_detectormodel' ]:
         self.assertIn( keyword, self.dds.prodparameters )
 
+#pylint: disable=protected-access
+
+class TestDDSim( unittest.TestCase ):
+  """tests for the DDSim interface"""
+
+  def setUp( self ):
+    pass
+
+  def tearDown( self ):
+    """cleanup any files"""
+    pass
 
 
+  @patch( "ILCDIRAC.Interfaces.API.NewInterface.Applications.DDSim.getKnownDetectorModels",
+          new = Mock(return_value=S_OK({'CLIC_o2_v03':"/some/path"})))
+  def test_setDetectorModel1( self ):
+    """test DDSIm setDetectorModel part of software................................................."""
+    detModel = "CLIC_o2_v03"
+    ddsim = DDSim()
+    ddsim.setDetectorModel( detModel )
+    self.assertEqual( ddsim.detectorModel, detModel )
+
+  @patch( "ILCDIRAC.Interfaces.API.NewInterface.Applications.DDSim.getKnownDetectorModels",
+          new = Mock(return_value=S_ERROR("No known models")))
+  def test_setDetectorModel2( self ):
+    """test DDSIm setDetectorModel part of software failure........................................."""
+    detModel = "CLIC_o2_v03"
+    ddsim = DDSim()
+    res = ddsim.setDetectorModel( detModel )
+    self.assertEqual( res['Message'], "No known models" )
+
+  @patch( "ILCDIRAC.Interfaces.API.NewInterface.Applications.DDSim.getKnownDetectorModels",
+          new = Mock(return_value=S_OK({'CLIC_o2_v04':"/some/path"})))
+  def test_setDetectorModel3( self ):
+    """test DDSIm setDetectorModel is not known....................................................."""
+    detModel = "ATLAS"
+    ddsim = DDSim()
+    ret = ddsim.setDetectorModel( detModel )
+    self.assertEqual( ddsim.detectorModel, '' )
+    self.assertFalse( ret['OK'] )
+    self.assertIn( "Unknown detector model in ddsim: ATLAS", ret['Message'] )
+
+  @patch( "os.path.exists", new = Mock(return_value=True ) )
+  def test_setDetectorModel_TB_success( self ):
+    """test DDSIm setDetectorModel tarBall success.................................................."""
+    detModel = "CLIC_o2_v03"
+    ext = ".tar.gz"
+    ddsim = DDSim()
+    ddsim.setDetectorModel( detModel+ext )
+    self.assertEqual( ddsim.detectorModel, detModel )
+    self.assertTrue( detModel+ext in ddsim.inputSB )
+
+  @patch( "os.path.exists", new = Mock(return_value=False))
+  def test_setDetectorModel_TB_notLocal( self ):
+    """test DDSIm setDetectorModel tarBall notLocal................................................."""
+    detModel = "CLIC_o2_v03"
+    ext = ".tgz"
+    ddsim = DDSim()
+    ddsim.setDetectorModel( detModel+ext )
+    self.assertEqual( ddsim.inputSB, [] )
+    self.assertEqual( ddsim.detectorModel, detModel )
+
+  def test_setDetectorModel_LFN_succcess( self ):
+    """test DDSIm setDetectorModel lfn success......................................................"""
+    detModel = "lfn:/ilc/user/s/sailer/CLIC_o2_v03.tar.gz"
+    ddsim = DDSim()
+    ddsim.setDetectorModel( detModel )
+    self.assertEqual( ddsim.detectorModel, "CLIC_o2_v03" )
+    self.assertTrue( detModel in ddsim.inputSB )
+
+  def test_setStartFrom1( self ):
+    """test DDSIm setStartFrom 1...................................................................."""
+    ddsim = DDSim()
+    ddsim.setStartFrom( "Arg")
+    self.assertTrue( ddsim._errorDict )
+
+  def test_setStartFrom2( self ):
+    """test DDSIm setStartFrom 2...................................................................."""
+    ddsim = DDSim()
+    ddsim.setStartFrom( 42 )
+    self.assertEqual( ddsim.startFrom, 42 )
+
+  def test_getKnownDetModels1( self ):
+    """test getKnownDetectorModels failure no version..............................................."""
+    ddsim = DDSim()
+    ret = ddsim.getKnownDetectorModels()
+    self.assertFalse( ret['OK'] )
+    self.assertEqual( "No software version defined", ret['Message'] )
+
+  def test_getKnownDetModels2( self ):
+    """test getKnownDetectorModels success.........................................................."""
+    ddsim = DDSim()
+    ddsim.version = "test"
+    import DIRAC
+    ddsim._ops = create_autospec(DIRAC.ConfigurationSystem.Client.Helpers.Operations.Operations, spec_set=True)
+    ddsim._ops.getOptionsDict.return_value = S_OK({"detModel1":"/path", "detModel2":"/path2"})
+    ret = ddsim.getKnownDetectorModels()
+    self.assertIn( "detModel1", ret['Value'] )
+    self.assertTrue( ret['OK'] )
+
+def runTests():
+  """Runs our tests"""
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase( TestDDSim )
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
+  print testResult
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase( DDSimTestCase )
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
+  print testResult
 
 
+if __name__ == '__main__':
+  runTests()
