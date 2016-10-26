ea9a46e0c91d41d50b6050280915cf92e2387bec
Jan Ebbing <jan.hendrik.ebbing@cern.ch>
2016-10-26 14:14:46 +0200

    Refactored PrepareLibs test to use the new method.

--- Core/Utilities/tests/Test_PrepareLibs.py
+++ Core/Utilities/tests/Test_PrepareLibs.py
@@ -7,7 +7,7 @@ import sys
 from mock import patch, MagicMock as Mock
 
 from ILCDIRAC.Core.Utilities.PrepareLibs import removeLibc, main
-from ILCDIRAC.Tests.Utilities.GeneralUtils import assertEqualsImproved
+from ILCDIRAC.Tests.Utilities.GeneralUtils import assertEqualsImproved, assertMockCalls
 
 __RCSID__ = "$Id$"
 
@@ -23,15 +23,10 @@ class TestPrepareLibs( unittest.TestCase ):
          patch( '%s.os.listdir' % MODULE_NAME, new=Mock(return_value=[ 'directory_content1.txt', 'libc.so', 'libstdc++.so' ]) ) as listdir_mock:
       result = removeLibc( 'mytestpath' )
       self.assertTrue( result )
-      chdir_mock.assert_any_call( 'mytestpath' )
-      chdir_mock.assert_called_with( 'current_dir' )
-      assertEqualsImproved( len( chdir_mock.mock_calls ), 2, self )
+      assertMockCalls( chdir_mock, [ 'mytestpath', 'current_dir' ], self )
       listdir_mock.assert_called_once_with( 'mytestpath' )
-      remove_mock.assert_any_call( 'mytestpath/libc.so' )
-      remove_mock.assert_called_with( 'mytestpath/libstdc++.so' )
-      assertEqualsImproved( len( remove_mock.mock_calls ), 2, self )
-      getcwd_mock.assert_called_with()
-      assertEqualsImproved( len( getcwd_mock.mock_calls ), 4, self )
+      assertMockCalls( remove_mock, [ 'mytestpath/libc.so', 'mytestpath/libstdc++.so' ], self )
+      assertMockCalls( getcwd_mock, [ () ] * 4, self )
 
   def test_remove_libc_chdir_fails( self ):
     with patch( '%s.os.getcwd' % MODULE_NAME, new=Mock(side_effect=[ 'current_dir', 'mytestpath', 'mytestpath', 'mytestpath' ]) ) as getcwd_mock, \
@@ -52,13 +47,10 @@ class TestPrepareLibs( unittest.TestCase ):
          patch( '%s.os.listdir' % MODULE_NAME, new=Mock(return_value=[ 'directory_content1.txt', 'libc.so', 'libstdc++.so' ]) ) as listdir_mock:
       result = removeLibc( 'mytestpath' )
       self.assertFalse( result )
-      chdir_mock.assert_any_call( 'mytestpath' )
-      chdir_mock.assert_called_with( 'current_dir' )
-      assertEqualsImproved( len( chdir_mock.mock_calls ), 2, self )
+      assertMockCalls( chdir_mock, [ 'current_dir', 'mytestpath' ], self )
       listdir_mock.assert_called_once_with( 'mytestpath' )
       remove_mock.assert_called_once_with( 'mytestpath/libc.so' )
-      getcwd_mock.assert_called_with()
-      assertEqualsImproved( len( getcwd_mock.mock_calls ), 3, self )
+      assertMockCalls( getcwd_mock, [ () ] * 3, self )
 
   def test_remove_libc_nothing_to_remove( self ):
     with patch( '%s.os.getcwd' % MODULE_NAME, new=Mock(side_effect=[ 'current_dir', 'mytestpath', 'mytestpath', 'mytestpath' ]) ) as getcwd_mock, \
@@ -67,13 +59,10 @@ class TestPrepareLibs( unittest.TestCase ):
          patch( '%s.os.listdir' % MODULE_NAME, new=Mock(return_value=[ 'directory_content1.txt', 'not_a_library.txt', 'dont_delete_me_libsomething.so', 'innocentlibc_.so' ]) ) as listdir_mock:
       result = removeLibc( 'mytestpath' )
       self.assertTrue( result )
-      chdir_mock.assert_any_call( 'mytestpath' )
-      chdir_mock.assert_called_with( 'current_dir' )
-      assertEqualsImproved( len( chdir_mock.mock_calls ), 2, self )
+      assertMockCalls( chdir_mock, [ 'mytestpath', 'current_dir' ], self )
       listdir_mock.assert_called_once_with( 'mytestpath' )
       self.assertFalse( remove_mock.called )
-      getcwd_mock.assert_called_with()
-      assertEqualsImproved( len( getcwd_mock.mock_calls ), 2, self )
+      assertMockCalls( getcwd_mock, [ () ] * 2, self )
 
   def test_main( self ):
     backup_sys = sys.modules['sys']
