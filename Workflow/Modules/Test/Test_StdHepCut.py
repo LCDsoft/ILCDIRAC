36303f9d0b443c95e8a164ccdd334985e1209b8a
Jan Ebbing <jan.hendrik.ebbing@cern.ch>
2016-11-09 17:38:04 +0100

    Complete StdHepCut runIt tests.

--- Workflow/Modules/Test/Test_StdHepCut.py
+++ Workflow/Modules/Test/Test_StdHepCut.py
@@ -9,20 +9,17 @@ from ILCDIRAC.Tests.Utilities.FileUtils import FileUtil
 from ILCDIRAC.Tests.Utilities.GeneralUtils import assertEqualsImproved, assertDiracFailsWith, \
   assertDiracSucceeds, assertDiracSucceedsWith_equals, assertMockCalls
 from ILCDIRAC.Workflow.Modules.StdHepCut import StdHepCut
-from DIRAC import S_ERROR
+from DIRAC import S_OK, S_ERROR
 
 __RCSID__ = "$Id$"
 
 MODULE_NAME = 'ILCDIRAC.Workflow.Modules.StdHepCut'
 
-#pylint: disable=protected-access
 class StdHepCutTestCase( unittest.TestCase ):
   """ Contains tests for the StdHepCut class"""
 
   def setUp( self ):
     """set up the objects"""
-    # Mock out modules that spawn other threads
-    #sys.modules['DIRAC.DataManagementSystem.Client.DataManager'] = Mock()
     self.shc = StdHepCut()
 
   def test_applicationspecificinputs_nooutputfile( self ):
@@ -151,3 +148,128 @@ class StdHepCutTestCase( unittest.TestCase ):
     with patch('%s.getSoftwareFolder' % MODULE_NAME, new=Mock(return_value=S_ERROR( 'getsw_test' ))) as getfolder_mock:
       assertDiracFailsWith( self.shc.runIt(), 'getsw_test', self )
       getfolder_mock.assert_called_once_with( 'testPlatform', 'AppTestName', 'vT' )
+
+  def test_runit_getsteeringdir_fails( self ):
+    self.shc.OutputFile = 'something'
+    self.shc.SteeringFile = '/my/dir/SteerFile.testme'
+    ( self.shc.platform, self.shc.applicationName, self.shc.applicationVersion ) = ( 'testPlatform',
+                                                                                     'AppTestName', 'vT' )
+    with patch('%s.getSoftwareFolder' % MODULE_NAME, new=Mock(return_value=S_OK('SoftDir'))), \
+         patch('%s.getSteeringFileDirName' % MODULE_NAME, new=Mock(return_value=S_ERROR('getsteeringdir_err_test'))) as getsteering_mock, \
+         patch('%s.os.path.exists' % MODULE_NAME, new=Mock(return_value=False)) as exists_mock:
+      assertDiracFailsWith( self.shc.runIt(), 'getsteeringdir_err_test', self )
+      getsteering_mock.assert_called_once_with( 'testPlatform', 'AppTestName', 'vT' )
+      exists_mock.assert_called_once_with( 'SteerFile.testme' )
+
+  def test_runit_copy_steeringfile_fails( self ):
+    self.shc.OutputFile = 'something'
+    self.shc.SteeringFile = '/my/dir/SteerFile.testme'
+    ( self.shc.platform, self.shc.applicationName, self.shc.applicationVersion ) = ( 'testPlatform',
+                                                                                     'AppTestName', 'vT' )
+    with patch('%s.getSoftwareFolder' % MODULE_NAME, new=Mock(return_value=S_OK('SoftDir'))), \
+         patch('%s.getSteeringFileDirName' % MODULE_NAME, new=Mock(return_value=S_OK('/my/steer/dir'))), \
+         patch('%s.os.path.exists' % MODULE_NAME, new=Mock(side_effect=[False,True])) as exists_mock, \
+         patch('%s.shutil.copy' % MODULE_NAME, new=Mock(side_effect=EnvironmentError('copy_test_err'))) as copy_mock:
+      assertDiracFailsWith( self.shc.runIt(), 'failed to access file steerfile.testme: copy_test_err', self )
+      assertMockCalls( exists_mock, [ 'SteerFile.testme', '/my/steer/dir/SteerFile.testme' ], self )
+      copy_mock.assert_called_once_with( '/my/steer/dir/SteerFile.testme', './SteerFile.testme' )
+
+  def test_runit_missinglog( self ):
+    exists_dict = { 'SteerFile.testme' : True, 'myAppTestLog.log' : False }
+    open_mock = Mock()
+    open_mock.readlines.side_effect = [ 'line1\n', 'line2\n', 'newline\n', '\n', 'ok\n' ]
+    self.shc.scriptName = 'my_test_script.sh'
+    self.shc.OutputFile = 'something'
+    self.shc.applicationLog = 'myAppTestLog.log'
+    self.shc.SteeringFile = '/my/dir/SteerFile.testme'
+    ( self.shc.platform, self.shc.applicationName, self.shc.applicationVersion ) = ( 'testPlatform',
+                                                                                     'AppTestName', 'vT' )
+    with patch('%s.getSoftwareFolder' % MODULE_NAME, new=Mock(return_value=S_OK('SoftDir'))), \
+         patch('%s.os.path.exists' % MODULE_NAME, new=Mock(side_effect=lambda path: exists_dict[path])), \
+         patch('%s.open' % MODULE_NAME, new=Mock(return_value=open_mock)) as open_mock, \
+         patch('%s.os.remove' % MODULE_NAME, new=Mock()) as remove_mock, \
+         patch('%s.os.chmod' % MODULE_NAME, new=Mock()) as chmod_mock, \
+         patch('%s.shellCall' % MODULE_NAME, new=Mock(return_value=S_OK( ( 'status', )))) as shell_mock, \
+         patch('%s.StdHepCut.prepareScript' % MODULE_NAME, new=Mock()) as prep_mock:
+      assertDiracFailsWith( self.shc.runIt(), 'apptestname did not produce the expected log', self )
+      self.assertFalse( remove_mock.called )
+      shell_mock.assert_called_once_with( 0, 'sh -c "./my_test_script.sh"',
+                                          callbackFunction = self.shc.redirectLogOutput, bufferLimit = 20971520 )
+      chmod_mock.assert_called_once_with( 'my_test_script.sh', 0755 )
+      prep_mock.assert_called_once_with( 'SoftDir' )
+
+  def test_runit_missing_numbers( self ):
+    exists_dict = { '/my/steer/dir/SteerFile.testme' : False, 'SteerFile.testme' : False,
+                    'myAppTestLog.log' : True }
+    open_mock = Mock()
+    open_mock.readlines.side_effect = [ 'line1\n', 'line2\n', 'newline\n', '\n', 'ok\n' ]
+    open_mock.__enter__.return_value = [ 'someline\n', 'otherline' ]
+    self.shc.applicationLog = 'myAppTestLog.log'
+    self.shc.OutputFile = 'something'
+    self.shc.SteeringFile = '/my/dir/SteerFile.testme'
+    ( self.shc.platform, self.shc.applicationName, self.shc.applicationVersion ) = ( 'testPlatform',
+                                                                                     'AppTestName', 'vT' )
+    with patch('%s.getSoftwareFolder' % MODULE_NAME, new=Mock(return_value=S_OK('SoftDir'))), \
+         patch('%s.os.path.exists' % MODULE_NAME, new=Mock(side_effect=lambda path: exists_dict[path])), \
+         patch('%s.open' % MODULE_NAME, new=Mock(return_value=open_mock)) as open_mock, \
+         patch('%s.os.remove' % MODULE_NAME, new=Mock()) as remove_mock, \
+         patch('%s.os.chmod' % MODULE_NAME, new=Mock()), \
+         patch('%s.shellCall' % MODULE_NAME, new=Mock(return_value=S_OK( ( 'status', )))), \
+         patch('%s.StdHepCut.prepareScript' % MODULE_NAME, new=Mock()), \
+         patch('%s.getSteeringFileDirName' % MODULE_NAME, new=Mock(return_value=S_OK('/my/steer/dir'))):
+      assertDiracFailsWith( self.shc.runIt(), 'apptestname exited with status 1', self )
+      remove_mock.assert_called_once_with( 'myAppTestLog.log' )
+
+  def test_runit_complete( self ):
+    exists_dict = { '/my/steer/dir/SteerFile.testme' : False, 'SteerFile.testme' : False,
+                    'myAppTestLog.log' : True }
+    open_mock = Mock()
+    open_mock.readlines.side_effect = [ 'line1\n', 'line2\n', 'newline\n', '\n', 'ok\n' ]
+    open_mock.__enter__.return_value = [ 'Events kept 12', 'Events passing cuts 2984', 'Events total 2996' ]
+    self.shc.applicationLog = 'myAppTestLog.log'
+    self.shc.OutputFile = 'something'
+    self.shc.SteeringFile = '/my/dir/SteerFile.testme'
+    self.shc.workflow_commons[ 'Luminosity' ] = 13.0
+    ( self.shc.platform, self.shc.applicationName, self.shc.applicationVersion ) = ( 'testPlatform',
+                                                                                     'AppTestName', 'vT' )
+    with patch('%s.getSoftwareFolder' % MODULE_NAME, new=Mock(return_value=S_OK('SoftDir'))), \
+         patch('%s.os.path.exists' % MODULE_NAME, new=Mock(side_effect=lambda path: exists_dict[path])), \
+         patch('%s.open' % MODULE_NAME, new=Mock(return_value=open_mock)) as open_mock, \
+         patch('%s.os.remove' % MODULE_NAME, new=Mock()) as remove_mock, \
+         patch('%s.os.chmod' % MODULE_NAME, new=Mock()), \
+         patch('%s.shellCall' % MODULE_NAME, new=Mock(return_value=S_OK( ( 0, )))), \
+         patch('%s.StdHepCut.prepareScript' % MODULE_NAME, new=Mock()), \
+         patch('%s.getSteeringFileDirName' % MODULE_NAME, new=Mock(return_value=S_OK('/my/steer/dir'))):
+      assertDiracSucceedsWith_equals( self.shc.runIt(), 'AppTestName vT Successful', self )
+      remove_mock.assert_called_once_with( 'myAppTestLog.log' )
+      assertEqualsImproved( self.shc.workflow_commons[ 'Luminosity' ], 13.0 * ( 1. * 12 / 2984 ), self )
+      assertEqualsImproved( self.shc.workflow_commons[ 'Info' ],
+                            { 'stdhepcut' : { 'Reduction' : 1. * 12 / 2984,
+                                              'CutEfficiency' : 1. * 2984 / 2996 } }, self )
+
+  def test_runit_othercases( self ):
+    exists_dict = { 'SteerFile.testme' : True, 'myAppTestLog.log' : False }
+    open_mock = Mock()
+    open_mock.readlines.side_effect = [ 'line1\n', 'line2\n', 'newline\n', '\n', 'ok\n' ]
+    open_mock.__enter__.return_value = [ 'Events kept 23', 'Events passing cuts 14', 'Events total 37' ]
+    self.shc.scriptName = 'my_test_script.sh'
+    self.shc.ignoreapperrors = True
+    self.shc.OutputFile = 'something'
+    self.shc.applicationLog = 'myAppTestLog.log'
+    self.shc.SteeringFile = '/my/dir/SteerFile.testme'
+    self.shc.workflow_commons[ 'Info' ] = { 'some_entry' : 'rememberMe' }
+    self.shc.MaxNbEvts = 100
+    ( self.shc.platform, self.shc.applicationName, self.shc.applicationVersion ) = ( 'testPlatform',
+                                                                                     'AppTestName', 'vT' )
+    with patch('%s.getSoftwareFolder' % MODULE_NAME, new=Mock(return_value=S_OK('SoftDir'))), \
+         patch('%s.os.path.exists' % MODULE_NAME, new=Mock(side_effect=lambda path: exists_dict[path])), \
+         patch('%s.open' % MODULE_NAME, new=Mock(return_value=open_mock)) as open_mock, \
+         patch('%s.os.remove' % MODULE_NAME, new=Mock()), \
+         patch('%s.os.chmod' % MODULE_NAME, new=Mock()), \
+         patch('%s.shellCall' % MODULE_NAME, new=Mock(return_value=S_OK( ( 0, )))), \
+         patch('%s.StdHepCut.prepareScript' % MODULE_NAME, new=Mock()):
+      assertDiracSucceedsWith_equals( self.shc.runIt(), 'AppTestName vT Successful', self )
+      assertEqualsImproved( self.shc.workflow_commons[ 'Info'],
+                            { 'some_entry' : 'rememberMe', 'stdhepcut' :
+                              { 'Reduction' : 1. * 23 / 14, 'CutEfficiency' : 1. * 14 / 37 } }, self )
+      assert 'Luminosity' not in self.shc.workflow_commons
