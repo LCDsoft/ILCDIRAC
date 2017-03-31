f4c4c04cf61976c8da090e8430013950dc72ef65
Jan Ebbing <jan.hendrik.ebbing@cern.ch>
2017-03-31 15:56:40 +0200

    Add 3 files: CalibrationDistributedScript.py, the start at running the script on the worker nodes. run_calibration.sh, which prepares the environment before running CalibrationDistributedScript.py, dev_create_events.py which I used to generate the particles.

---
+++ CalibrationSystem/Client/run_calibration.sh
@@ -0,0 +1,6 @@
+source $1/init_ilcsoft.sh # Directory which to source
+/usr/bin/krenew
+export KRB5CCNAME=FILE:./credentials.krb5 
+export AKLOG=/usr/bin/aklog
+CLIENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # Gets the full path to this file
+python $CLIENT_DIR/CalibrationDistributedScript.py $2 # StartFrom
