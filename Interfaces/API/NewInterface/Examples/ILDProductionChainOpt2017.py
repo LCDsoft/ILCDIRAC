7fc6f04b407d22b007d1ef667d99b52fa2f0413c
akiyamiyamoto <akiya.miyamoto@kek.jp>
2017-04-11 15:24:12 +0900

    ILDProductionChainOpt2017.py : Prepared running example and output SE could be different depending on application.

--- Interfaces/API/NewInterface/Examples/ILDProductionChainOpt2017.py
+++ Interfaces/API/NewInterface/Examples/ILDProductionChainOpt2017.py
@@ -7,27 +7,56 @@ Create Chain of productions for ILD
 
 '''
 
-#pylint: disable=invalid-name, wrong-import-position
+#pylint: disable=invalid-name, wrong-import-position, bad-whitespace, line-too-long
+#pylint: disable=bad-indentation
+
+import os
+import pprint
 
 from DIRAC.Core.Base import Script
 Script.parseCommandLine()
 
 from DIRAC.ConfigurationSystem.Client.Helpers.Operations    import Operations
 
-
 from ILCDIRAC.Interfaces.API.NewInterface.ILDProductionJobOpt2017 import ILDProductionJobOpt2017
 from ILCDIRAC.Interfaces.API.NewInterface.Applications     import Mokka, Marlin, OverlayInput, DDSim
 from ILCDIRAC.Interfaces.API.NewInterface.Applications     import SLCIOSplit, StdHepSplit
 
 
+####################################################################################
+'''
+Example to run ILDProductionChainOpt2017.py. This script creates 2 transformations
+Mokka  or DD4Sim simulation, and Marlin reconsturuction with Overlay.
 
+stdhepsplit won't work for the moment due to an innconsistensy
+in creating output filename. This example output sim/rec output to
+a temporary directory in /ilc/prod/ild/test/temp1
+
+Simulation and recconstruction step could process more than one GenProcessID.
+prodid meta key should be defined to select input file properly.
+
+Define Input serch path, output and input file path properly so that
+proper files can be found and processed. This script used prodid = 500002,
+for input data query. prodid=500002 is selected, because it will be not used
+for the time being.  prodid should be defined to the directory, not as a file meta key
+because it is defined as a indexed meta key of directory.
+
+This script limits nb_task for sim and rec to 5. This should be increased to process
+more files. dirac-ilc-add-tasks-to-prod command can be used for this purpose.
+
+'''
+#####################################################################################
+
+
+
+dryrun       = False
 # TODO: add evttype to the ProdGroup
-analysis         = 'ILD-DDSim-Test' ##Some analysis: the prods will belong to the ProdGroup
+analysis         = 'ILD-Opt2017-Test' ##Some analysis: the prods will belong to the ProdGroup
 my_evttype       = '3f'
 my_evtclass      = '3f'
 selectedfile     = 0
-prodid           = 500001
-genprocessname   = 'ae_ell'
+prodid           = 500002
+genprocessname   = 'ea_lvv'
 process          = ''
 energy           = 500. ##This is mostly needed to define easily the steering files and the overlay parameters
 analysis += '_' + my_evttype
@@ -47,10 +76,10 @@ DDSimILDConfig = "v01-19_lcgeo"
 
 ILDConfig = '' ## Set below for different energies
 MokkaVer     = "080003"
-MokkaILDConfig = "v01-17-11-p01"
+# MokkaILDConfig = "v01-17-11-p01"
+MokkaILDConfig = "v01-14-01-p00"
 
 banned_sites = [""]
-dryrun       = False
 # do not register anything nor create anything.
 # Should be used once the splitting-at-stdhep-level prods are submitted.
 
@@ -78,26 +107,53 @@ UseDD4hepGeometry = False
 
 ## ILDConfig for Marlin with or without DD4hep Geometry
 ILDConfigRec_DD = "v01-19_lcgeo"
-ILDConfigRec    = "v01-17-11-p01"
+MarlinVer_DD = "ILCSoft-01-19_gcc48"
+# ILDConfigRec    = "v01-17-11-p01"
+# MarlinVer_DBD = "ILCSoft-01-17-11_gcc48"
+ILDConfigRec = "v01-16-p05_500"
+MarlinVer_DBD = "v01-16-02"
 
 ILDConfig = ILDConfigRec_DD if UseDD4hepGeometry else ILDConfigRec
-
 ILDConfigSim = DDSimILDConfig if UseDD4hepGeometry else MokkaILDConfig
+MarlinVer = MarlinVer_DD if UseDD4hepGeometry else MarlinVer_DBD
 
-additional_name   = '_' + genprocessname + '_20170324_06_' + str(selectedfile) + '_ildconfig-' + ILDConfig
+additional_name   = '_' + analysis + '_' + genprocessname + '_20170411_1_' + str(selectedfile) + '_ildconfig-' + ILDConfig
 
 energyMachinePars        = meta_energy + '-' + machineParameters
 # Following variables avoid output from stdhepsplit being used
 # as input for the same production. Also speed up the job submission.
-basepath = Operations().getValue( '/Production/ILC_ILD/BasePath', '/ilc/prod/ilc/mc-dbd/test/ild' ).rstrip("/")
+
+# basepath = Operations().getValue( '/Production/ILC_ILD/BasePath', '/ilc/prod/ilc/mc-dbd/ild' ).rstrip("/")
+# matchToInput_stdhepsplit = '/ilc/prod/ilc/mc-dbd/generated/' + energyMachinePars + '/' + my_evtclass
+# matchToInput_mokka       = '/ilc/prod/ilc/mc-dbd.generated/' + energyMachinePars + '/' + my_evttype
+# matchToInput_marlin      = basepath + "sim/" + energyMachinePars + '/' + my_evttype + '/' + detectorModel + '/' + ILDConfigSim
+# recPath = '/ilc/prod/ilc/mc-dbd/ild/'
+# dstPath = '/ilc/prod/ilc/mc-dbd.log/ild/'
+
+# grand_base = '/ilc/prod/ilc/'
+grand_base = '/ilc/prod/ilc/ild/test/temp1/'  # Test area for development
+basepath = grand_base + 'mc-opt/ild/'
+diskpath = grand_base + 'mc-opt.disk/ild/'
+my_basepath = basepath # my_basepath is a base path for Mokka and Marlin
 
 matchToInput_stdhepsplit = '/ilc/prod/ilc/mc-dbd/generated/' + energyMachinePars + '/' + my_evtclass
-matchToInput_mokka       = '/ilc/prod/ilc/mc-dbd.generated/' + energyMachinePars + '/' + my_evttype
-matchToInput_marlin      = basepath + "sim/" + energyMachinePars + '/' + my_evttype + '/' + detectorModel + '/' + ILDConfigSim
-recPath = '/ilc/prod/ilc/mc-dbd/ild/'
-dstPath = '/ilc/prod/ilc/mc-dbd.log/ild/'
+# matchToInput_stdhepsplit is where input stdhep files are for stdhepsplit valid when stdhepsplit production is included.
+stdhepsplit_basepath = diskpath + 'splitted/' # Output directory of stdhepsplit, equal to matchToInput_mokka,
 
-SE        = "CERN-SRM"
+
+matchToInput_mokka = grand_base + 'gensplit/' + energyMachinePars + '/' + my_evttype  + '/run002' # Where to find input for Mokka
+# matchToInput_mokka is input directory for simulation. This example uses Mokka input prepared by other script
+matchToInput_marlin = basepath + "sim/" + energyMachinePars + '/' + my_evttype + '/' + detectorModel + '/' + ILDConfigSim
+recPath = basepath
+dstPath = diskpath
+
+# Define output SE depending on expected data size
+SE_stdhepsplit = "PNNL-SRM"
+SE_sim = "KEK-SRM"
+SE_psplit = "PNNL-SRM"
+SE_rec = "DESY-SRM"
+
+
 ###LCG_SITE  = "LCG.KEK.jp"
 input_sand_box = [""]
 ##This is where magic happens
@@ -125,7 +181,7 @@ else:
   meta['MachineParams'] = machineParameters
   meta['ProdID']        = prodid
 
-inputFileFolder= "/ilc/prod/ilc/ild/test/temp1/gensplit/500-TDR_ws/3f/run001"
+# inputFileFolder= "/ilc/prod/ilc/ild/test/temp1/gensplit/500-TDR_ws/3f/run001"
 
 #DoSplit at stdhep level
 activesplitstdhep   = False
@@ -146,8 +202,8 @@ else:
 
 #Do Sim
 ild_sim  = True
-nbtasks_sim = 10
-#nbtasks_sim = 1 #Take 10 files from input meta data query result
+nbtasks_sim = 3
+#nbtasks_sim = 10 # create 10 jobs from input meta data query result. -1 to create for all files.
 #It can be extended with dirac-ilc-add-tasks-to-prod
 #It's possible to get this number automatically by getting the number of events per file (if known)
 #nbtasks = math.ceil(number_of_events_to_process/nb_events_per_signal_file) #needs import math
@@ -157,12 +213,26 @@ activesplit   = False
 nbevtsperfile = 200
 
 #Do Reco with Overlay
-ild_rec_ov    = False
-nbtasks_rec_ov = -1 # See comment on nbtasks_sim
+ild_rec_ov    = True
+nbtasks_rec_ov = 3 # See comment on nbtasks_sim
 #Do Reco
-ild_rec       = True # please, use WITH OVERLAY
+ild_rec       = False # please, use WITH OVERLAY
 nbtasks_rec = -1 # See comment on nbtasks_sim
 
+print "####################################################################################"
+if activesplitstdhep:
+  print "matchToInput_stdhepsplit (input)      = " + matchToInput_stdhepsplit + '  (Valid when stdhep production is included)'
+  print "stdhepsplit_basepath (output)         = " + stdhepsplit_basepath + '  (Valid when stdhep production is included)'
+
+print "matchToInput_mokka (input)            = " + matchToInput_mokka
+print "matchToInput_marlin (input)           = " + matchToInput_marlin
+print "Basepath for sim&rec (output)         = " + basepath
+print "Diskpath for stdhepsplit&dst (output) = " + diskpath
+print "Outtput SE : stdhepsplit(%s), SUM(%s), psplit(%s), REC&DST(%s)" %  (SE_stdhepsplit, SE_sim, SE_psplit, SE_rec)
+print "####################################################################################"
+##############################################################################################
+## [PART2] Below is not to be touched
+##############################################################################################
 ###### Whatever is below is not to be touched... Or at least only when something changes
 
 ##Split
@@ -258,8 +328,10 @@ if ild_rec:
 ###################################################################################
 ### HERE WE DEFINE THE PRODUCTIONS
 if activesplitstdhep and meta:
+  print "################## Createing a production for stdhepsplit. Input meta is"
+  pprint.pprint(meta)
   pstdhepsplit = ILDProductionJobOpt2017()
-  pstdhepsplit.basepath = '/ilc/prod/ilc/mc-dbd.generated/ild' # Sailer suggestion
+  pstdhepsplit.basepath = stdhepsplit_basepath # Sailer suggestion
   pstdhepsplit.matchToInput = matchToInput_stdhepsplit
   pstdhepsplit.setDryRun(dryrun)
   #pstdhepsplit.setILDConfig(ILDConfig) ## stdhepsplit does not need ILDConfig
@@ -271,7 +343,6 @@ if activesplitstdhep and meta:
   pstdhepsplit.setBannedSites(banned_sites)
   pstdhepsplit.setProdPlugin('Limited') # exit with error: it seems i need
                                         # to set the Prod. plugin
-  pstdhepsplit.setNbOfTasks(nbtasks_split)
 
   # generated files has not SoftwareTag: we exclude it from inputdataquery, but
   # reinserted at the end of the module to be used in the next simulation module
@@ -284,7 +355,7 @@ if activesplitstdhep and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
-  pstdhepsplit.setOutputSE(SE)
+  pstdhepsplit.setOutputSE(SE_stdhepsplit)
   wname = process+"_"+str(energy)+"_split"
   wname += additional_name
   pstdhepsplit.setWorkflowName(wname)
@@ -295,7 +366,7 @@ if activesplitstdhep and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
-  pstdhepsplit.addFinalization(True,True,True,True)
+  pstdhepsplit.addFinalization(True, True, True, True)
   descrp = "Splitting stdhep files"
   if additional_name:
     descrp += ", %s"%additional_name
@@ -313,17 +384,23 @@ if activesplitstdhep and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
+  pstdhepsplit.setNbOfTasks(nbtasks_split)
   #As before: get the metadata for this production to input into the next
   meta = pstdhepsplit.getMetadata()
   if tmp_softwaretag_val: # reinsert SoftwareTag: used in path construction
     meta.update({'SoftwareTag' : tmp_softwaretag_val})
 
-  print " Done With Stdhepsplit","\n"*5
+  print "### Following meta data were created for subsequent simulation production"
+  pprint.pprint(meta)
+  print "### Done With Stdhepsplit", "\n"*5
 
 if ild_sim and meta:
+  print "################## Createing a production for simulation. Input meta is"
+  pprint.pprint(meta)
   ####################
   ##Define the second production (simulation). Notice the setInputDataQuery call
   pSim = ILDProductionJobOpt2017()
+  pSim.basepath = my_basepath
   pSim.matchToInput = matchToInput_mokka
   pSim.setDryRun(dryrun)
   pSim.setProdPlugin('Standard')
@@ -334,16 +411,15 @@ if ild_sim and meta:
   pSim.setLogLevel("verbose")
   pSim.setProdType('MCSimulation_ILD')
   pSim.setBannedSites(banned_sites)
-  pSim.setInputSandbox( input_sand_box )
+  pSim.setInputSandbox(input_sand_box)
   # pSim.setDestination(LCG_SITE)
-  pmo.setProdPlugin('Limited')
-  pmo.setNbOfTasks(nbtasks_sim)
+  pSim.setProdPlugin('Limited')
 
   res = pSim.setInputDataQuery(meta)
   if not res['OK']:
     print res['Message']
     exit(1)
-  pSim.setOutputSE(SE)
+  pSim.setOutputSE(SE_sim)
   wname = process+"_"+str(energy)+"_ild_sim"
   wname += additional_name
   pSim.setWorkflowName(wname)
@@ -356,7 +432,7 @@ if ild_sim and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
-  pSim.addFinalization(True,True,True,True)
+  pSim.addFinalization(True, True, True, True)
   descrp = "%s model" % detectorModel
 
   if additional_name:
@@ -374,11 +450,16 @@ if ild_sim and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
+  pSim.setNbOfTasks(nbtasks_sim)
   #As before: get the metadata for this production to input into the next
   meta = pSim.getMetadata()
 
+  print "### Done With a creation of ild_sim transformation", "\n"*5
+
 ##Split at slcio level (after sim)
 if activesplit and meta:
+  print "################## Creating a prduction for sim-slcio split: input meta is"
+  pprint.pprint(meta)
   #######################
   ## Split the input files.
   psplit =  ILDProductionJobOpt2017()
@@ -386,12 +467,12 @@ if activesplit and meta:
   psplit.setCPUTime(30000)
   psplit.setLogLevel("verbose")
   psplit.setProdType('Split')
-  psplit.setInputSandbox( input_sand_box )
+  psplit.setInputSandbox(input_sand_box)
   res = psplit.setInputDataQuery(meta)
   if not res['OK']:
     print res['Message']
     exit(1)
-  psplit.setOutputSE(SE)
+  psplit.setOutputSE(SE_psplit)
   wname = process+"_"+str(energy)+"_split"
   wname += additional_name
   psplit.setWorkflowName(wname)
@@ -402,7 +483,7 @@ if activesplit and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
-  psplit.addFinalization(True,True,True,True)
+  psplit.addFinalization(True, True, True, True)
   descrp = "Splitting slcio files"
   if additional_name:
     descrp += ", %s"%additional_name
@@ -420,24 +501,28 @@ if activesplit and meta:
   meta = psplit.getMetadata()
 
 if ild_rec and meta:
+  print "################## Creating a production for reconstruction WITHOUT overlay. Input meta is"
+  pprint.pprint(meta)
   #######################
   #Define the reconstruction prod
   pma = ILDProductionJobOpt2017()
   pma.setDryRun(dryrun)
+  pma.matchToInput = matchToInput_marlin
   pma.setILDConfig(ILDConfigRec)
   pma.setLogLevel("verbose")
   pma.setProdType('MCReconstruction_ILD')
   pma.setEvtType(my_evttype)
   pma.setReconstructionBasePaths(recPath, dstPath)
   pma.setProdPlugin('Limited')
-  pma.setNbOfTasks(nbtasks_rec)
+#   pma.setInputSandbox(input_sand_box_marlin)
+  pma.setOutputSandbox(["*.log", "*dd4hep.xml", "*.sh", "TaggingEfficiency.root", "PfoAnalysis.root"])
 
   res = pma.setInputDataQuery(meta)
   if not res['OK']:
     print res['Message']
     exit(1)
 
-  pma.setOutputSE(SE)
+  pma.setOutputSE(SE_rec)
   wname = process+"_"+str(energy)+"_ild_rec"
   wname += additional_name
   pma.setWorkflowName(wname)
@@ -448,7 +533,7 @@ if ild_rec and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
-  pma.addFinalization(True,True,True,True)
+  pma.addFinalization(True, True, True, True)
   descrp = "%s, No overlay" % detectorModel
   if additional_name:
     descrp += ", %s"%additional_name
@@ -461,8 +546,11 @@ if ild_rec and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
+  pma.setNbOfTasks(nbtasks_rec)
 
 if ild_rec_ov and meta:
+  print "################## Creating a production for reconstruction WITH overlay. Input meta is"
+  pprint.pprint(meta)
   #######################
   #Define the reconstruction prod
   pmao = ILDProductionJobOpt2017()
@@ -475,15 +563,15 @@ if ild_rec_ov and meta:
   pmao.setLogLevel("verbose")
   pmao.setProdType('MCReconstruction_Overlay_ILD')
   pmao.setBannedSites(banned_sites)
+  pmao.setOutputSandbox(["*.log", "*.sh", "*.xml"])
   pmao.setReconstructionBasePaths(recPath, dstPath)
   pmao.setProdPlugin('Limited')
-  pmao.setNbOfTasks(nbtasks_rec_ov)
 
   res = pmao.setInputDataQuery(meta)
   if not res['OK']:
     print res['Message']
     exit(1)
-  pmao.setOutputSE(SE)
+  pmao.setOutputSE(SE_rec)
   wname = process+"_"+str(energy)+"_ild_rec_overlay"
   wname += additional_name
   pmao.setWorkflowName(wname)
@@ -499,7 +587,7 @@ if ild_rec_ov and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
-  pmao.addFinalization(True,True,True,True)
+  pmao.addFinalization(True, True, True, True)
   descrp = "%s, Overlay" % detectorModel
 
   if additional_name:
@@ -517,3 +605,4 @@ if ild_rec_ov and meta:
   if not res['OK']:
     print res['Message']
     exit(1)
+  pmao.setNbOfTasks(nbtasks_rec_ov)
