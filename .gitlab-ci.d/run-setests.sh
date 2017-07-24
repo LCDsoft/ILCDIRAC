aafc8b2d62b9faac16f8b00110cc931036769b81
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-07-24 10:05:25 +0200

    Add stages in pipeline, pass installed ilcdirac to other stages
    Add coverage to individual stages, ignore pylint reducing coverage average
    Pass coverage file from unittests to subsequent stages to calculate proper total
    
    Backport installation from master and simplify install
    
    Move things to subfolder so we can add to artifact
    
    Use recent git version from cvmfs
    
    Allow failure for se and cvmfs tests for people that cannot use the proxy
    
    Expire artifacts in 1 day
    pass coverage to subsequent tests
    
    Fix missing dirac-proxy-init -x for setest cc7

--- .gitlab-ci.d/run-setests.sh
+++ .gitlab-ci.d/run-setests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source .gitlab-ci.d/set-reportstyle.sh
+export PYTEST_ADDOPTS="${PYTEST_ADDOPTS} --cov-append"
 export XrdSecPROTOCOL=gsi,unix
 py.test $DIR/../Workflow/Modules/Test/Test_SEs.py
