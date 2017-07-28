2faea07a46f3563560896a6d80968b9c88a35306
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-07-28 17:46:35 +0200

    MarlinXML: implement changing group parameters

--- Core/Utilities/MarlinXML.py
+++ Core/Utilities/MarlinXML.py
@@ -15,11 +15,43 @@ def setOverlayFilesParameter( tree, overlay=False, eventsPerBackgroundFile=0 ):
 
   :param tree: XML tree of marlin steering file
   """
+  resOT = __checkOverlayTiming( tree, overlay, eventsPerBackgroundFile )
+  if not resOT['OK']:
+    return resOT
+  overlay = resOT['Value']
+  resBGO = __checkBGOverlay( tree, overlay, eventsPerBackgroundFile )
+  if not resBGO['OK']:
+    return resBGO
+  overlay = resBGO['Value']
+
+  resGroupO = __checkOverlayGroup( tree, overlay, eventsPerBackgroundFile )
+  if not resGroupO['OK']:
+    return resGroupO
+  overlay = resGroupO['Value']
+
+  return S_OK()
+
+def __checkOverlayGroup( tree, overlay, eventsPerBackgroundFile ):
+  """ check if there is an OverlayProcessor, also handling overlay processors that get parameters from a group """
+  groups = tree.findall('group')
+  for group in groups:
+    groupParameters = group.findall('parameter')
+    resG = __checkOverlayTiming( group, overlay, eventsPerBackgroundFile, 'overlaytiming', groupParameters )
+    if not resG['OK']:
+      return resG
+    overlay = resG['Value']
+
+  return S_OK( overlay )
+
+
+
+def __checkOverlayTiming( tree, overlay, _eventsPerBackgroundFile, processorType='overlaytiming', groupParameters=None ):
+  """ check the for the overlayTiming processor and set the appropriate parameter values """
   for processor in tree.findall('processor'):
     # OverlayTiming  processor treatment
-    if processor.attrib.get('name', '').lower().count('overlaytiming') or \
-       processor.attrib.get('type', '').lower().count('overlaytiming'):
-      for param in processor.findall('parameter'):
+    if processor.attrib.get('name', '').lower().count(processorType.lower()) or \
+       processor.attrib.get('type', '').lower().count(processorType.lower()):
+      for param in processor.findall('parameter') + ( groupParameters if groupParameters else [] ):
         if ( param.attrib.get('name') == 'NumberBackground' and param.attrib['value'] == '0.0' ) or \
            ( param.attrib.get('name') == 'NBunchtrain' and param.attrib['value'] == '0' ):
           overlay = False
@@ -27,8 +59,13 @@ def setOverlayFilesParameter( tree, overlay=False, eventsPerBackgroundFile=0 ):
         files = getOverlayFiles()
         if not files:
           return S_ERROR('Could not find any overlay files')
-        __changeProcessorTagValue( processor, 'parameter', 'BackgroundFileNames', '\n'.join(files), "Overlay files changed")
-    # BGOverlay Processor Treatment
+        __changeProcessorTagValue( processor, 'parameter', 'BackgroundFileNames', '\n'.join(files), "Overlay files changed", groupParameters )
+  return S_OK( overlay )
+
+
+def __checkBGOverlay( tree, overlay, eventsPerBackgroundFile ):
+  """ check for the BGOverlay processor used in ILD productions """
+  for processor in tree.findall('processor'):
     if processor.attrib.get('name','').lower().count('bgoverlay'):
       bkg_Type = 'aa_lowpt' #specific to ILD_DBD #FIXME
       params = processor.findall('parameter')
@@ -42,8 +79,7 @@ def setOverlayFilesParameter( tree, overlay=False, eventsPerBackgroundFile=0 ):
         __changeProcessorTagValue( processor, 'parameter', "InputFileNames", "\n".join(files), "Overlay files changed" )
         __changeProcessorTagValue( processor, 'parameter', "NSkipEventsRandom",
                                    "%d" % int( len(files) * eventsPerBackgroundFile ), "NSkipEventsRandom Changed" )
-
-  return S_OK()
+  return S_OK( overlay )
 
 
 def setOutputFileParameter( tree, outputFile, outputREC, outputDST ):
@@ -64,7 +100,7 @@ def setOutputFileParameter( tree, outputFile, outputREC, outputDST ):
   return S_OK()
 
 
-def __changeProcessorTagValue( processor, tagTypename, parameterName, newValue, newComment):
+def __changeProcessorTagValue( processor, tagTypename, parameterName, newValue, newComment, additionalParameters=None ):
   """ modify the value of the tag with *tagTypename* with name attribute equall to *parameterName* to *newValue*
 
   :param processor: XML object representing processor
@@ -73,7 +109,7 @@ def __changeProcessorTagValue( processor, tagTypename, parameterName, newValue, 
   :param newValue: new value to set for the parameter
   :param newComment: comment to set
   """
-  tags = processor.findall( tagTypename )
+  tags = processor.findall( tagTypename ) + ( additionalParameters if additionalParameters else [] )
   for tag in tags:
     if tag.attrib.get('name') == parameterName:
       tag.text = newValue
