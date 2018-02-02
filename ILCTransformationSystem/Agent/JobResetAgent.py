353a3a407d96c41e483d0add2aa7bdf908a9221c
Hamza Zafar <hamza.zafar@cern.ch>
2018-02-02 14:13:17 +0100

    adding treatment funcs for prod and user jobs

--- ILCTransformationSystem/Agent/JobResetAgent.py
+++ ILCTransformationSystem/Agent/JobResetAgent.py
@@ -78,11 +78,14 @@ class JobResetAgent(AgentModule):
     self.log.error(errStr, varMsg)
     self.errors.append(errStr + varMsg)
 
-  def getJobs(self, status, jobType=None):
+  def getJobs(self, status, jobType=None, minorStatus=None):
 
     attrDict = dict(Status=status)
     if jobType:
-      attrDict['JobType']=jobType
+      attrDict['JobType'] = jobType
+
+    if minorStatus:
+      attrDict['MinorStatus'] = minorStatus
 
     res = self.jobMonClient.getJobs(attrDict)
     if not res['OK']:
@@ -92,16 +95,86 @@ class JobResetAgent(AgentModule):
     jobIDs = map(int, res['Value'])
     return S_OK(jobIDs)
 
-  def checkRequest(self, jobIDs):
+
+  def treatUserJobWithNoRequest(self, jobID):
+    self.log.notice("No request found for job: %s" % jobID)
+    res = self.jobMonClient.getJobsMinorStatus(jobID)
+    if not res['OK']:
+      self.logError("Failure to get Minor Status", "Job IDs: %s, Message: %s" % (checkMinorStatList,
+                                                                                 res['Message']))
+    return res
+
+    minorStatus = res['Value'][jobID]['MinorStatus']
+
+    res = self.jobMonClient.getJobsApplicationStatus(checkAppStatList)
+    if not res['OK']:
+      self.logError("Failure to get Application Status", "Job IDs: %s, Message: %s" % (checkAppStatList,
+                                                                                       res['Message']))
+    return res
+
+    appStatus = res['Value'][jobID]['ApplicationStatus']
+
+    if minorStatus in FINAL_MINOR_STATES and appStatus in FINAL_APP_STATES:
+      self.markJob(jobID, "Done")
+    else:
+      self.log.warn("Something not as expected for Job Status, please check: %s" % jobID)
+
+    return S_OK()
+
+
+  def treatUserJobWithRequest(self, jobID, request):
+    if request.Status == "Done":
+      self.log.notice("Request is Done: %s " % request)
+      return self.markJob(jobID, "Done")
+
+    self.log.notice("Request not Done: %s " % request)
+    return self.resetRequest(request.RequestID)
+
+  def treatFailedProdWithReq(self, jobID, request):
+    if request.Status == 'Done':
+      self.log.notice('Request is Done: %s ' % request)
+      return self.markJob(jobID, "Failed")
+
+    for op in request:
+      lfns = "\n\t".join(lfn.LFN for lfn in op)
+      self.log.notice('Operation for failed job: %s, %s, %s, %s\n\t%s' %
+                      (request.RequestID, op.Type, op.Status, op.Error, lfns))
+
+      if op.Type == "RemoveFile" and op.Status == 'Failed':
+        filesToRemove=[lfn.LFN for lfn in op]
+        #TODO: remove files using datamanager
+        self.log.notice("Removing files %s"%filesToRemove)
+        self.resetRequest(request.RequestID)
+      elif op.Status == "Failed":
+        self.log.notice("Can't handle operation of type: %s" % op.Type)
+
+    return S_OK()
+
+
+  def treatCompletedProdWithReq(self, jobID, request):
+    if request.Status == "Done":
+      self.log.notice("Request is Done: %s " % request )
+      return self.markJob(jobID, "Done")
+
+    for op in request:
+      self.log.info("Operation for completed job: %s, %s, %s, %s" %
+                   (request.RequestID, op.Type, op.Status, op.Error))
+      if op.Type == 'ReplicateAndRegister' and op.Status == 'Failed':
+        # Check if it failed because the file no longer exists
+        for lfn in op:
+          if lfn.Error == "No such file":
+            return self.markJob(jobID, "Done")
+        return self.resetRequest(requestID)
+
+      elif op.Status == "Failed":
+        self.log.notice("Cannot handle Operation of Type: %s " % op.Type )
+
+    return S_OK()
+
+  def checkJobs(self, jobIDs, treatJobWithNoReq, treatJobWithReq):
     """ docs... """
 
-    checkMinorStatList = []
-    checkAppStatList = []
-    markJobsDoneList = []
-    resetRequestsList = []
-
     res = self.reqClient.readRequestsForJobs(jobIDs)
-
     if not res['OK']:
       self.logError('Failure to read requests for jobs', res['Message'])
       return res
@@ -111,87 +184,44 @@ class JobResetAgent(AgentModule):
       if ((jobID not in result['Successful'] and jobID not in result['Failed']) or
          (jobID in result['Failed'] and result['Failed'][jobID]=='Request not found')):
         self.log.notice("No request found for job: %s" % jobID)
-        checkMinorStatList.append(jobID)
+        treatJobWithNoReq(jobID)
 
       elif jobID in result['Successful']:
         self.log.notice("Found the request for Job: %s " % jobID)
         request = result['Successful'][jobID]
-
-        if request.Status == "Done":
-          self.log.notice("Request is Done: %s " % request)
-          self.log.notice("Setting job %s done" % jobID)
-          markJobsDoneList.append(jobID)
-        else:
-          self.log.notice("Request not Done: %s " % request)
-          self.log.notice("Resetting request for Job")
-          resetRequestsList.append(request.RequestID)
-
-    if checkMinorStatList:
-      res = self.jobMonClient.getJobsMinorStatus(checkMinorStatList)
-      if not res['OK']:
-        self.logError("Failure to get Minor Status", "Job IDs: %s, Message: %s" % (checkMinorStatList,
-                                                                                   res['Message']))
-        return res
-
-      for jobID in checkMinorStatList:
-        if jobID in res['Value']:
-          minorStatus = res['Value'][jobID]['MinorStatus']
-          self.log.notice("Job %s has %s minor status" % (jobID, minorStatus))
-          if minorStatus in FINAL_MINOR_STATES:
-            checkAppStatList.append(jobID)
-          else:
-            self.log.warn("Minor Status for jobID %s is not in Final States, please check!" % jobID)
-
-    if checkAppStatList:
-      res = self.jobMonClient.getJobsApplicationStatus(checkAppStatList)
-      if not res['OK']:
-        self.logError("Failure to get Application Status", "Job IDs: %s, Message: %s" % (checkAppStatList,
-                                                                                         res['Message']))
-        return res
-
-      for jobID in checkAppStatList:
-        if jobID in res['Value']:
-          appStatus = res['Value'][jobID]['ApplicationStatus']
-          self.log.notice("Job %s has %s application status" % (jobID, appStatus))
-          if appStatus in FINAL_APP_STATES:
-            self.log.notice("Setting Job %s to Done" % jobID)
-            markJobsDoneList.append(jobID)
-
-
-    if markJobsDoneList:
-      self.markJobs(markJobsDoneList, "Done")
-
-    if resetRequestsList:
-      self.resetRequests(resetRequestsList)
+        treatJobWithReq(jobID, request)
 
     return S_OK("All Done")
 
-  def resetRequests(self, requestIDs):
+  def resetRequests(self, requestID):
     """reset requests given the requestIDs list"""
     if not self.enabled:
       return S_OK()
 
-    for reqID in requestIDs:
-      res = self.reqClient.resetFailedRequest(reqID, allR=True)
-      if not res["OK"] or res['Value']=="Not reset":
-        self.logError("Failed to reset request", "Request ID: %s, Message: %s"%(reqID, res['Message']))
-        continue
+    res = self.reqClient.resetFailedRequest(requestID, allR=True)
+    if not res["OK"] or res['Value']=="Not reset":
+      self.logError("Failed to reset request", "Request ID: %s, Message: %s"%(reqID, res['Message']))
+      return res
 
-      self.log.notice("Request %s is successfully reset" % reqID)
+    self.log.notice("Request %s is successfully reset" % reqID)
+    return S_OK()
 
-  def markJobs(self, jobIDs, status):
+  def markJob(self, jobID, status, minorStatus="Requests Done", application="CompletedJobChecker"):
     """ docs here..."""
+
+    self.log.notice("Marking job %s as %s" % (jobID, status))
+
     if not self.enabled:
       return S_OK()
 
-    for jobID in jobIDs:
-      self.log.notice("Marking job %s as %s" % (jobID, status))
-      res = self.jobStateUpdateClient.setJobStatus(jobID, status, "Requests Done", "CompletedJobChecker")
-      if not res["OK"]:
-        self.logError("Failed to set mark", "Job: %s as %s"%(jobID, status))
-        continue
+    res = self.jobStateUpdateClient.setJobStatus(jobID, status, minorStatus, application)
+    if not res["OK"]:
+      self.logError("Failed to set mark", "Job: %s as %s"%(jobID, status))
+      return res
 
-      self.log.notice("Job %s is successfully maked as %s" % (jobID, status))
+    self.log.notice("Job %s is successfully maked as %s" % (jobID, status))
+    return S_OK()
+
 
   def execute(self):
     """ main execution loop of Agent """
