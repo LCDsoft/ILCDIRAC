ab4550d27e33e553c5528059d5522f043a0d3360
Andre Sailer <andre.philippe.sailer@cern.ch>
2019-02-04 12:56:28 +0100

    ArchiveFiles: test: also test exception from rmtree

--- DataManagementSystem/Agent/RequestOperations/Tests/Test_ArchiveFiles.py
+++ DataManagementSystem/Agent/RequestOperations/Tests/Test_ArchiveFiles.py
@@ -224,8 +224,7 @@ def test_call_withError(archiveFiles, myMocker, listOfLFNs):
 
 def test_cleanup(archiveFiles, mocker):
   osMocker = mocker.patch(MODULE + '.os.remove', side_effect=OSError('No such file or directory'))
-  rmTreeMock = mocker.patch(MODULE + '.shutil.rmtree')
-
+  rmTreeMock = mocker.patch(MODULE + '.shutil.rmtree', side_effect=OSError('No such file or directory'))
   archiveFiles.parameterDict = {'ArchiveLFN': '/vo.lfn/nofile.tar'}
   archiveFiles._cleanup()
   osMocker.assert_called_with('nofile.tar')
