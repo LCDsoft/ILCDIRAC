fc6b82932409139aa98518605c4689722b7f1634
Andre Sailer <andre.philippe.sailer@cern.ch>
2019-04-11 18:46:40 +0200

    CI: patchSphinx: try lib and lib64 (diracos) for patching

--- docs/patchSphinx.sh
+++ docs/patchSphinx.sh
@@ -5,10 +5,12 @@
 
 SPHINXBIN=$( which sphinx-apidoc )
 SPHINXBASE=$( dirname $( dirname $SPHINXBIN ) )
-SPHINXEXT="$SPHINXBASE/lib/python2.7/site-packages/sphinx/ext"
-AUTODOC=$SPHINXEXT/autodoc/__init__.py
 
-cat > autodoc.patch << EOF
+for LIB in  lib lib64; do
+    SPHINXEXT="$SPHINXBASE/${LIB}/python2.7/site-packages/sphinx/ext"
+    AUTODOC=$SPHINXEXT/autodoc/__init__.py
+
+    cat > autodoc.patch << EOF
 --- autodoc.py~        2015-11-09 20:53:34.104991094 +0100
 +++ autodoc.py 2015-11-09 20:57:02.519263402 +0100
 @@ -56,9 +56,9 @@
@@ -26,8 +28,9 @@ cat > autodoc.patch << EOF
            )? $                   # and nothing more
 EOF
 
-patch -N $AUTODOC < autodoc.patch
+    patch -N $AUTODOC < autodoc.patch
 
-rm autodoc.patch
+    rm autodoc.patch
 
-echo Patched this file: $AUTODOC
+    echo Patched this file: $AUTODOC
+done
