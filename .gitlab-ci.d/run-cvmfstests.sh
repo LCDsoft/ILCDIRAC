129a799dfee5f04c18bd06825dd6015a080a90af
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-01-14 12:35:44 +0100

    CI: add junit outputs

--- .gitlab-ci.d/run-cvmfstests.sh
+++ .gitlab-ci.d/run-cvmfstests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source .gitlab-ci.d/set-reportstyle.sh
-export PYTEST_ADDOPTS="${PYTEST_ADDOPTS} --cov-append"
+export PYTEST_ADDOPTS="${PYTEST_ADDOPTS} --cov-append --junitxml=junit_job.xml"
 py.test $DIR/../Interfaces/API/NewInterface/Tests/Test_FullCVMFSTests.py
--- .gitlab-ci.d/run-setests.sh
+++ .gitlab-ci.d/run-setests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source .gitlab-ci.d/set-reportstyle.sh
-export PYTEST_ADDOPTS="${PYTEST_ADDOPTS} --cov-append"
+export PYTEST_ADDOPTS="${PYTEST_ADDOPTS} --cov-append --junit-xml=junit_se.xml"
 export XrdSecPROTOCOL=gsi,unix
 py.test $DIR/../Workflow/Modules/Test/Test_SEs.py
